53cf4c9a14d255a0bce651f992e638fde79cc6b9
Michael Matzen <mim@informatik.uni-kiel.de>
2009-08-30 13:44:10 +0000

    removed UI and Viewmagment classes

--- plugins/de.cau.cs.kieler.ksbase/src/de/cau/cs/kieler/ksbase/KSBasEPlugin.java
+++ plugins/de.cau.cs.kieler.ksbase/src/de/cau/cs/kieler/ksbase/KSBasEPlugin.java
@@ -14,41 +14,11 @@
  *****************************************************************************/
 package de.cau.cs.kieler.ksbase;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.core.commands.Category;
-import org.eclipse.core.commands.Command;
-import org.eclipse.core.expressions.ElementHandler;
-import org.eclipse.core.expressions.EvaluationResult;
-import org.eclipse.core.expressions.Expression;
-import org.eclipse.core.expressions.ExpressionConverter;
-import org.eclipse.core.expressions.IEvaluationContext;
-import org.eclipse.core.internal.expressions.EnablementExpression;
-import org.eclipse.core.internal.expressions.InstanceofExpression;
-import org.eclipse.core.internal.expressions.IterateExpression;
-import org.eclipse.core.internal.expressions.WithExpression;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.commands.ICommandService;
-import org.eclipse.ui.internal.expressions.AlwaysEnabledExpression;
-import org.eclipse.ui.menus.AbstractContributionFactory;
-import org.eclipse.ui.menus.CommandContributionItem;
-import org.eclipse.ui.menus.CommandContributionItemParameter;
-import org.eclipse.ui.menus.IContributionRoot;
-import org.eclipse.ui.menus.IMenuService;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.eclipse.ui.services.IServiceLocator;
 import org.osgi.framework.BundleContext;
 
-import de.cau.cs.kieler.ksbase.core.EditorTransformationSettings;
-import de.cau.cs.kieler.ksbase.core.Transformation;
 import de.cau.cs.kieler.ksbase.core.TransformationManager;
-import de.cau.cs.kieler.ksbase.ui.handler.TransformationCommandHandler;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -88,151 +58,9 @@ public class KSBasEPlugin extends AbstractUIPlugin {
         super.start(context);
         plugin = this;
         TransformationManager.instance.initializeTransformations();
-        createMenu();
     }
 
-    public void createMenu() {
-        // Foreach editor create menus:
-        IMenuService menuService = (IMenuService) PlatformUI.getWorkbench()
-                .getService(IMenuService.class);
-        final ICommandService cmdService = (ICommandService) PlatformUI
-                .getWorkbench().getService(ICommandService.class);
-        final Category kielerCategory = cmdService
-                .getCategory("de.cau.cs.kieler.commands.category");
-        //
-        // !! Hier weiter mit den anderen Menüs und vor allem den visibleWhen
-        // Expressions !!
-
-        for (final EditorTransformationSettings settings : TransformationManager
-                .instance.getEditors()) {
-            
-            if (settings.isShownInMenu()
-                    && settings.getMenuLocation().length() > 0) {
-                // Create menu contributions
-                AbstractContributionFactory menuContribution = new AbstractContributionFactory(
-                        settings.getMenuLocation(), null) {
-
-                    @SuppressWarnings("restriction")
-                    @Override
-                    public void createContributionItems(
-                            IServiceLocator serviceLocator,
-                            IContributionRoot additions) {
-                        
-                        MenuManager dynamicMenu = new MenuManager(settings
-                                .getMenuName(), "de.cau.cs.kieler.ksbase.menu."
-                                + settings.getMenuName());
-                        for (final Transformation t : settings
-                                .getTransformations()) {
-                            String cmdID = "de.cau.cs.kieler.ksbase.commands."
-                                    + settings.getMenuName() + "."
-                                    + t.getName().replace(' ', '_');
-                            Command transformationCommand = cmdService
-                                    .getCommand(cmdID);
-                            if (!transformationCommand.isDefined()) {
-                                transformationCommand.define(t.getName(), "",
-                                        kielerCategory);
-                            }
-                            transformationCommand
-                                    .setHandler(new TransformationCommandHandler(
-                                            settings, t));
-                            CommandContributionItemParameter p = new CommandContributionItemParameter(
-                                    PlatformUI.getWorkbench(), null, cmdID,
-                                    CommandContributionItem.STYLE_PUSH);
-                            CommandContributionItem cmd = new CommandContributionItem(
-                                    p);
-                            if (t.getPartConfig().length > 0) {
-                                try {
-                                    
-                                    IterateExpression iterate = new IterateExpression(
-                                            "or", "false");
-                                    for (String part : t.getPartConfig()) {
-                                        InstanceofExpression instance = new InstanceofExpression(
-                                                part);
-                                        iterate.add(instance);
-                                    }
-                                    
-                                    additions.registerVisibilityForChild(cmd,
-                                            iterate);
-                                    dynamicMenu.add(cmd);
-                                } catch (CoreException e) {
-
-                                }
-                            }
-                        }
-                        
-                        WithExpression we = new WithExpression("activeEditor");
-                        
-                        InstanceofExpression exp = new InstanceofExpression(
-                                settings.getEditor());
-                        we.add(exp);
-                        
-                        additions.addContributionItem(dynamicMenu, we);
-                    }
-                };
-                menuService.addContributionFactory(menuContribution);
-            }
-            if (settings.isShownIToolbar() 
-                    && settings.getToolbarLocation().length() > 0) {
-                // Create menu contributions
-                AbstractContributionFactory toolbarContribution = new AbstractContributionFactory(
-                        settings.getToolbarLocation(), null) {
-
-                    @SuppressWarnings("restriction")
-                    @Override
-                    public void createContributionItems(
-                            IServiceLocator serviceLocator,
-                            IContributionRoot additions) {
-                        
-                        ToolBarManager dynamicToolbar = new ToolBarManager();
-                        
-                                //+ settings.getMenuName());
-                        for (final Transformation t : settings
-                                .getTransformations()) {
-                            String cmdID = "de.cau.cs.kieler.ksbase.commands."
-                                    + settings.getMenuName() + "."
-                                    + t.getName().replace(' ', '_');
-                            Command transformationCommand = cmdService
-                                    .getCommand(cmdID);
-                            if (!transformationCommand.isDefined()) {
-                                transformationCommand.define(t.getName(), "",
-                                        kielerCategory);
-                            }
-                            transformationCommand
-                                    .setHandler(new TransformationCommandHandler(
-                                            settings, t));
-                            CommandContributionItemParameter p = new CommandContributionItemParameter(
-                                    PlatformUI.getWorkbench(), null, cmdID,
-                                    CommandContributionItem.STYLE_PUSH);
-                            CommandContributionItem cmd = new CommandContributionItem(
-                                    p);
-                            if (t.getPartConfig().length > 0) {
-                                try {
-                                    
-                                    
-                                    IterateExpression iterate = new IterateExpression(
-                                            "or", "false");
-                                    
-                                    for (String part : t.getPartConfig()) {
-                                        InstanceofExpression instance = new InstanceofExpression(
-                                                part);
-                                        iterate.add(instance);
-                                    }
-                                    /*
-                                    additions.registerVisibilityForChild(cmd,
-                                            iterate);
-                                    */
-                                    dynamicToolbar.add(cmd);
-                                } catch (CoreException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    }
-                };
-                menuService.addContributionFactory(toolbarContribution);
-            }
-        }
-    }
+    
 
     /*
      * (non-Javadoc)
--- plugins/de.cau.cs.kieler.ksbase/src/de/cau/cs/kieler/ksbase/core/TransformationManager.java
+++ plugins/de.cau.cs.kieler.ksbase/src/de/cau/cs/kieler/ksbase/core/TransformationManager.java
@@ -14,8 +14,6 @@
  *****************************************************************************/
 package de.cau.cs.kieler.ksbase.core;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -23,42 +21,11 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URI;
-import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CommandStack;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CanonicalEditPolicy;
-import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramCommandStack;
-import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
-import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.ui.ide.IDE;
-import org.eclipse.ui.part.FileEditorInput;
 
 import de.cau.cs.kieler.ksbase.KSBasEPlugin;
-import de.cau.cs.kieler.ksbase.ui.AutoLayoutTrigger;
-import de.cau.cs.kieler.ksbase.ui.handler.ExecuteTransformationRequest;
-import de.cau.cs.kieler.viewmanagement.ATrigger;
-import de.cau.cs.kieler.viewmanagement.RunLogic;
 
 /**
  * The main storage and management class. Contains a list of currently
@@ -85,109 +52,7 @@ public class TransformationManager {
         registeredEditors = new LinkedList<EditorTransformationSettings>();
     }
 
-    /**
-     * Creates and executes a transformation command by creating a request and
-     * execute the resulting command on the diagram command stack
-     * 
-     * @param event
-     *            Execution event for which this command should be created
-     * @param editor
-     *            The editor for which this transformation is
-     * @param transformation
-     *            The transformation that should be executed
-     */
-    public void createAndExecuteTransformationCommand(ExecutionEvent event,
-            EditorTransformationSettings editor, Transformation transformation) {
-        IEditorPart activeEditor = HandlerUtil.getActiveEditor(event);
-
-        ISelection selection = HandlerUtil.getActiveWorkbenchWindow(event)
-                .getSelectionService().getSelection();
-
-        if (selection instanceof StructuredSelection && !selection.isEmpty()) {
-
-            // First, we have to store the transformation file because Xtend
-            // doesn't execute strings
-            // We have to do this every time, because we can't be sure that the
-            // file we wrote last time is still valid. We will write it
-            // to the meta-inf folder:
-
-            EditPart selectedElement = (EditPart) ((StructuredSelection) selection)
-                    .getFirstElement();
-            File file = null;
-            try {
-                IPath path = ResourcesPlugin.getPlugin().getStateLocation();
-                file = File.createTempFile("extension", ".ext", new File(path
-                        .toOSString()));
-                FileOutputStream out = new FileOutputStream(file);
-                if (!file.exists()) {
-                    file.createNewFile();
-                }
-                out.write(editor.getExtFile().getBytes());
-                out.flush();
-                out.close();
-
-                // IProject p =
-                // ((FileEditorInput)activeEditor.getEditorInput()).getFile().getProject();
-                // System.out.println(p.getName());
-
-                // Create request
-                ExecuteTransformationRequest request = new ExecuteTransformationRequest(
-                        activeEditor, transformation.getTransformationName(),
-                        file.getAbsolutePath(), selection, transformation
-                                .getNumSelections(), editor
-                                .getModelPackageClass());
-                Command transformationCommand = selectedElement
-                        .getCommand(request);
-
-                // gets a command stack to execute the command
-                DiagramCommandStack commandStack = null;
-                Object adapter = activeEditor.getAdapter(CommandStack.class);
-                if (adapter instanceof DiagramCommandStack)
-                    commandStack = (DiagramCommandStack) adapter;
-                if (commandStack == null)
-                    commandStack = new DiagramCommandStack(null);
-                commandStack.execute(transformationCommand);
-            } catch (FileNotFoundException e) {
-
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } finally {
-                // Remove temporary Xtend file
-                if (file != null)
-                    file.delete();
-                // update edit policies, so gmf will generate diagram elements
-                // for model elements which have been generated during the
-                // transformation but
-                // not translated to gmf now:
-                if (activeEditor instanceof DiagramEditor) {
-                    EObject obj = ((View) ((DiagramEditor) activeEditor)
-                            .getDiagramEditPart().getModel()).getElement();
-                    List<?> editPolicies = CanonicalEditPolicy
-                            .getRegisteredEditPolicies(obj);
-                    for (Iterator<?> it = editPolicies.iterator(); it.hasNext();) {
-                        CanonicalEditPolicy nextEditPolicy = (CanonicalEditPolicy) it
-                                .next();
-                        nextEditPolicy.refresh();
-                    }
-
-                    // If auto-layout is activated, execute now:
-                    if (editor.isPerformAutoLayout()) {
-                        EditPart e = ((DiagramEditor) activeEditor)
-                                .getDiagramEditPart().getRoot().getContents();
-                        while (!(e instanceof ShapeEditPart)) {
-                            e = (EditPart) e.getChildren().get(0);
-                        }
-                        ((AutoLayoutTrigger) RunLogic
-                                .getTrigger("de.cau.cs.kieler.ksbase.AutoLayoutTrigger"))
-                                .triggerAutoLayout(e, activeEditor);
-
-                    }
-                }
-
-            }
-        }
-    }
+ 
 
     /**
      * Get the currently registered editors
