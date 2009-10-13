a0d5dd387f55ef89078e37974bd9455342ed75ca
Michael Matzen <mim@informatik.uni-kiel.de>
2009-10-13 15:04:51 +0000

    deleting old project and changing plug-in -> java project

--- plugins/de.cau.cs.kieler.ksbase/src/de/cau/cs/kieler/ksbase/core/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Core package of the KSBasE plug-in.
- * Contains a custom MWE workflow implementation and storage 
- * classes for editors and transformations.  
- */
-package de.cau.cs.kieler.ksbase.core;
--- plugins/de.cau.cs.kieler.ksbase/src/de/cau/cs/kieler/ksbase/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Base package of the KSBasE core plug-in.
- */
-package de.cau.cs.kieler.ksbase;
