771871a1a52068f56d047e818d6e52abf4820b0f
Martin Riess <mri@informatik.uni-kiel.de>
2010-05-18 13:07:48 +0000

    Added spline support. Moved prefence page to KIELER.

--- plugins/de.cau.cs.kieler.graphs/src/de/cau/cs/kieler/graphs/custom/NodeLayout.java
+++ plugins/de.cau.cs.kieler.graphs/src/de/cau/cs/kieler/graphs/custom/NodeLayout.java
@@ -1,3 +1,16 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.graphs.custom;
 
 import org.eclipse.draw2d.AbstractHintLayout;
@@ -9,6 +22,8 @@ import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.WrappingLabel;
 
 /**
+ * A basic layout for compound nodes.
+ * 
  * @author msp
  */
 public class NodeLayout extends AbstractHintLayout {
