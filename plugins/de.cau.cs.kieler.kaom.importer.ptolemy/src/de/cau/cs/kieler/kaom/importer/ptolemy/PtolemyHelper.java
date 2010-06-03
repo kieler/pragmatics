7b4ad12c163a37f501e1b9facbe7d0f8dcfd1171
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2010-06-03 18:23:16 +0000

    optimization to remove ports of states for correct modal model rendering

--- plugins/de.cau.cs.kieler.kaom.importer.ptolemy/src/de/cau/cs/kieler/kaom/importer/ptolemy/PtolemyHelper.java
+++ plugins/de.cau.cs.kieler.kaom.importer.ptolemy/src/de/cau/cs/kieler/kaom/importer/ptolemy/PtolemyHelper.java
@@ -8,7 +8,9 @@ import org.eclipse.xtend.expression.ExecutionContext;
 import org.eclipse.xtend.expression.IExecutionContextAware;
 
 import ptolemy.Moml.EntityType;
+import ptolemy.actor.IOPort;
 import ptolemy.actor.TypedCompositeActor;
+import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.Entity;
 import ptolemy.kernel.util.Attribute;
 import ptolemy.kernel.util.NamedObj;
@@ -23,12 +25,13 @@ import de.cau.cs.kieler.annotations.StringAnnotation;
 import de.cau.cs.kieler.kaom.KaomFactory;
 import de.cau.cs.kieler.kaom.Port;
 
-public class PtolemyHelper implements IExecutionContextAware{
+public class PtolemyHelper implements IExecutionContextAware {
 
     ExecutionContext ctx;
-    
-    public PtolemyHelper() {}
-    
+
+    public PtolemyHelper() {
+    }
+
     public List<Port> getPorts(List<EntityType> entities) {
         List<Port> ports = new BasicEList<Port>();
         for (EntityType entity : entities) {
@@ -41,47 +44,49 @@ public class PtolemyHelper implements IExecutionContextAware{
         List<Port> kaomPorts = new LinkedList<Port>();
         try {
             NamedObj actor = instanciatePtolemyEntity(ptolemyEntity);
-            
+
             // get lists of all ports
-            List ptolemyPorts = new LinkedList();
-            List inputs = ((ptolemy.actor.Actor) actor).inputPortList();
-            List outputs = ((ptolemy.actor.Actor) actor).outputPortList();
-            ptolemyPorts.addAll(inputs);
-            // avoid duplicates. A port may be both, input and output
-            for (Object output : outputs) {
-                if (ptolemyPorts.contains(output)) {
-                    continue;
-                }
-                ptolemyPorts.add(output);
-            }
-            
-            for (Object obj : ptolemyPorts) {
-                ptolemy.kernel.Port ptolemyPort = (ptolemy.kernel.Port) obj;
-                Port kaomPort = KaomFactory.eINSTANCE.createPort();
-                // find out whether it is an input or output (or both)
-                if (inputs.contains(ptolemyPort)) {
-                    Annotation isInput = AnnotationsFactory.eINSTANCE.createAnnotation();
-                    isInput.setName("input");
-                    kaomPort.getAnnotations().add(isInput);
-                }
-                if (outputs.contains(ptolemyPort)) {
-                    Annotation isOutput = AnnotationsFactory.eINSTANCE.createAnnotation();
-                    isOutput.setName("output");
-                    kaomPort.getAnnotations().add(isOutput);
-                }
-                // set the name
-                kaomPort.setName(ptolemyPort.getName());
-                
-                for(Object attribute : ptolemyPort.attributeList()){
-                    if(attribute instanceof Attribute){
-                        kaomPort.getAnnotations().add(getAnnotation((Attribute) attribute));
+            /*
+             * List ptolemyPorts = new LinkedList(); List inputs =
+             * ((ptolemy.actor.Actor) actor).inputPortList(); List outputs =
+             * ((ptolemy.actor.Actor) actor).outputPortList();
+             * ptolemyPorts.addAll(inputs); // avoid duplicates. A port may be
+             * both, input and output for (Object output : outputs) { if
+             * (ptolemyPorts.contains(output)) { continue; }
+             * ptolemyPorts.add(output); }
+             */
+            // for (Object obj : ptolemyPorts) {
+            for (Object obj : ((Entity) actor).portList()) {
+                if (obj instanceof IOPort) {
+                    IOPort ptolemyPort = (IOPort) obj;
+                    Port kaomPort = KaomFactory.eINSTANCE.createPort();
+                    // find out whether it is an input or output (or both)
+                    //if (inputs.contains(ptolemyPort)) {
+                    if (ptolemyPort.isInput()) {
+                        Annotation isInput = AnnotationsFactory.eINSTANCE.createAnnotation();
+                        isInput.setName("input");
+                        kaomPort.getAnnotations().add(isInput);
                     }
+                    //if (outputs.contains(ptolemyPort)) {
+                    if (ptolemyPort.isOutput()) {
+                        Annotation isOutput = AnnotationsFactory.eINSTANCE.createAnnotation();
+                        isOutput.setName("output");
+                        kaomPort.getAnnotations().add(isOutput);
+                    }
+                    // set the name
+                    kaomPort.setName(ptolemyPort.getName());
+
+                    for (Object attribute : ptolemyPort.attributeList()) {
+                        if (attribute instanceof Attribute) {
+                            kaomPort.getAnnotations().add(getAnnotation((Attribute) attribute));
+                        }
+                    }
+
+                    kaomPorts.add(kaomPort);
                 }
-                
-                kaomPorts.add(kaomPort);
             }
-        } catch (XmlException xe){
-            System.out.println("Could not instanciate entity: "+xe.getMessage());
+        } catch (XmlException xe) {
+            System.out.println("Could not instanciate entity: " + xe.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -90,40 +95,73 @@ public class PtolemyHelper implements IExecutionContextAware{
 
     /**
      * Instanciate a Ptolemy Entity for a given EntityType model object.
-     * @param entity given EMF EntityType model object
+     * 
+     * @param entity
+     *            given EMF EntityType model object
      * @return corresponding Ptolemy object
-     * @throws Exception may throw different Exceptions during parsing
+     * @throws Exception
+     *             may throw different Exceptions during parsing
      */
     private NamedObj instanciatePtolemyEntity(EntityType entity) throws Exception {
         String classname = entity.getClass1();
-        // use the Ptolemy internal Model ML (MoML) parser parsing XML and creates Ptolemy models
+        if (classname.equals("ptolemy.domains.modal.kernel.State")) {
+            return instanciatePtolemyState(entity);
+        }
+        return instanciatePtolemyActor(entity);
+    }
+
+    private NamedObj instanciatePtolemyActor(EntityType entity) throws Exception {
+        String classname = entity.getClass1();
+        // use the Ptolemy internal Model ML (MoML) parser parsing XML and
+        // creates Ptolemy models
         MoMLParser parser = new MoMLParser();
         // atomic actors require a valid parent so create a dummy parent
         String parent = "<entity name=\"TopLevel\" class=\"ptolemy.actor.TypedCompositeActor\">";
         // embed the real entity description in the parent
-        String child = parent + "<entity name=\""+entity.getName()+"\"class=\""+classname+"\"/> </entity>";
+        String child = parent + "<entity name=\"" + entity.getName() + "\"class=\"" + classname
+                + "\"/> </entity>";
         // let the parser do the job
         NamedObj actor = parser.parse(child);
-        actor = (Entity)((TypedCompositeActor)actor).entityList().get(0);
+        actor = (Entity) ((TypedCompositeActor) actor).entityList().get(0);
+        return actor;
+    }
+
+    private NamedObj instanciatePtolemyState(EntityType entity) throws Exception {
+        String classname = entity.getClass1();
+        // use the Ptolemy internal Model ML (MoML) parser parsing XML and
+        // creates Ptolemy models
+        MoMLParser parser = new MoMLParser();
+        // atomic actors require a valid parent so create a dummy parent
+        // states may only be in a ModalController and not in a normal
+        // CompositeActor
+        String parent = "<entity name=\"TopLevel\" class=\"ptolemy.domains.modal.modal.ModalController\">";
+        // embed the real entity description in the parent
+        String child = parent + "<entity name=\"" + entity.getName() + "\"class=\"" + classname
+                + "\"/> </entity>";
+        // let the parser do the job
+        NamedObj actor = parser.parse(child);
+        actor = (Entity) ((CompositeEntity) actor).entityList().get(0);
         return actor;
     }
 
     /**
-     * Transform Ptolemy Attribute to a KAOM Annotation including all nested Attributes.
+     * Transform Ptolemy Attribute to a KAOM Annotation including all nested
+     * Attributes.
+     * 
      * @param ptolemyAttribute
      * @return the created KAOM Annotation
      */
-    static private Annotation getAnnotation(Attribute ptolemyAttribute){
+    static private Annotation getAnnotation(Attribute ptolemyAttribute) {
         StringAnnotation kaomAnnotation = AnnotationsFactory.eINSTANCE.createStringAnnotation();
         StringAnnotation classAnnotation = AnnotationsFactory.eINSTANCE.createStringAnnotation();
-        
+
         kaomAnnotation.setName(ptolemyAttribute.getName());
         classAnnotation.setName("ptolemyClass");
         classAnnotation.setValue(ptolemyAttribute.getClassName());
         kaomAnnotation.getAnnotations().add(classAnnotation);
-        
-        for(Object childAttribute : ptolemyAttribute.attributeList()){
-            if(childAttribute instanceof Attribute){
+
+        for (Object childAttribute : ptolemyAttribute.attributeList()) {
+            if (childAttribute instanceof Attribute) {
                 kaomAnnotation.getAnnotations().add(getAnnotation((Attribute) childAttribute));
             }
         }
