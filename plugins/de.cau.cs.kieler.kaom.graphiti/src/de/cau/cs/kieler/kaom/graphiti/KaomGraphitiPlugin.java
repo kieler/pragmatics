579977ef112acab63ffbc228df232ba47dbc6883
Atin Ruia <atr@informatik.uni-kiel.de>
2010-06-16 13:57:38 +0000

    changed properties

--- plugins/de.cau.cs.kieler.kaom.graphiti/src/de/cau/cs/kieler/kaom/graphiti/KaomGraphitiPlugin.java
+++ plugins/de.cau.cs.kieler.kaom.graphiti/src/de/cau/cs/kieler/kaom/graphiti/KaomGraphitiPlugin.java
@@ -6,18 +6,18 @@ import org.osgi.framework.BundleContext;
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class KaomGraphitiPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "de.cau.cs.kieler.kaom.graphiti";
 
 	// The shared instance
-	private static Activator plugin;
+	private static KaomGraphitiPlugin plugin;
 	
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public KaomGraphitiPlugin() {
 	}
 
 	/*
@@ -43,7 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static KaomGraphitiPlugin getDefault() {
 		return plugin;
 	}
 
