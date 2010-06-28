93eaf08a62b6a4f60bed7e74267c97d7bfa89b02
Paul Klose <pkl@informatik.uni-kiel.de>
2010-06-28 09:27:07 +0000

    ui extended

--- plugins/de.cau.cs.kieler.kex.ui/src/de/cau/cs/kieler/kex/ui/wizards/exportwizard/ExamplePage.java
+++ plugins/de.cau.cs.kieler.kex.ui/src/de/cau/cs/kieler/kex/ui/wizards/exportwizard/ExamplePage.java
@@ -1,5 +1,6 @@
 package de.cau.cs.kieler.kex.ui.wizards.exportwizard;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
@@ -9,96 +10,97 @@ import org.eclipse.swt.widgets.Text;
 
 public class ExamplePage extends WizardPage {
 
-    private String projectId;
+	private String projectId;
 
-    private String location;
+	private String location;
 
-    private Text id;
-    // TODO Textarea waere besser geeignet.
-    private Text exampleName;
+	private Text id;
+	// TODO Textarea waere besser geeignet.
+	private Text exampleName;
 
-    private Text exampleDescription;
+	private Text exampleDescription;
 
-    private Text version;
+	private Text version;
 
-    public ExamplePage(String pageName) {
-        super(pageName);
-        setTitle("Address Information");
-        setDescription("Please enter your address information");
-    }
+	public ExamplePage(String pageName, IProject[] localProjects) {
+		super(pageName);
+		// this.localProjects = lo
+		setTitle("Address Information");
+		setDescription("Please enter your address information");
+	}
 
-    public void createControl(Composite parent) {
+	public void createControl(Composite parent) {
 
-        // TODO projectId und location noch zusammenbauen
-        // schönen mechanismus überlegen
+		// TODO projectId und location noch zusammenbauen
+		// schï¿½nen mechanismus ï¿½berlegen
 
-        Composite composite = new Composite(parent, SWT.NONE);
-        GridLayout layout = new GridLayout();
-        layout.numColumns = 2;
-        composite.setLayout(layout);
-        setControl(composite);
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		composite.setLayout(layout);
+		setControl(composite);
 
-        new Label(composite, SWT.NONE).setText("ID");
-        setId(new Text(composite, SWT.NONE));
+		new Label(composite, SWT.NONE).setText("ID");
+		setId(new Text(composite, SWT.NONE));
 
-        new Label(composite, SWT.NONE).setText("Name");
-        setExampleName(new Text(composite, SWT.NONE));
+		new Label(composite, SWT.NONE).setText("Name");
+		setExampleName(new Text(composite, SWT.NONE));
 
-        Text text = new Text(composite, SWT.NONE);
-        text.setText("1.0.0");
-        setVersion(text);
+		Text text = new Text(composite, SWT.NONE);
+		text.setText("1.0.0");
+		setVersion(text);
 
-        new Label(composite, SWT.NONE).setText("description");
-        setExampleDescription(new Text(composite, SWT.NONE));
+		new Label(composite, SWT.NONE).setText("description");
+		setExampleDescription(new Text(composite, SWT.NONE));
 
-    }
+	}
 
-    public void setId(Text id) {
-        this.id = id;
-    }
+	public void setId(Text id) {
+		this.id = id;
+	}
 
-    public Text getId() {
-        return id;
-    }
+	public Text getId() {
+		return id;
+	}
 
-    public void setVersion(Text version) {
-        this.version = version;
-    }
+	public void setVersion(Text version) {
+		this.version = version;
+	}
 
-    public Text getVersion() {
-        return version;
-    }
+	public Text getVersion() {
+		return version;
+	}
 
-    public void setExampleName(Text exampleName) {
-        this.exampleName = exampleName;
-    }
+	public void setExampleName(Text exampleName) {
+		this.exampleName = exampleName;
+	}
 
-    public Text getExampleName() {
-        return this.exampleName;
-    }
+	public Text getExampleName() {
+		return this.exampleName;
+	}
 
-    public void setExampleDescription(Text exampleDescription) {
-        this.exampleDescription = exampleDescription;
-    }
+	public void setExampleDescription(Text exampleDescription) {
+		this.exampleDescription = exampleDescription;
+	}
 
-    public Text getExampleDescription() {
-        return exampleDescription;
-    }
+	public Text getExampleDescription() {
+		return exampleDescription;
+	}
 
-    public void setProjectId(String projectId) {
-        this.projectId = projectId;
-    }
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
 
-    public String getProjectId() {
-        return projectId;
-    }
+	public String getProjectId() {
+		return projectId;
+	}
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-    public String getLocation() {
-        return location;
-    }
+	public String getLocation() {
+		return location;
+	}
 
 }
--- plugins/de.cau.cs.kieler.kex.ui/src/de/cau/cs/kieler/kex/ui/wizards/exportwizard/ExportExampleWizard.java
+++ plugins/de.cau.cs.kieler.kex.ui/src/de/cau/cs/kieler/kex/ui/wizards/exportwizard/ExportExampleWizard.java
@@ -3,6 +3,7 @@ package de.cau.cs.kieler.kex.ui.wizards.exportwizard;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.Wizard;
@@ -13,41 +14,51 @@ import de.cau.cs.kieler.kex.controller.ExampleManager;
 
 public class ExportExampleWizard extends Wizard implements IWizard {
 
-    private ExamplePage examplePage;
+	private ExamplePage examplePage;
 
-    @Override
-    public void addPages() {
-        examplePage = new ExamplePage("examplePage");
+	private IProject[] localProjects;
 
-    }
+	public ExportExampleWizard() {
+		IProject[] localProjects = ExampleManager.get().getLocalProjects();
 
-    @Override
-    public boolean performFinish() {
+		// TODO lade hier die projectList
+	}
 
-        // TODO exampleResource mit einbinden...
+	@Override
+	public void addPages() {
 
-        String projectId = examplePage.getProjectId();
-        String location = examplePage.getLocation();
+		examplePage = new ExamplePage("examplePage", localProjects);
 
-        Map<ExampleElement, Object> result = new HashMap<ExampleElement, Object>();
-        String exampleId = examplePage.getId().toString();
-        result.put(ExampleElement.ID, exampleId);
-        String exampleName = examplePage.getName().toString();
-        result.put(ExampleElement.NAME, exampleName);
-        String exampleDescription = examplePage.getDescription().toString();
-        result.put(ExampleElement.DESCRIPTION, exampleDescription);
-        String exampleVersion = examplePage.getVersion().toString();
-        result.put(ExampleElement.VERSION, exampleVersion);
-        result.put(ExampleElement.PROJECTID, examplePage.getProjectId());
-        result.put(ExampleElement.LOCATION, examplePage.getLocation());
+	}
 
-        try {
-            ExampleManager.get().exportExample(result);
-        } catch (KielerException e) {
-            MessageDialog.openError(getShell(), "Error while exporting example.", e.getMessage());
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean performFinish() {
+
+		// TODO exampleResource mit einbinden...
+
+		String projectId = examplePage.getProjectId();
+		String location = examplePage.getLocation();
+
+		Map<ExampleElement, Object> result = new HashMap<ExampleElement, Object>();
+		String exampleId = examplePage.getId().toString();
+		result.put(ExampleElement.ID, exampleId);
+		String exampleName = examplePage.getName().toString();
+		result.put(ExampleElement.NAME, exampleName);
+		String exampleDescription = examplePage.getDescription().toString();
+		result.put(ExampleElement.DESCRIPTION, exampleDescription);
+		String exampleVersion = examplePage.getVersion().toString();
+		result.put(ExampleElement.VERSION, exampleVersion);
+		result.put(ExampleElement.PROJECTID, examplePage.getProjectId());
+		result.put(ExampleElement.LOCATION, examplePage.getLocation());
+
+		try {
+			ExampleManager.get().exportExample(result);
+		} catch (KielerException e) {
+			MessageDialog.openError(getShell(),
+					"Error while exporting example.", e.getMessage());
+			return false;
+		}
+		return true;
+	}
 
 }
