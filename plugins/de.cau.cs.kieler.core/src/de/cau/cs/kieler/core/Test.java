9a075d48ef4c4e13fa8003bc8fbb96bb00defee4
Hauke Fuhrmann <haf@informatik.uni-kiel.de>
2010-08-03 11:35:32 +0000

    test case for svn mime type

---
+++ plugins/de.cau.cs.kieler.core/src/de/cau/cs/kieler/core/Test.java
@@ -0,0 +1,12 @@
+package de.cau.cs.kieler.core;
+
+public class Test {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+       System.out.println("Test of correct Java mime types.");
+    }
+
+}
