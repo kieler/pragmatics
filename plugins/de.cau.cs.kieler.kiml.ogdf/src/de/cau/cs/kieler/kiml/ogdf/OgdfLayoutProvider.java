6e6bbe51f75658552e868a8ad961a80142548e3e
Miro Spoenemann <msp@informatik.uni-kiel.de>
2010-08-11 07:39:51 +0000

    removed experimental warning

--- plugins/de.cau.cs.kieler.kiml.ogdf/src/de/cau/cs/kieler/kiml/ogdf/OgdfLayoutProvider.java
+++ plugins/de.cau.cs.kieler.kiml.ogdf/src/de/cau/cs/kieler/kiml/ogdf/OgdfLayoutProvider.java
@@ -13,23 +13,10 @@
  */
 package de.cau.cs.kieler.kiml.ogdf;
 
-import org.eclipse.jface.dialogs.MessageDialogWithToggle;
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.eclipse.jface.window.Window;
-import org.eclipse.swt.widgets.Display;
-
 import de.cau.cs.kieler.core.KielerException;
 import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
-import de.cau.cs.kieler.core.kgraph.KEdge;
-import de.cau.cs.kieler.core.kgraph.KLabel;
 import de.cau.cs.kieler.core.kgraph.KNode;
-import de.cau.cs.kieler.core.kgraph.KPort;
-import de.cau.cs.kieler.core.util.Maybe;
 import de.cau.cs.kieler.kiml.AbstractLayoutProvider;
-import de.cau.cs.kieler.kiml.klayoutdata.KEdgeLayout;
-import de.cau.cs.kieler.kiml.klayoutdata.KShapeLayout;
-import de.cau.cs.kieler.kiml.options.LayoutOptions;
-import de.cau.cs.kieler.kiml.util.KimlLayoutUtil;
 
 /**
  * The OGDF layout provider, that is the entry class used by KIML to call
@@ -118,8 +105,6 @@ public class OgdfLayoutProvider extends AbstractLayoutProvider {
         }
     }
 
-    private static final String PREF_EXPERIMENTAL = "experimental.dialog";
-
     /**
      * {@inheritDoc}
      */
@@ -133,57 +118,8 @@ public class OgdfLayoutProvider extends AbstractLayoutProvider {
                             + " Please check the parameter in the extension point");
         }
 
-        // display a warning that this feature is experimental
-        final IPreferenceStore preferenceStore =
-                OgdfPlugin.getDefault().getPreferenceStore();
-        final Maybe<Boolean> docontinue = new Maybe<Boolean>();
-        docontinue.set(true);
-        if (!preferenceStore.getString(PREF_EXPERIMENTAL).equals(
-                MessageDialogWithToggle.ALWAYS)) {
-            final Display display = Display.getDefault();
-            display.syncExec(new Runnable() {
-                public void run() {
-                    Window dialog =
-                            MessageDialogWithToggle.openOkCancelConfirm(
-                                    display.getActiveShell(),
-                                    "Experimental Feature",
-                                    "Warning: The OGDF layouters feature is to be regarded as experimental"
-                                            + " and may lead to sudden crash of the Eclipse process. Continue?",
-                                    "Don't show this message again", true,
-                                    preferenceStore, PREF_EXPERIMENTAL);
-                    docontinue.set(dialog.getReturnCode() == Window.OK);
-                }
-            });
-        }
-
-        if (docontinue.get()) {
-            // layout the graph with the selected algorithm
-            layoutAlgorithm.doLayout(layoutNode, progressMonitor);
-        } else {
-            // mark the graph as not layouted
-            for (KNode child : layoutNode.getChildren()) {
-                KShapeLayout nodeLayout = KimlLayoutUtil.getShapeLayout(child);
-                LayoutOptions.setBoolean(nodeLayout, LayoutOptions.NO_LAYOUT,
-                        true);
-                for (KPort port : layoutNode.getPorts()) {
-                    KShapeLayout portLayout =
-                            KimlLayoutUtil.getShapeLayout(port);
-                    LayoutOptions.setBoolean(portLayout,
-                            LayoutOptions.NO_LAYOUT, true);
-                }
-                for (KEdge edge : layoutNode.getOutgoingEdges()) {
-                    KEdgeLayout edgeLayout = KimlLayoutUtil.getEdgeLayout(edge);
-                    LayoutOptions.setBoolean(edgeLayout,
-                            LayoutOptions.NO_LAYOUT, true);
-                    for (KLabel label : edge.getLabels()) {
-                        KShapeLayout labelLayout =
-                                KimlLayoutUtil.getShapeLayout(label);
-                        LayoutOptions.setBoolean(labelLayout,
-                                LayoutOptions.NO_LAYOUT, true);
-                    }
-                }
-            }
-        }
+        // layout the graph with the selected algorithm
+        layoutAlgorithm.doLayout(layoutNode, progressMonitor);
     }
 
     /**
