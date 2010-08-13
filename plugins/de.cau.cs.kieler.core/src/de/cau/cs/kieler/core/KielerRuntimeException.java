e6dfa2cf726045abf8b0439c1cd1a8a47c888ada
Christian Schneider <chsch@informatik.uni-kiel.de>
2010-08-13 14:08:32 +0000

    added kieler runtime exception to kieler.core

---
+++ plugins/de.cau.cs.kieler.core/src/de/cau/cs/kieler/core/KielerRuntimeException.java
@@ -0,0 +1,35 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.core;
+
+/**
+ * Kieler runtime exception.
+ * Might be helpful to indicate erroneous behavior
+ * of (overloaded) methods that are not allowed to throw exceptions.
+ * 
+ * @author chsch
+ */
+public class KielerRuntimeException extends RuntimeException {
+
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 5052139882180628740L;
+
+    public KielerRuntimeException(String message) {
+        super(message);
+    }
+    
+}
