7282584f3450249640d1fd0b115e13a3c0836539
Miro Spoenemann <msp@informatik.uni-kiel.de>
2010-08-16 10:32:53 +0000

    Share project "de.cau.cs.kieler.graphs.diagram.custom" into "https://rtsys.informatik.uni-kiel.de/svn/kieler"

---
+++ plugins/de.cau.cs.kieler.graphs.diagram.custom/src/de/cau/cs/kieler/graphs/diagram/custom/GraphsDiagramLayoutManager.java
@@ -0,0 +1,25 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.graphs.diagram.custom;
+
+import de.cau.cs.kieler.kiml.gmf.GmfDiagramLayoutManager;
+
+/**
+ * Specialized Layout Manager for Graphs diagrams.
+ *
+ * @author msp
+ */
+public class GraphsDiagramLayoutManager extends GmfDiagramLayoutManager {
+
+}
