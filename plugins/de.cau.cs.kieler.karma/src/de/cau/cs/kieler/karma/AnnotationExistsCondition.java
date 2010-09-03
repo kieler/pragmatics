dd2c3594e2b40809b23222d520d2303dc43e38ee
Christoph Krueger <ckru@informatik.uni-kiel.de>
2010-09-03 13:30:16 +0000

    ptolemy entitys now display at least some kind of box

---
+++ plugins/de.cau.cs.kieler.karma/src/de/cau/cs/kieler/karma/AnnotationExistsCondition.java
@@ -0,0 +1,30 @@
+package de.cau.cs.kieler.karma;
+
+import org.eclipse.emf.ecore.EObject;
+
+import de.cau.cs.kieler.core.annotations.Annotatable;
+import de.cau.cs.kieler.core.annotations.Annotation;
+
+public class AnnotationExistsCondition extends ICustomCondition<EObject> {
+
+    @Override
+    public boolean evaluate(final EObject object) {
+        if (object instanceof Annotatable) {
+            Annotatable annotatable = ((Annotatable) object);
+            if (!annotatable.getAnnotations().isEmpty()) {
+                Annotation annotation = annotatable.getAnnotation(key);
+                if (annotation != null) {
+                    return true;
+                } else {
+                    for (Annotation ann : annotatable.getAnnotations()){
+                        if (evaluate(ann)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
