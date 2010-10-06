70c9ebd4d99f4dd378c614b4a48e4fbcdee00cb0
Christoph Krueger <ckru@informatik.uni-kiel.de>
2010-10-06 13:29:42 +0000

    Added karma editpart for labels

---
+++ plugins/de.cau.cs.kieler.karma/src/de/cau/cs/kieler/karma/AdvancedRenderingLabelEditPart.java
@@ -0,0 +1,75 @@
+package de.cau.cs.kieler.karma;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.LabelEditPart;
+import org.eclipse.gmf.runtime.notation.View;
+
+public class AdvancedRenderingLabelEditPart extends LabelEditPart implements IAdvancedRenderingEditPart {
+
+    /**
+     * Figure that that represents the model element.
+     */
+    // SUPPRESS CHECKSTYLE NEXT VisibilityModifier
+    protected IFigure primaryShape;
+
+    /**
+     * Utility class containing the actual methods. Used to eliminate redundant code.
+     */
+    private AdvancedRenderingEditPartUtil util;
+
+    /**
+     * The constructor. Just calls super.
+     * 
+     * @param view
+     *            to be given to super
+     */
+    public AdvancedRenderingLabelEditPart(final View view) {
+        super(view);
+        String className = this.getClass().getName();
+        ConditionProvider conditionProvider = ConditionProvider.getInstance();
+        List<HashMap<String, Object>> conditions = conditionProvider.getPairs(className);
+        util = new AdvancedRenderingEditPartUtil(conditions);
+    }
+
+    @Override
+    public void handleNotificationEvent(final Notification notification) {
+        super.handleNotificationEvent(notification);
+        util.handleNotificationEvent(notification, primaryShape, this.getModelElement(), this);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean updateFigure(final IFigure figure) {
+        return util.updateFigure(figure, this.getModelElement(), this);
+    }
+
+    /**
+     * Getter of the model element for conveniences sake.
+     * 
+     * @return the modelElement of this editPart
+     */
+    public EObject getModelElement() {
+        return this.getNotationView().getElement();
+    }
+
+    /**
+     * Flag for calling the updateFigure method only once while initializing the diagram.
+     */
+    private boolean updateTriggerFigure = true;
+
+    @Override
+    public void refresh() {
+        super.refresh();
+        if (updateTriggerFigure) {
+            updateTriggerFigure = false;
+            updateFigure(primaryShape);
+        }
+    }
+    
+}
