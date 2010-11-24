124915beaa25c5c81abdd199ebe33b5497a116db
Soeren Hansen <soh@informatik.uni-kiel.de>
2010-11-24 12:12:40 +0000

    conform to new interface

--- plugins/de.cau.cs.kieler.kiml.graphiti/src/de/cau/cs/kieler/kiml/graphiti/GraphitiLayoutInspector.java
+++ plugins/de.cau.cs.kieler.kiml.graphiti/src/de/cau/cs/kieler/kiml/graphiti/GraphitiLayoutInspector.java
@@ -13,122 +13,34 @@
  */
 package de.cau.cs.kieler.kiml.graphiti;
 
-import java.util.List;
-
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gef.EditPart;
+import org.eclipse.gef.editparts.ZoomManager;
 import org.eclipse.graphiti.ui.internal.parts.IPictogramElementEditPart;
 
-import de.cau.cs.kieler.kiml.LayoutOptionData;
-import de.cau.cs.kieler.kiml.LayoutProviderData;
-import de.cau.cs.kieler.kiml.klayoutdata.KOption;
 import de.cau.cs.kieler.kiml.ui.layout.ILayoutInspector;
 
 /**
  * @author atr
- *
+ * 
  */
 public class GraphitiLayoutInspector implements ILayoutInspector {
 
     IPictogramElementEditPart focusEditPart;
-    
-  
-    
-    public GraphitiLayoutInspector(IPictogramElementEditPart editPart) {
+
+    public GraphitiLayoutInspector(final IPictogramElementEditPart editPart) {
         if (editPart == null) {
             throw new NullPointerException("editPart");
         }
         focusEditPart = editPart;
-    }
-   
-    
-    /**
-     * {@inheritDoc}
-     */
-    public void initOptions() {
-        // TODO Auto-generated method stub
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public List<LayoutOptionData> getOptionData() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public KOption getKOption(LayoutOptionData optionData, boolean create) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void removeKOption(LayoutOptionData optionData) {
-        // TODO Auto-generated method stub
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public LayoutProviderData getFocusLayouterData() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public EditPart getContainerPart() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public LayoutProviderData getContainerLayouterData() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void removeAllKOptions() {
-        // TODO Auto-generated method stub
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public Object getDefault(LayoutOptionData optionData) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public void setDefault(LayoutOptionData optionData, Object value) {
-        // TODO Auto-generated method stub
-
     }
 
     /**
      * {@inheritDoc}
      */
     public EditPart getFocusPart() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -136,15 +48,6 @@ public class GraphitiLayoutInspector implements ILayoutInspector {
      * {@inheritDoc}
      */
     public EObject getFocusModel() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public IFigure getDrawingLayer() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -152,7 +55,20 @@ public class GraphitiLayoutInspector implements ILayoutInspector {
      * {@inheritDoc}
      */
     public TransactionalEditingDomain getEditingDomain() {
-        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public IFigure getDrawingLayer() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public ZoomManager getZoomManager() {
         return null;
     }
 
