ed557a40e4794959a9f6fdd8ef64bacd800cc0ca
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2011-01-06 13:15:35 +0000

    - Added: Implementation for AreaAnalysis. - Changed: Removed unnecessary abbreviations from analysis components.

--- plugins/de.cau.cs.kieler.kiml.grana/src/de/cau/cs/kieler/kiml/grana/analyses/AreaAnalysis.java
+++ plugins/de.cau.cs.kieler.kiml.grana/src/de/cau/cs/kieler/kiml/grana/analyses/AreaAnalysis.java
@@ -14,12 +14,14 @@
 
 package de.cau.cs.kieler.kiml.grana.analyses;
 
+import java.awt.geom.Point2D;
 import java.util.Map;
 
 import de.cau.cs.kieler.core.KielerException;
 import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
 import de.cau.cs.kieler.core.kgraph.KNode;
 import de.cau.cs.kieler.kiml.grana.IAnalysis;
+import de.cau.cs.kieler.kiml.klayoutdata.KShapeLayout;
 
 
 /**
@@ -35,7 +37,7 @@ public class AreaAnalysis implements IAnalysis {
      */
     public static final String ANALYSIS_ID = "de.cau.cs.kieler.kiml.grana.area";
     
-
+    
     /**
      * {@inheritDoc}
      */
@@ -43,12 +45,69 @@ public class AreaAnalysis implements IAnalysis {
             final IKielerProgressMonitor progressMonitor) throws KielerException {
         
         progressMonitor.begin("Area analysis", 1);
-        
-        // TODO Do the actual analysis
-        
+        Point2D.Float area = computeArea(parentNode);
         progressMonitor.done();
         
-        return new Object[] {100, 10};
+        return new Object[] {
+                (int) area.x,
+                (int) area.y
+        };
     }
-
+    
+    /**
+     * Computes the size of the area occupied by the given node's children.
+     * 
+     * @param parentNode the parent node.
+     * @return a point whose x coordinate gives the width and whose y coordinate gives the
+     *         height of the drawing area used.
+     */
+    private Point2D.Float computeArea(final KNode parentNode) {
+        // The bounds of the area that is actually used for drawing nodes
+        float minX = Float.POSITIVE_INFINITY;
+        float maxX = Float.NEGATIVE_INFINITY;
+        float minY = Float.POSITIVE_INFINITY;
+        float maxY = Float.NEGATIVE_INFINITY;
+        
+        // Iterate through the parent node's children
+        for (KNode child : parentNode.getChildren()) {
+            float nodeX = 0.0f;
+            float nodeY = 0.0f;
+            float nodeWidth = 0.0f;
+            float nodeHeight = 0.0f;
+            
+            // Get the node's layout data, and the layout data of the node's label, if any
+            KShapeLayout layoutData = child.getData(KShapeLayout.class);
+            KShapeLayout labelLayoutData = child.getLabel() == null
+                ? null
+                : child.getLabel().getData(KShapeLayout.class);
+            
+            // Compute the node's size and position
+            if (layoutData != null) {
+                if (labelLayoutData != null) {
+                    nodeX = Math.min(layoutData.getXpos(), labelLayoutData.getXpos());
+                    nodeY = Math.min(layoutData.getYpos(), labelLayoutData.getYpos());
+                    
+                    nodeWidth = Math.max(layoutData.getWidth(),
+                            labelLayoutData.getXpos() + labelLayoutData.getWidth());
+                    nodeHeight = Math.max(layoutData.getHeight(),
+                            labelLayoutData.getYpos() + labelLayoutData.getHeight());
+                } else {
+                    nodeX = layoutData.getXpos();
+                    nodeY = layoutData.getYpos();
+                    
+                    nodeWidth = layoutData.getWidth();
+                    nodeHeight = layoutData.getHeight();
+                }
+            }
+            
+            // Compute the new bounds of the drawing area
+            minX = Math.min(minX, nodeX);
+            minY = Math.min(minY, nodeY);
+            
+            maxX = Math.max(maxX, nodeX + nodeWidth);
+            maxY = Math.max(maxY, nodeY + nodeHeight);
+        }
+        
+        return new Point2D.Float(maxX - minX, maxY - minY);
+    }
 }
