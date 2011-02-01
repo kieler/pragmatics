f4174b65349bb0a7335c7a6fde6ebfb519e1ee44
Soeren Hansen <soh@informatik.uni-kiel.de>
2011-02-01 14:59:06 +0000

    styleguide and comments

--- plugins/de.cau.cs.kieler.rail.editor/src/de/cau/cs/kieler/rail/editor/RailEditorActionBarContributor.java
+++ plugins/de.cau.cs.kieler.rail.editor/src/de/cau/cs/kieler/rail/editor/RailEditorActionBarContributor.java
@@ -1,3 +1,16 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.rail.editor;
 
 import org.eclipse.gef.editparts.ZoomManager;
@@ -5,6 +18,11 @@ import org.eclipse.gef.ui.actions.ZoomComboContributionItem;
 import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.ui.part.EditorActionBarContributor;
 
+/**
+ * Contribute zoom actions to the action bar.
+ * 
+ * @author soh
+ */
 public class RailEditorActionBarContributor extends EditorActionBarContributor {
 
     @Override
--- plugins/de.cau.cs.kieler.rail.editor/src/de/cau/cs/kieler/rail/editor/ReInitKrailDiagramCommand.java
+++ plugins/de.cau.cs.kieler.rail.editor/src/de/cau/cs/kieler/rail/editor/ReInitKrailDiagramCommand.java
@@ -1,3 +1,16 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.rail.editor;
 
 import org.eclipse.emf.ecore.EObject;
@@ -5,9 +18,18 @@ import org.eclipse.emf.ecore.EObject;
 import de.cau.cs.kieler.core.model.graphiti.ui.AbstractReInitGraphitiDiagramCommand;
 import de.cau.cs.kieler.rail.Topologie.Basegraph.Edge;
 
+/**
+ * 
+ * @author soh
+ */
 public class ReInitKrailDiagramCommand extends
         AbstractReInitGraphitiDiagramCommand {
 
+    /**
+     * 
+     * Creates a new ReInitKrailDiagramCommand.
+     * 
+     */
     public ReInitKrailDiagramCommand() {
         super(KrailDiagramEditor.DIAGRAM_TYPE, 0, false);
     }
