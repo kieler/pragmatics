e033a9244ba66d9431ab458edc8e06159a09380c
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2011-02-21 13:34:08 +0000

    - Added: List of layerless nodes to LayeredGraph. (#1487)

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/graph/LayeredGraph.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/graph/LayeredGraph.java
@@ -13,6 +13,7 @@
  */
 package de.cau.cs.kieler.klay.layered.graph;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
@@ -23,11 +24,12 @@ import de.cau.cs.kieler.kiml.options.PortType;
 import de.cau.cs.kieler.klay.layered.Properties;
 
 /**
- * A layered graph has a set of layers that contain the nodes.
- * Layout algorithms are required to layout the graph from left to right. If
- * another layout direction is desired, it can be obtained by pre-processing
- * and post-processing the graph.
+ * A layered graph has a set of layers that contain the nodes, as well as a
+ * list of nodes that are not yet assigned to a layer. Layout algorithms are
+ * required to layout the graph from left to right. If another layout direction
+ * is desired, it can be obtained by pre-processing and post-processing the graph.
  * 
+ * <p>
  * TODO add methods to rotate / mirror the graph for alternative layout directions
  *
  * @author msp
@@ -38,6 +40,8 @@ public class LayeredGraph extends LGraphElement {
     private KVector size = new KVector();
     /** the offset to be added to all positions. */
     private KVector offset = new KVector();
+    /** nodes that are not currently part of a layer. */
+    private List<LNode> layerlessNodes = new ArrayList<LNode>();
     /** the layers of the layered graph. */
     private List<Layer> layers = new LinkedList<Layer>();
     
@@ -68,6 +72,15 @@ public class LayeredGraph extends LGraphElement {
     public KVector getOffset() {
         return offset;
     }
+    
+    /**
+     * Returns the list of nodes that are not currently assigned to a layer.
+     * 
+     * @return the layerless nodes.
+     */
+    public List<LNode> getLayerlessNodes() {
+        return layerlessNodes;
+    }
 
     /**
      * Returns the list of layers of the graph.
@@ -84,7 +97,8 @@ public class LayeredGraph extends LGraphElement {
      * from each other, create a dummy node to split the edge. The resulting layering
      * is <i>proper</i>, i.e. all edges connect only nodes from subsequent layers.
      * 
-     * TODO create additional dummy nodes for feedback edges
+     * TODO create additional dummy nodes for feedback edges (in another module)
+     * TODO move this out into its own module
      */
     public void splitEdges() {
         ListIterator<Layer> layerIter = layers.listIterator();
@@ -121,6 +135,8 @@ public class LayeredGraph extends LGraphElement {
     
     /**
      * Arrange the ports of all nodes in the layered graph.
+     * 
+     * TODO move this out into its own module?
      */
     public void arrangePorts() {
         for (Layer layer : layers) {
