10bf70cde89a24c2f1af45c2fa254ba9ea5646cc
Miro Spoenemann <msp@informatik.uni-kiel.de>
2011-03-09 11:47:42 +0000

    added KEG importer / exporter and OGDF server to build system

--- plugins/net.ogdf.bin/src/net/ogdf/bin/OgdfPlugin.java
+++ plugins/net.ogdf.bin/src/net/ogdf/bin/OgdfPlugin.java
@@ -9,7 +9,6 @@
  *     + Real-Time and Embedded Systems Group
  * 
  * This code is provided under the terms of the Eclipse Public License (EPL).
- * See the file epl-v10.html for the license text.
  */
 package net.ogdf.bin;
 
