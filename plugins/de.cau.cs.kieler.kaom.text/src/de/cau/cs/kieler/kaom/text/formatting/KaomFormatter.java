cc52c49d8162f81ca754c94bfa3bf003f74d6821
Christian Schneider <chsch@informatik.uni-kiel.de>
2011-04-07 17:45:23 +0000

    Kaom formatter modularized

--- plugins/de.cau.cs.kieler.kaom.text/src/de/cau/cs/kieler/kaom/text/formatting/KaomFormatter.java
+++ plugins/de.cau.cs.kieler.kaom.text/src/de/cau/cs/kieler/kaom/text/formatting/KaomFormatter.java
@@ -3,10 +3,12 @@
  */
 package de.cau.cs.kieler.kaom.text.formatting;
 
-import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
 import org.eclipse.xtext.Keyword;
 import org.eclipse.xtext.util.Pair;
+
+import de.cau.cs.kieler.core.annotations.formatting.AnnotationsFormatter;
+import de.cau.cs.kieler.kaom.text.services.KaomGrammarAccess;
 
 /**
  * This class contains custom formatting description.
@@ -16,11 +18,26 @@ import org.eclipse.xtext.util.Pair;
  * 
  * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
-public class KaomFormatter extends AbstractDeclarativeFormatter {
+public class KaomFormatter extends AnnotationsFormatter {
 	
-	@Override
-	protected void configureFormatting(FormattingConfig c) {
-		de.cau.cs.kieler.kaom.text.services.KaomGrammarAccess f = (de.cau.cs.kieler.kaom.text.services.KaomGrammarAccess) getGrammarAccess();
+    @Override
+    protected void configureFormatting(FormattingConfig c) {
+        KaomGrammarAccess f = (KaomGrammarAccess) getGrammarAccess();
+        customConfigureFormatting(c, f);
+    }
+
+    /**
+     * Method contains actual formatting instructions while GrammarAccess class maybe parameterized
+     * allowing the reuse within other Formatters.
+     * 
+     * @param c
+     *            FormattingConfig provided by caller
+     * @param f
+     *            GrammarAccess provided by caller
+     */
+    protected void customConfigureFormatting(FormattingConfig c, KaomGrammarAccess f) {
+        super.customConfigureFormatting(c, f.getAnnotationsGrammarAccess());
+
 		for(Pair<Keyword, Keyword> pair: f.findKeywordPairs("{", "}")) {
 			c.setIndentation(pair.getFirst(), pair.getSecond());
 			c.setLinewrap(1).after(pair.getFirst());
@@ -32,8 +49,8 @@ public class KaomFormatter extends AbstractDeclarativeFormatter {
 			c.setNoSpace().before(comma);
 			c.setLinewrap().after(comma);
 		}
-		c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
-		c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
-		c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
+//		c.setLinewrap(0, 1, 2).before(f.getSL_COMMENTRule());
+//		c.setLinewrap(0, 1, 2).before(f.getML_COMMENTRule());
+//		c.setLinewrap(0, 1, 1).after(f.getML_COMMENTRule());
 	}
 }
