1b189a5dd8c3b53c01789204dd8d5f0fd19679b7
Christian Schneider <chsch@informatik.uni-kiel.de>
2011-04-08 14:38:31 +0000

    KaomValueConverter introduced and registered

---
+++ plugins/de.cau.cs.kieler.kaom.text/src/de/cau/cs/kieler/kaom/text/formatting/KaomValueConverter.java
@@ -0,0 +1,27 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2010 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.kaom.text.formatting;
+
+import de.cau.cs.kieler.core.annotations.formatting.AnnotationsValueConverter;
+
+/**
+ * Customized {@link IValueConverter}.
+ * Realizes the mapping of special terminal symbols to model content.
+ * 
+ * @author chsch
+ */
+
+public class KaomValueConverter extends AnnotationsValueConverter {
+
+}
