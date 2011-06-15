90239a8e4fe4b17d408a046e0ea1e2435911e77b
Christian Schneider <chsch@informatik.uni-kiel.de>
2011-06-15 11:22:15 +0000

    comments added to java stuff in the klighd.xtext bundle

--- plugins/de.cau.cs.kieler.klighd.xtext/src/de/cau/cs/kieler/klighd/xtext/UpdateXtextModelKLighDCombination.java
+++ plugins/de.cau.cs.kieler.klighd.xtext/src/de/cau/cs/kieler/klighd/xtext/UpdateXtextModelKLighDCombination.java
@@ -1,20 +1,37 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse Rich Client
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.klighd.xtext;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.ui.statushandlers.StatusManager;
-
 import de.cau.cs.kieler.core.kivi.AbstractCombination;
+import de.cau.cs.kieler.core.kivi.ITriggerState;
 import de.cau.cs.kieler.core.model.xtext.XtextBasedEditorActivationChangeTrigger.XtextModelChangeState;
 import de.cau.cs.kieler.klighd.effects.KlighdDiagramEffect;
 
+/**
+ * A combination for initializing/refreshing of KLighD views of Xtext-based models.
+ * 
+ * @author chsch
+ */
 public class UpdateXtextModelKLighDCombination extends AbstractCombination {
     
+    
+    /**
+     * The execute method revealed and invoked by KIVi.
+     * 
+     * @param state The {@link ITriggerState} for denoting the related EMF resource 
+     */
     public void execute(XtextModelChangeState state) {
-        StatusManager.getManager().addLoggedStatus(
-                new Status(IStatus.INFO, KLighDXtextPlugin.PLUGIN_ID, state.getResource().getURI()
-                        .toPlatformString(false)));
-        System.out.println(state.getResource().getURI().toPlatformString(false));
         this.schedule(new KlighdDiagramEffect(state.getResource().getURI().toPlatformString(false), state
                 .getResource().getContents().get(0)));
     }
