34a152e74f00f0d1b926c88937dc778fefde9e53
Insa Marie-Ann Fuhrmann <ima@informatik.uni-kiel.de>
2011-06-30 12:49:30 +0000

    Introduced EdgeType similar to NodeType

---
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/properties/EdgeType.java
@@ -0,0 +1,26 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2011 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klay.layered.properties;
+
+/**
+ * Definition of edge types used in the layered approach.
+ * 
+ * @author ima
+ */
+public enum EdgeType {
+    /** a normal edge is created from an edge of the original graph. */
+    NORMAL,
+    /** a dummy edge created for the layering phase of compound graphs. */
+    COMPOUND_DUMMY
+}
