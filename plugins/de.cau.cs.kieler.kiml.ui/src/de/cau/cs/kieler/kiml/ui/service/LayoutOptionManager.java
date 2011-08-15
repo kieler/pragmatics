dcc96813dce3b471b67fd2873c291b3ed5019d08
Martin Riess <mri@informatik.uni-kiel.de>
2011-08-15 07:56:28 +0000

    Added property to disable the usage of the eclipse layout config to the layout option manager.

--- plugins/de.cau.cs.kieler.kiml.ui/src/de/cau/cs/kieler/kiml/ui/service/LayoutOptionManager.java
+++ plugins/de.cau.cs.kieler.kiml.ui/src/de/cau/cs/kieler/kiml/ui/service/LayoutOptionManager.java
@@ -29,6 +29,7 @@ import de.cau.cs.kieler.core.kgraph.KGraphElement;
 import de.cau.cs.kieler.core.kgraph.KLabel;
 import de.cau.cs.kieler.core.kgraph.KNode;
 import de.cau.cs.kieler.core.kgraph.KPort;
+import de.cau.cs.kieler.core.properties.Property;
 import de.cau.cs.kieler.kiml.LayoutContext;
 import de.cau.cs.kieler.kiml.config.CompoundLayoutConfig;
 import de.cau.cs.kieler.kiml.config.DefaultLayoutConfig;
@@ -40,40 +41,49 @@ import de.cau.cs.kieler.kiml.ui.diagram.LayoutMapping;
 
 /**
  * The main class for configuration of KGraph instances.
- *
+ * 
  * @author msp
  */
 public class LayoutOptionManager {
+
+    /** the option for the usage of the Eclipse layout config. */
+    public static final Property<Boolean> USE_ECLIPSE_LAYOUT_CONFIG = new Property<Boolean>(
+            "layoutOptionManager.useEclipseLayoutConfig", true);
     
     /** internal cache of semantic layout configurations. */
     private Map<EClass, List<ILayoutConfig>> semanticConfigMap = Maps.newHashMap();
-    
+
     /**
      * Configure the layout graph in the given layout mapping.
      * 
-     * @param layoutMapping a layout mapping
+     * @param layoutMapping
+     *            a layout mapping
      */
     public void configure(final LayoutMapping<?> layoutMapping) {
         // create basic layout configuration
         CompoundLayoutConfig clc = new CompoundLayoutConfig();
         clc.add(new DefaultLayoutConfig());
-        clc.add(new EclipseLayoutConfig());
+        if (layoutMapping.getProperty(USE_ECLIPSE_LAYOUT_CONFIG)) {
+            clc.add(new EclipseLayoutConfig());
+        }
         clc.addAll(layoutMapping.getLayoutConfigs());
-        
+
         // configure the layout graph recursively
         KNode layoutGraph = layoutMapping.getLayoutGraph();
         recursiveConf(layoutGraph, layoutMapping, clc);
     }
-    
+
     /**
      * Create a layout configuration that can be used to access all actual values of layout options.
      * 
-     * @param domainElement a domain model element
-     * @param extraConfigs optional additional layout configurations to include
+     * @param domainElement
+     *            a domain model element
+     * @param extraConfigs
+     *            optional additional layout configurations to include
      * @return a complete layout configuration
      */
     public IMutableLayoutConfig createConfig(final EObject domainElement,
-            final ILayoutConfig ... extraConfigs) {
+            final ILayoutConfig... extraConfigs) {
         CompoundLayoutConfig clc = new CompoundLayoutConfig();
         clc.add(new DefaultLayoutConfig());
         clc.add(new EclipseLayoutConfig());
@@ -83,22 +93,25 @@ public class LayoutOptionManager {
         }
         return clc;
     }
-    
+
     /**
      * Configure all elements contained in the given node.
      * 
-     * @param node a node from the layout graph
-     * @param layoutMapping a layout mapping
-     * @param config the basic layout configuration
+     * @param node
+     *            a node from the layout graph
+     * @param layoutMapping
+     *            a layout mapping
+     * @param config
+     *            the basic layout configuration
      */
     private void recursiveConf(final KNode node, final LayoutMapping<?> layoutMapping,
-            final CompoundLayoutConfig config) {        
+            final CompoundLayoutConfig config) {
         // configure the node and its label
         configure(node, layoutMapping, config);
         if (node.getLabel() != null) {
             configure(node.getLabel(), layoutMapping, config);
         }
-        
+
         // configure ports
         for (KPort port : node.getPorts()) {
             configure(port, layoutMapping, config);
@@ -106,7 +119,7 @@ public class LayoutOptionManager {
                 configure(port.getLabel(), layoutMapping, config);
             }
         }
-        
+
         // configure outgoing edges
         for (KEdge edge : node.getOutgoingEdges()) {
             configure(edge, layoutMapping, config);
@@ -114,34 +127,38 @@ public class LayoutOptionManager {
                 configure(label, layoutMapping, config);
             }
         }
-        
+
         // configure child nodes
         for (KNode child : node.getChildren()) {
             recursiveConf(child, layoutMapping, config);
         }
     }
-    
+
     /**
      * Configure a graph element.
      * 
-     * @param graphElement a graph element
-     * @param layoutMapping a layout mapping
-     * @param config the basic layout configuration
+     * @param graphElement
+     *            a graph element
+     * @param layoutMapping
+     *            a layout mapping
+     * @param config
+     *            the basic layout configuration
      */
     private void configure(final KGraphElement graphElement, final LayoutMapping<?> layoutMapping,
             final CompoundLayoutConfig config) {
         // create a layout context for the current graph element
         LayoutContext context = new LayoutContext();
         context.setProperty(LayoutContext.GRAPH_ELEM, graphElement);
-        context.setProperty(LayoutContext.DIAGRAM_PART, layoutMapping.getGraphMap().get(graphElement));
+        context.setProperty(LayoutContext.DIAGRAM_PART,
+                layoutMapping.getGraphMap().get(graphElement));
         IWorkbenchPart workbenchPart = layoutMapping.getProperty(IWorkbenchPart.class);
         if (workbenchPart != null) {
             context.setProperty(EclipseLayoutConfig.WORKBENCH_PART, workbenchPart);
         }
-        
+
         // enrich the layout context using the basic configuration
         config.enrich(context);
-        
+
         // add semantic configurations from the extension point
         EObject modelElement = context.getProperty(LayoutContext.DOMAIN_MODEL);
         List<ILayoutConfig> semanticConfigs = getSemanticConfigs(modelElement);
@@ -149,21 +166,22 @@ public class LayoutOptionManager {
             sc.enrich(context);
             config.add(sc);
         }
-        
+
         // transfer the options from the layout configuration
-        KGraphData graphData = graphElement.getData(graphElement instanceof KEdge
-                ? KEdgeLayout.class : KShapeLayout.class);
+        KGraphData graphData = graphElement
+                .getData(graphElement instanceof KEdge ? KEdgeLayout.class : KShapeLayout.class);
         config.transferValues(graphData, context);
-        
+
         // remove the semantic layout configurations again
         config.removeAll(semanticConfigs);
     }
-    
+
     /**
      * Get a list of semantic layout configurations for the given model element, using a cached
      * value if available.
      * 
-     * @param modelElement a domain model element
+     * @param modelElement
+     *            a domain model element
      * @return the list of semantic layout configurations
      */
     private List<ILayoutConfig> getSemanticConfigs(final EObject modelElement) {
