5835cb40c1f88c9f911eb23f92c43fda0bb5e192
Stephan Wersig <swe@informatik.uni-kiel.de>
2011-10-04 07:09:56 +0000

    Update on KWebS start script

--- plugins/de.cau.cs.kieler.kwebs.server/kwebs_start.sh
+++ plugins/de.cau.cs.kieler.kwebs.server/kwebs_start.sh
@@ -13,13 +13,37 @@
 
 # Start the server ignoring the HUP signal so the server
 # keeps running after closing a terminal connection
+echo Starting server...
 nohup ./kwebs >kwebs.out 2>kwebs.err </dev/null &
 
+# Remember process id
 PID=$!
 
-# Store process id in file
-echo KWebS server started, process id is $PID
-echo $PID > kwebs.pid
-
 # Wait some time just to be sure...
-sleep 5
+echo Waiting for server to finish bootup process...
+sleep 10
+
+# Test if server is running
+#if [ ps -A | awk '{print $1}' | grep -q $PID 2>/dev/null ]; then
+if [ -f /proc/$PID/exe ]; then
+
+  # If so, store process id in file
+  echo KWebS server started, process id is $PID
+  echo $PID > kwebs.pid
+
+else
+
+  # If not, exit with an error code
+  echo KWebS server failed to start, see kwebs.out and kwebs.err for details
+  echo
+
+  echo kwebs.out:
+  cat kwebs.out
+
+  echo
+  echo kwebs.err:
+  cat kwebs.err
+
+  exit 1
+
+fi
