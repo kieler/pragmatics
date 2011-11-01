d43f2d3eb72333aedadb749e162907de07af031d
Miro Spoenemann <msp@informatik.uni-kiel.de>
2011-11-01 18:48:10 +0000

    using new Xtext serializer

--- plugins/de.cau.cs.kieler.kiml.formats.gml/src-gen/de/cau/cs/kieler/kiml/formats/gml/AbstractGMLRuntimeModule.java
+++ plugins/de.cau.cs.kieler.kiml.formats.gml/src-gen/de/cau/cs/kieler/kiml/formats/gml/AbstractGMLRuntimeModule.java
@@ -39,9 +39,19 @@ public abstract class AbstractGMLRuntimeModule extends DefaultRuntimeModule {
 		return de.cau.cs.kieler.kiml.formats.gml.services.GMLGrammarAccess.class;
 	}
 
-	// contributed by org.eclipse.xtext.generator.parseTreeConstructor.ParseTreeConstructorFragment
-	public Class<? extends org.eclipse.xtext.parsetree.reconstr.IParseTreeConstructor> bindIParseTreeConstructor() {
-		return de.cau.cs.kieler.kiml.formats.gml.parseTreeConstruction.GMLParsetreeConstructor.class;
+	// contributed by org.eclipse.xtext.generator.serializer.SerializerFragment
+	public Class<? extends org.eclipse.xtext.serializer.sequencer.ISemanticSequencer> bindISemanticSequencer() {
+		return de.cau.cs.kieler.kiml.formats.gml.serializer.GMLSemanticSequencer.class;
+	}
+
+	// contributed by org.eclipse.xtext.generator.serializer.SerializerFragment
+	public Class<? extends org.eclipse.xtext.serializer.sequencer.ISyntacticSequencer> bindISyntacticSequencer() {
+		return de.cau.cs.kieler.kiml.formats.gml.serializer.GMLSyntacticSequencer.class;
+	}
+
+	// contributed by org.eclipse.xtext.generator.serializer.SerializerFragment
+	public Class<? extends org.eclipse.xtext.serializer.ISerializer> bindISerializer() {
+		return org.eclipse.xtext.serializer.impl.Serializer.class;
 	}
 
 	// contributed by org.eclipse.xtext.generator.parser.antlr.XtextAntlrGeneratorFragment
---
+++ plugins/de.cau.cs.kieler.kiml.formats.gml/src-gen/de/cau/cs/kieler/kiml/formats/gml/serializer/AbstractGMLSemanticSequencer.java
@@ -0,0 +1,88 @@
+package de.cau.cs.kieler.kiml.formats.gml.serializer;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import de.cau.cs.kieler.kiml.formats.gml.gml.Element;
+import de.cau.cs.kieler.kiml.formats.gml.gml.GmlModel;
+import de.cau.cs.kieler.kiml.formats.gml.gml.GmlPackage;
+import de.cau.cs.kieler.kiml.formats.gml.services.GMLGrammarAccess;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.serializer.acceptor.ISemanticSequenceAcceptor;
+import org.eclipse.xtext.serializer.diagnostic.ISemanticSequencerDiagnosticProvider;
+import org.eclipse.xtext.serializer.diagnostic.ISerializationDiagnostic.Acceptor;
+import org.eclipse.xtext.serializer.sequencer.AbstractSemanticSequencer;
+import org.eclipse.xtext.serializer.sequencer.GenericSequencer;
+import org.eclipse.xtext.serializer.sequencer.ISemanticSequencer;
+import org.eclipse.xtext.serializer.sequencer.ITransientValueService;
+
+@SuppressWarnings("restriction")
+public class AbstractGMLSemanticSequencer extends AbstractSemanticSequencer {
+
+	@Inject
+	protected GMLGrammarAccess grammarAccess;
+	
+	@Inject
+	protected ISemanticSequencerDiagnosticProvider diagnosticProvider;
+	
+	@Inject
+	protected ITransientValueService transientValues;
+	
+	@Inject
+	@GenericSequencer
+	protected Provider<ISemanticSequencer> genericSequencerProvider;
+	
+	protected ISemanticSequencer genericSequencer;
+	
+	
+	@Override
+	public void init(ISemanticSequencer sequencer, ISemanticSequenceAcceptor sequenceAcceptor, Acceptor errorAcceptor) {
+		super.init(sequencer, sequenceAcceptor, errorAcceptor);
+		this.genericSequencer = genericSequencerProvider.get();
+		this.genericSequencer.init(sequencer, sequenceAcceptor, errorAcceptor);
+	}
+	
+	public void createSequence(EObject context, EObject semanticObject) {
+		if(semanticObject.eClass().getEPackage() == GmlPackage.eINSTANCE) switch(semanticObject.eClass().getClassifierID()) {
+			case GmlPackage.ELEMENT:
+				if(context == grammarAccess.getElementRule()) {
+					sequence_Element(context, (Element) semanticObject); 
+					return; 
+				}
+				else break;
+			case GmlPackage.GML_MODEL:
+				if(context == grammarAccess.getGmlModelRule()) {
+					sequence_GmlModel(context, (GmlModel) semanticObject); 
+					return; 
+				}
+				else break;
+			}
+		if (errorAcceptor != null) errorAcceptor.accept(diagnosticProvider.createInvalidContextOrTypeDiagnostic(semanticObject, context));
+	}
+	
+	/**
+	 * Constraint:
+	 *     (key=ID (value=Value | elements+=Element*))
+	 *
+	 * Features:
+	 *    key[1, 1]
+	 *    value[0, 1]
+	 *         EXCLUDE_IF_SET elements
+	 *    elements[0, *]
+	 *         EXCLUDE_IF_SET value
+	 */
+	protected void sequence_Element(EObject context, Element semanticObject) {
+		genericSequencer.createSequence(context, semanticObject);
+	}
+	
+	
+	/**
+	 * Constraint:
+	 *     elements+=Element*
+	 *
+	 * Features:
+	 *    elements[0, *]
+	 */
+	protected void sequence_GmlModel(EObject context, GmlModel semanticObject) {
+		genericSequencer.createSequence(context, semanticObject);
+	}
+}
