c669bc5faf21368a9961ac1cb2eb35fd7be66c5f
Christoph Krueger <ckru@informatik.uni-kiel.de>
2011-12-13 11:33:47 +0000

    ksbase transformations for ptolemy

---
+++ plugins/de.cau.cs.kieler.kaom.ksbase.ptolemy/src/de/cau/cs/kieler/kaom/ksbase/ptolemy/xtend2Transformations/Xtend2test.java
@@ -0,0 +1,18 @@
+package de.cau.cs.kieler.kaom.ksbase.ptolemy.xtend2Transformations;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+
+import de.cau.cs.kieler.kaom.Entity;
+import de.cau.cs.kieler.kaom.Link;
+import de.cau.cs.kieler.kaom.impl.EntityImpl;
+
+public class Xtend2test {
+
+    public void test(List<Entity> obj, List<Link> ln) {
+        int i = 42;
+        i++;
+    }
+    
+}
