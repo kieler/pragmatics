6a7c3ef480c37daa23ddf1d51e2bfd5a5939fb14
Wahbi <wah@informatik.uni-kiel.de>
2012-03-07 17:27:02 +0100

    JUnit tests for de.cau.cs.kieler.core.math

--- test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/test/MathTest.java
+++ test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/test/MathTest.java
@@ -7,15 +7,44 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import de.cau.cs.kieler.core.math.KielerMath;
+
 /**
  * @author wah
- *
+ * 
  */
 public class MathTest {
 
-	@Test
-	public void testFact() {
-		fail("Not yet implemented");
-	}
+    KielerMath kielerMath;
+
+    /**
+     * Tests some valid combinations of Factl from kielerMath class.
+     */
+    @SuppressWarnings("static-access")
+    @Test
+    public void testFactl() {
+        assertEquals(kielerMath.factl(0), 1);
+        assertEquals(kielerMath.factl(1), 1);
+        assertEquals(kielerMath.factl(20), 2432902008176640000L);
+
+    }
+
+    /**
+     * Tests some the Little argument exception of Factl from kielerMath class.
+     */
+    @SuppressWarnings("static-access")
+    @Test(expected = IllegalArgumentException.class)
+    public void testFactlLittleIllegalArgumentException() {
+        assertEquals(kielerMath.factl(-50), 1);
+    }
+
+    /**
+     * Tests some the Big argument exception of Factl from kielerMath class.
+     */
+    @SuppressWarnings("static-access")
+    @Test(expected = IllegalArgumentException.class)
+    public void testFactlBigIllegalArgumentException() {
+        assertEquals(kielerMath.factl(21), 1);
+    }
 
 }
