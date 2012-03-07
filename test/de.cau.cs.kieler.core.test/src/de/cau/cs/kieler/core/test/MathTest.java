9282ded17dbfd652ed478932be8fe42eb2393540
Wahbi <wah@informatik.uni-kiel.de>
2012-03-07 15:44:33 +0100

    Created Kieler Core Test Project

---
+++ test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/test/MathTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.cau.cs.kieler.core.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author wah
+ *
+ */
+public class MathTest {
+
+	@Test
+	public void testFact() {
+		fail("Not yet implemented");
+	}
+
+}
