98183bee26386dbef0839f02156cd6e9d688dc71
Wahbi <wah@informatik.uni-kiel.de>
2012-03-12 16:25:07 +0100

    -added: JUnit tests for functions pow and calcBezierPoints

--- test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/math/test/MathTest.java
+++ test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/math/test/MathTest.java
@@ -17,6 +17,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import de.cau.cs.kieler.core.math.KVector;
 import de.cau.cs.kieler.core.math.KielerMath;
 
 /**
@@ -102,6 +103,7 @@ public class MathTest {
     @Test
     public void testBinomiall() {
         assertEquals(kielerMath.binomiall(2,0), 1);
+        assertEquals(kielerMath.binomiall(20,20), 1);
         assertEquals(kielerMath.binomiall(2,1), 2);
     }
 
@@ -114,5 +116,75 @@ public class MathTest {
         assertEquals(kielerMath.binomiall(2,5),0);
     }
     
+    /**
+    *
+    * Tests some valid combinations 
+    * of binomiald from kielerMath class.
+    *
+    */
+    
+    /** Tests some valid combinations of binomiald from kielerMath class. */
+    @SuppressWarnings("static-access")
+    @Test
+    public void testBinomiald() {
+        assertEquals(kielerMath.binomiald(2,0), 1,0);
+        assertEquals(kielerMath.binomiald(20,20), 1,0);
+        assertEquals(kielerMath.binomiald(2,1), 2,0);
+    }
+
+    /** Tests the Little argument exception of binomiald from kielerMath class. */
+    @SuppressWarnings("static-access")
+    @Test(expected = IllegalArgumentException.class)
+    public void testBinomialdLittleIllegalArgumentException() {
+        assertEquals(kielerMath.binomiald(-1,1),0,0);
+        assertEquals(kielerMath.binomiald(1,-1),0,0);
+        assertEquals(kielerMath.binomiald(2,5),0,0);
+    }
+    
+    /**
+    *
+    * Tests some valid combinations 
+    * of pow from kielerMath class.
+    *
+    */
+    @SuppressWarnings("static-access")
+    @Test
+    public void testPow() {
+        assertEquals(kielerMath.pow(20,0), 1,0);
+        assertEquals(kielerMath.pow(10,2), 100,0);
+        
+    }
+    
+    /**
+    *
+    * Tests some valid combinations 
+    * of CalcBezierPoints from kielerMath class.
+    *
+    */
+    @SuppressWarnings("static-access")
+    @Test
+    public void testCalcBezierPoints() {
+        /** some KVectors */
+        KVector kvector1 = new KVector(10,10);
+        KVector kvector2 = new KVector(20,20);
+        KVector kvector3 = new KVector(30,30);
+        KVector kvector4 = new KVector(50,50);
+        /** test if the last KVector  of the result similar to kvector4*/
+        KVector[] result = kielerMath.calcBezierPoints(20,kvector1,kvector2,kvector3,kvector4);
+        assertEquals(result[result.length-1].x, kvector4.x,0.000000001);
+        assertEquals(result[result.length-1].y, kvector4.y,0.000000001);
+        
+        /** some KVectors with y=10 */
+        kvector1 = new KVector(50,10);
+        kvector2 = new KVector(70,10);
+        kvector3 = new KVector(80,10);
+        kvector4 = new KVector(100,10);
+        /** test if the all result-KVectors with y=10*/
+        result = kielerMath.calcBezierPoints(20,kvector1,kvector2,kvector3,kvector4);
+        for(KVector k:result){
+            assertEquals(k.y, 10,0.000000001);
+        }
+        
+    }
     
 }
