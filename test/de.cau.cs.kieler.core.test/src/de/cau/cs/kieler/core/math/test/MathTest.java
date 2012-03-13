6321edec6b6d6834a9a41770028c5be425751ed1
Wahbi <wah@informatik.uni-kiel.de>
2012-03-13 13:33:34 +0100

    -added: JUnit tests for functions mailintestAppoximateSpline and testDistanceFromSpline

--- test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/math/test/MathTest.java
+++ test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/math/test/MathTest.java
@@ -18,10 +18,12 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import de.cau.cs.kieler.core.math.KVector;
+import de.cau.cs.kieler.core.math.KVectorChain;
 import de.cau.cs.kieler.core.math.KielerMath;
 
 /**
- * Mathematics utility class for the KIELER projects.
+ * Junit test-class for de.cau.cs.kieler.core.math: Mathematics utility class for the KIELER
+ * projects.
  * 
  * @author wah
  */
@@ -29,12 +31,10 @@ public class MathTest {
 
     KielerMath kielerMath;
 
-    
     /**
-     *
-     * Tests some valid combinations 
-     * of Factl from kielerMath class.
-     *
+     * 
+     * Tests some valid combinations of Factl from kielerMath class.
+     * 
      */
 
     /** Tests some valid combinations of Factl from kielerMath class. */
@@ -51,24 +51,22 @@ public class MathTest {
     @SuppressWarnings("static-access")
     @Test(expected = IllegalArgumentException.class)
     public void testFactlLittleIllegalArgumentException() {
-        assertEquals(kielerMath.factl(-50), 1);
+        kielerMath.factl(-50);
     }
 
     /** Tests the Big argument exception of Factl from kielerMath class. */
     @SuppressWarnings("static-access")
     @Test(expected = IllegalArgumentException.class)
     public void testFactlBigIllegalArgumentException() {
-        assertEquals(kielerMath.factl(21), 1);
+        kielerMath.factl(21);
     }
 
-    
     /**
-    *
-    * Tests some valid combinations 
-    * of Factd from kielerMath class.
-    *
-    */
-    
+     * 
+     * Tests some valid combinations of Factd from kielerMath class.
+     * 
+     */
+
     /** Tests some valid combinations of Factd from kielerMath class. */
     @SuppressWarnings("static-access")
     @Test
@@ -81,110 +79,178 @@ public class MathTest {
     @SuppressWarnings("static-access")
     @Test(expected = IllegalArgumentException.class)
     public void testFacdlLittleIllegalArgumentException() {
-        assertEquals(kielerMath.factd(-1), 1, 0);
+        kielerMath.factd(-1);
     }
-    
+
     /** Tests the Big argument exception of Factd from kielerMath class. */
     @SuppressWarnings("static-access")
     @Test(expected = IllegalArgumentException.class)
     public void testFactdBigIllegalArgumentException() {
-        assertEquals(kielerMath.factd(27), 1,0);
+        kielerMath.factd(27);
     }
-    
+
     /**
-    *
-    * Tests some valid combinations 
-    * of binomiall from kielerMath class.
-    *
-    */
-    
+     * 
+     * Tests some valid combinations of binomiall from kielerMath class.
+     * 
+     */
+
     /** Tests some valid combinations of binomiall from kielerMath class. */
     @SuppressWarnings("static-access")
     @Test
     public void testBinomiall() {
-        assertEquals(kielerMath.binomiall(2,0), 1);
-        assertEquals(kielerMath.binomiall(20,20), 1);
-        assertEquals(kielerMath.binomiall(2,1), 2);
+        assertEquals(kielerMath.binomiall(2, 0), 1);
+        assertEquals(kielerMath.binomiall(20, 20), 1);
+        assertEquals(kielerMath.binomiall(2, 1), 2);
     }
 
     /** Tests the Little argument exception of binomiall from kielerMath class. */
     @SuppressWarnings("static-access")
     @Test(expected = IllegalArgumentException.class)
     public void testBinomialllLittleIllegalArgumentException() {
-        assertEquals(kielerMath.binomiall(-1,1),0);
-        assertEquals(kielerMath.binomiall(1,-1),0);
-        assertEquals(kielerMath.binomiall(2,5),0);
+        kielerMath.binomiall(-1, 1);
     }
-    
+
     /**
-    *
-    * Tests some valid combinations 
-    * of binomiald from kielerMath class.
-    *
-    */
-    
+     * 
+     * Tests some valid combinations of binomiald from kielerMath class.
+     * 
+     */
+
     /** Tests some valid combinations of binomiald from kielerMath class. */
     @SuppressWarnings("static-access")
     @Test
     public void testBinomiald() {
-        assertEquals(kielerMath.binomiald(2,0), 1,0);
-        assertEquals(kielerMath.binomiald(20,20), 1,0);
-        assertEquals(kielerMath.binomiald(2,1), 2,0);
+        assertEquals(kielerMath.binomiald(2, 0), 1, 0);
+        assertEquals(kielerMath.binomiald(20, 20), 1, 0);
+        assertEquals(kielerMath.binomiald(2, 1), 2, 0);
     }
 
     /** Tests the Little argument exception of binomiald from kielerMath class. */
     @SuppressWarnings("static-access")
     @Test(expected = IllegalArgumentException.class)
     public void testBinomialdLittleIllegalArgumentException() {
-        assertEquals(kielerMath.binomiald(-1,1),0,0);
-        assertEquals(kielerMath.binomiald(1,-1),0,0);
-        assertEquals(kielerMath.binomiald(2,5),0,0);
+        kielerMath.binomiald(-1, 1);
     }
-    
+
     /**
-    *
-    * Tests some valid combinations 
-    * of pow from kielerMath class.
-    *
-    */
+     * 
+     * Tests some valid combinations of pow from kielerMath class.
+     * 
+     */
     @SuppressWarnings("static-access")
     @Test
     public void testPow() {
-        assertEquals(kielerMath.pow(20,0), 1,0);
-        assertEquals(kielerMath.pow(10,2), 100,0);
-        
+        double ad = 10;
+        float af = 10;
+        assertEquals(kielerMath.pow(ad, 0), 1, 0);
+        assertEquals(kielerMath.pow(af, 0), 1, 0);
+        assertEquals(kielerMath.pow(ad, 2), 100, 0);
+        assertEquals(kielerMath.pow(af, 2), 100, 0);
+
     }
-    
+
     /**
-    *
-    * Tests some valid combinations 
-    * of CalcBezierPoints from kielerMath class.
-    *
-    */
+     * 
+     * Tests some valid combinations of CalcBezierPoints from kielerMath class.
+     * 
+     */
     @SuppressWarnings("static-access")
     @Test
     public void testCalcBezierPoints() {
         /** some KVectors */
-        KVector kvector1 = new KVector(10,10);
-        KVector kvector2 = new KVector(20,20);
-        KVector kvector3 = new KVector(30,30);
-        KVector kvector4 = new KVector(50,50);
-        /** test if the last KVector  of the result similar to kvector4*/
-        KVector[] result = kielerMath.calcBezierPoints(20,kvector1,kvector2,kvector3,kvector4);
-        assertEquals(result[result.length-1].x, kvector4.x,0.000000001);
-        assertEquals(result[result.length-1].y, kvector4.y,0.000000001);
-        
+        KVector kvector1 = new KVector(10, 10);
+        KVector kvector2 = new KVector(20, 20);
+        KVector kvector3 = new KVector(30, 30);
+        KVector kvector4 = new KVector(50, 50);
+        /** test if the last KVector of the result similar to kvector4 */
+        KVector[] result = kielerMath.calcBezierPoints(20, kvector1, kvector2, kvector3, kvector4);
+        assertEquals(result[result.length - 1].x, kvector4.x, 0.000000001);
+        assertEquals(result[result.length - 1].y, kvector4.y, 0.000000001);
+
         /** some KVectors with y=10 */
-        kvector1 = new KVector(50,10);
-        kvector2 = new KVector(70,10);
-        kvector3 = new KVector(80,10);
-        kvector4 = new KVector(100,10);
-        /** test if the all result-KVectors with y=10*/
-        result = kielerMath.calcBezierPoints(20,kvector1,kvector2,kvector3,kvector4);
-        for(KVector k:result){
-            assertEquals(k.y, 10,0.000000001);
+        kvector1 = new KVector(50, 10);
+        kvector2 = new KVector(70, 10);
+        kvector3 = new KVector(80, 10);
+        kvector4 = new KVector(100, 10);
+        /** test if the all result-KVectors with y=10 */
+        result = kielerMath.calcBezierPoints(20, kvector1, kvector2, kvector3, kvector4);
+        for (KVector k : result) {
+            assertEquals(k.y, 10, 0.000000001);
         }
-        
+
     }
     
+    /**
+     * 
+     * Tests some valid combinations of appoximateSpline from kielerMath class.
+     * 
+     * 
+     */
+    
+    @SuppressWarnings("static-access")
+    @Test
+    public void testAppoximateSpline() {
+        /** some KVectors */
+        KVector kvector1 = new KVector(10, 10);
+        KVector kvector2 = new KVector(20, 20);
+        KVector kvector3 = new KVector(30, 30);
+        KVector kvector4 = new KVector(50, 50);
+        /** test if the last KVector of the result similar to kvector4 */
+        KVector[] vectors = kielerMath.calcBezierPoints(20, kvector1, kvector2, kvector3, kvector4);
+        KVectorChain controlPoints = new KVectorChain(vectors);
+        KVectorChain result = KielerMath.appoximateSpline(controlPoints);
+        KVector k = result.get(result.size()-1);
+        assertEquals(k.x, kvector4.x, 0.000000001);
+        assertEquals(k.y, kvector4.y, 0.000000001);
+
+        /** some KVectors with y=10 */
+        kvector1 = new KVector(50, 10);
+        kvector2 = new KVector(70, 10);
+        kvector3 = new KVector(80, 10);
+        kvector4 = new KVector(100, 10);
+        /** test if the all result-KVectors with y=10 */
+        vectors = kielerMath.calcBezierPoints(20, kvector1, kvector2, kvector3, kvector4);
+        controlPoints = new KVectorChain(vectors);
+        result = KielerMath.appoximateSpline(controlPoints);
+        
+        for (KVector kv : result) {
+            assertEquals(kv.y, 10, 0.000000001);
+        }
+
+    }
+    
+    /**
+     * 
+     * Tests some valid combinations of distanceFromSpline from kielerMath class.
+     * 
+     * 
+     */
+    
+    @Test
+    public void testDistanceFromSpline() {
+        /** some KVectors */
+        KVector kvector1 = new KVector(10, 10);
+        KVector kvector2 = new KVector(20, 20);
+        KVector kvector3 = new KVector(30, 30);
+        KVector kvector4 = new KVector(50, 50);
+        /** test if the result is 0 when kvector4 = needle */
+        KVector needle = kvector4;
+        double result = KielerMath.distanceFromSpline(kvector1, kvector2, kvector3, kvector4, needle);
+        assertEquals(result, 0, 0.01);
+        /** test if the result is 0 when kvector3 = needle */
+        needle = kvector3;
+        result = KielerMath.distanceFromSpline(kvector1, kvector2, kvector3, kvector4, needle);
+        assertEquals(result, 0, 0.01);
+        /** test if the result is 0 when kvector2 = needle */
+        needle = kvector2;
+        result = KielerMath.distanceFromSpline(kvector1, kvector2, kvector3, kvector4, needle);
+        assertEquals(result, 0, 0.01);
+        /** test if the result is 0 when kvector1 = needle */
+        needle = kvector1;
+        result = KielerMath.distanceFromSpline(kvector1, kvector2, kvector3, kvector4, needle);
+        assertEquals(result, 0, 0.01);
+
+    }
+
 }
