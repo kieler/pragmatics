446540112177d27b4a15c06bf104618d2638c9cb
Wahbi <wah@informatik.uni-kiel.de>
2012-03-16 15:10:39 +0100

    -modified: JUnit test functions calculateClosedBezierSpline for Class CubicSplineInterpolator

--- test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/math/test/CubicSplineInterpolatorTest.java
+++ test/de.cau.cs.kieler.core.test/src/de/cau/cs/kieler/core/math/test/CubicSplineInterpolatorTest.java
@@ -37,31 +37,45 @@ public class CubicSplineInterpolatorTest {
      */
     @Test
     public void calculateClosedBezierSplineTest() {
-        
-        //test if the endpoint of the returned BezierSpline equal to the startpoint
+
+        // test if the endpoint of the returned BezierSpline equal to the startpoint
         KVector v0 = new KVector(5, 50);
         KVector v1 = new KVector(10, 70);
         KVector v2 = new KVector(30, 100);
         KVector v3 = new KVector(60, 70);
         KVector v4 = new KVector(70, 40);
-        KVector[] vectors = {v0,v1,v2,v3,v4};
+        KVector[] vectors = { v0, v1, v2, v3, v4 };
         BezierSpline b = new CubicSplineInterpolator().calculateClosedBezierSpline(vectors);
         assertTrue(v0.equals(b.getEndPoint()));
-        
-        //test horizontal aligned points
+
+        // test horizontal aligned points
         v0 = new KVector(5, 50);
         v1 = new KVector(10, 50);
         v2 = new KVector(30, 50);
         v3 = new KVector(60, 50);
         v4 = new KVector(70, 50);
-        KVector[] vectors_h = {v0,v1,v2,v3,v4};
+        KVector[] vectors_h = { v0, v1, v2, v3, v4 };
         b = new CubicSplineInterpolator().calculateClosedBezierSpline(vectors_h);
-        assertEquals(b.getStartPoint().y,50,0);
-        assertEquals(b.getEndPoint().y,50,0);
-        for(KVector v:b.getInnerPoints()){
-            assertEquals(v.y,50,0);
+        assertEquals(b.getStartPoint().y, 50, 0);
+        assertEquals(b.getEndPoint().y, 50, 0);
+        for (KVector v : b.getInnerPoints()) {
+            assertEquals(v.y, 50, 0);
         }
-        
+
+        // test vertical aligned points
+        v0 = new KVector(100, 10);
+        v1 = new KVector(100, 50);
+        v2 = new KVector(100, 100);
+        v3 = new KVector(100, 120);
+        v4 = new KVector(100, 200);
+        KVector[] vectors_v = { v0, v1, v2, v3, v4 };
+        b = new CubicSplineInterpolator().calculateClosedBezierSpline(vectors_v);
+        assertEquals(b.getStartPoint().x, 100, 0);
+        assertEquals(b.getEndPoint().x, 100, 0);
+        for (KVector v : b.getInnerPoints()) {
+            assertEquals(v.x, 100, 0);
+        }
+
     }
 
 }
