382ae7f0b3248e0449f7341663b5e9ceb688e013
Miro Spoenemann <msp@informatik.uni-kiel.de>
2012-08-03 11:20:52 +0200

    updated KWebS deployment scripts

--- plugins/de.cau.cs.kieler.kwebs.server/kwebs_restore.sh
+++ plugins/de.cau.cs.kieler.kwebs.server/kwebs_restore.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Restore script for the KWebS server
-# Version 0.00
+# Version 0.01
 #
 # Author swe
-# Last edit 10.08.2011
-
-# !!!!!
+# Last edit 03.08.2012
+#
 # Please remember making this script executable by
 # doing chmod u+x kwebs_restore.sh
+
+# !!!!!
+# This script will overwrite the configuration contained in the working directory
 # !!!!!
 
 KWEBS_BACKUP=../kwebs-backup
@@ -17,14 +19,14 @@ KWEBS_BACKUP=../kwebs-backup
 
 if [ ! -d $KWEBS_BACKUP ];
 then
-  echo Backup does not exist
+  echo "Backup does not exist in $KWEBS_BACKUP"
   exit 1
 fi
 
 # Restore user server configuration file
 if [ -f $KWEBS_BACKUP/kwebs.user ];
 then
-    mv $KWEBS_BACKUP/kwebs.user .
+    cp -v $KWEBS_BACKUP/kwebs.user ./
 fi
 
 SERVER_SSL_CONTENT=server/kwebs/security/keystores
@@ -34,12 +36,12 @@ WEB_SSL_CONTENT=server/kwebs/web/security
 if [ -d $KWEBS_BACKUP/$SERVER_SSL_CONTENT ];
 then
     mkdir -p $SERVER_SSL_CONTENT
-    mv $KWEBS_BACKUP/$SERVER_SSL_CONTENT/*.jks $SERVER_SSL_CONTENT
+    cp -v $KWEBS_BACKUP/$SERVER_SSL_CONTENT/*.jks $SERVER_SSL_CONTENT/
 fi
 
 # Restore user SSL configuration
 if [ -d $KWEBS_BACKUP/$WEB_SSL_CONTENT ];
 then
     mkdir -p $WEB_SSL_CONTENT
-    mv $KWEBS_BACKUP/$WEB_SSL_CONTENT/client.jks $WEB_SSL_CONTENT
+    cp -v $KWEBS_BACKUP/$WEB_SSL_CONTENT/client.jks $WEB_SSL_CONTENT/
 fi
