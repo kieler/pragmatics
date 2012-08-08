f1e954888e6ac8f5a05eed3175065d91783ec875
Miro Spoenemann <msp@informatik.uni-kiel.de>
2012-08-08 12:07:54 +0200

    fixed a bug with north/south ports with multiple edges

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/NorthSouthPortPostprocessor.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/NorthSouthPortPostprocessor.java
@@ -97,16 +97,16 @@ public class NorthSouthPortPostprocessor extends AbstractAlgorithm implements IL
         LPort originPort = (LPort) inputPort.getProperty(Properties.ORIGIN);
         
         // Calculate the bend point
-        KVector bendPoint = new KVector(inputPort.getNode().getPosition());
-        bendPoint.x = originPort.getNode().getPosition().x + originPort.getPosition().x
+        double x = originPort.getNode().getPosition().x + originPort.getPosition().x
                 + originPort.getAnchor().x;
+        double y = inputPort.getNode().getPosition().y;
         
-        // Reroute the edges, inserting a new bend point at the position of
-        // the dummy node
-        LEdge[] edgeArray = inputPort.getIncomingEdges().toArray(new LEdge[0]);
+        // Reroute the edges, inserting a new bend point at the position of the dummy node
+        LEdge[] edgeArray = inputPort.getIncomingEdges().toArray(
+                new LEdge[inputPort.getIncomingEdges().size()]);
         for (LEdge inEdge : edgeArray) {
             inEdge.setTarget(originPort);
-            inEdge.getBendPoints().add(bendPoint);
+            inEdge.getBendPoints().addLast(x, y);
         }
     }
     
@@ -121,16 +121,16 @@ public class NorthSouthPortPostprocessor extends AbstractAlgorithm implements IL
         LPort originPort = (LPort) outputPort.getProperty(Properties.ORIGIN);
         
         // Calculate the bend point
-        KVector bendPoint = new KVector(outputPort.getNode().getPosition());
-        bendPoint.x = originPort.getNode().getPosition().x + originPort.getPosition().x
+        double x = originPort.getNode().getPosition().x + originPort.getPosition().x
                 + originPort.getAnchor().x;
+        double y = outputPort.getNode().getPosition().y;
         
-        // Reroute the edges, inserting a new bend point at the position of
-        // the dummy node
-        LEdge[] edgeArray = outputPort.getOutgoingEdges().toArray(new LEdge[0]);
+        // Reroute the edges, inserting a new bend point at the position of the dummy node
+        LEdge[] edgeArray = outputPort.getOutgoingEdges().toArray(
+                new LEdge[outputPort.getOutgoingEdges().size()]);
         for (LEdge outEdge : edgeArray) {
             outEdge.setSource(originPort);
-            outEdge.getBendPoints().addFirst(bendPoint);
+            outEdge.getBendPoints().addFirst(x, y);
         }
     }
     
