b830cedc3c51cd9bb11765b12a2931077ebe4686
Miro Spoenemann <msp@informatik.uni-kiel.de>
2012-08-08 15:06:23 +0200

    improved polyline edge routing

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/p5edges/PolylineEdgeRouter.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/p5edges/PolylineEdgeRouter.java
@@ -14,7 +14,7 @@
 package de.cau.cs.kieler.klay.layered.p5edges;
 
 import de.cau.cs.kieler.core.alg.AbstractAlgorithm;
-import de.cau.cs.kieler.core.math.KVector;
+import de.cau.cs.kieler.core.math.KielerMath;
 import de.cau.cs.kieler.klay.layered.ILayoutPhase;
 import de.cau.cs.kieler.klay.layered.IntermediateProcessingConfiguration;
 import de.cau.cs.kieler.klay.layered.graph.LEdge;
@@ -23,6 +23,7 @@ import de.cau.cs.kieler.klay.layered.graph.LPort;
 import de.cau.cs.kieler.klay.layered.graph.Layer;
 import de.cau.cs.kieler.klay.layered.graph.LGraph;
 import de.cau.cs.kieler.klay.layered.properties.NodeType;
+import de.cau.cs.kieler.klay.layered.properties.PortType;
 import de.cau.cs.kieler.klay.layered.properties.Properties;
 
 /**
@@ -42,15 +43,16 @@ import de.cau.cs.kieler.klay.layered.properties.Properties;
  */
 public class PolylineEdgeRouter extends AbstractAlgorithm implements ILayoutPhase {
     
-    /** maximal number of edges for which the default node spacing is taken. */
-    private static final int MAX_EDGES = 3;
+    /** the minimal vertical difference for creating bend points. */
+    private static final double MIN_VERT_DIFF = 1.0;
+    /** factor for layer spacing. */
+    private static final double LAYER_SPACE_FAC = 0.2;
     
     /**
      * {@inheritDoc}
      */
     public IntermediateProcessingConfiguration getIntermediateProcessingConfiguration(
             final LGraph graph) {
-        
         return null;
     }
     
@@ -59,64 +61,81 @@ public class PolylineEdgeRouter extends AbstractAlgorithm implements ILayoutPhas
      */
     public void process(final LGraph layeredGraph) {
         getMonitor().begin("Polyline edge routing", 1);
-        float defspacing = layeredGraph.getProperty(Properties.OBJ_SPACING);
+        float spacing = layeredGraph.getProperty(Properties.OBJ_SPACING);
+        float edgeSpaceFac = layeredGraph.getProperty(Properties.EDGE_SPACING_FACTOR);
         
-        // set horizontal positioning for each layer and add bend points
-        double xpos = 0.0, spacing = 0.0;
+        double xpos = 0.0, layerSpacing = 0.0;
         for (Layer layer : layeredGraph) {
+            // set horizontal coordinates for all nodes of the layer
             layer.placeNodes(xpos);
-            int edgeCount = 0;
-            for (LNode node : layer) {
-                for (LPort port : node.getPorts()) {
-                    edgeCount += port.getOutgoingEdges().size();
-                }
-                if (node.getProperty(Properties.NODE_TYPE) == NodeType.LONG_EDGE) {
-                    LEdge edge = (LEdge) node.getProperty(Properties.ORIGIN);
-                    if (isEndnode(node, true)) {
-                        edge.getBendPoints().add(new KVector(xpos, node.getPosition().y));
-                    }
-                    if (isEndnode(node, false)) {
-                        edge.getBendPoints().add(new KVector(xpos + layer.getSize().x,
-                                node.getPosition().y));
-                    }
-                }
-            }
-            // determine placement of next layer based on the number of edges
-            spacing = edgeCount <= MAX_EDGES ? defspacing
-                       : defspacing * Math.sqrt(edgeCount);
-            xpos += layer.getSize().x + spacing;
-        }
-        layeredGraph.getSize().x = xpos - spacing;
-        getMonitor().done();
-    }
-    
-    /**
-     * Determines whether the given node is the first or the last node of
-     * a series of aligned long edge parts, depending on the port type.
-     * 
-     * @param node a dummy node of a long edge
-     * @param first if {@code true}, the result is true if the node is the
-     *     first node of the long edge part; if {@code false}, the result is true if
-     *     the node is the last node of the long edge part
-     * @return true if the node is an end-node
-     */
-    private boolean isEndnode(final LNode node, final boolean first) {
-        double bendPos = node.getPosition().y;
-        for (LPort port : node.getPorts()) {
-            Iterable<LPort> connectedPorts = first
-                ? port.getPredecessorPorts()
-                : port.getSuccessorPorts();
             
-            for (LPort connectedPort : connectedPorts) {
-                LNode otherNode = connectedPort.getNode();
-                double otherPos = otherNode.getPosition().y + connectedPort.getPosition().y
-                        + connectedPort.getAnchor().y;
-                if (Math.abs(otherPos - bendPos) > 1) {
-                    return true;
+            double maxVertDiff = 0;
+            for (LNode node : layer) {
+                // count the maximal vertical difference of output edges
+                double nodeMaxOutputDiff = 0;
+                for (LPort port : node.getPorts(PortType.OUTPUT)) {
+                    double sourcePos = port.getNode().getPosition().y
+                            + port.getPosition().y + port.getAnchor().y;
+                    for (LPort targetPort : port.getSuccessorPorts()) {
+                        if (targetPort.getNode().getLayer() != node.getLayer()) {
+                            double targetPos = targetPort.getNode().getPosition().y
+                                    + targetPort.getPosition().y + targetPort.getAnchor().y;
+                            nodeMaxOutputDiff = KielerMath.maxd(nodeMaxOutputDiff,
+                                    targetPos - sourcePos, sourcePos - targetPos);
+                        }
+                    }
                 }
+                
+                if (node.getProperty(Properties.NODE_TYPE) == NodeType.LONG_EDGE) {
+                    // count the maximal vertical difference of input edges
+                    double nodeMaxInputDiff = 0;
+                    for (LPort port : node.getPorts(PortType.INPUT)) {
+                        double targetPos = port.getNode().getPosition().y
+                                + port.getPosition().y + port.getAnchor().y;
+                        for (LPort sourcePort : port.getPredecessorPorts()) {
+                            double sourcePos = sourcePort.getNode().getPosition().y
+                                    + sourcePort.getPosition().y + sourcePort.getAnchor().y;
+                            nodeMaxInputDiff = KielerMath.maxd(nodeMaxInputDiff,
+                                    targetPos - sourcePos, sourcePos - targetPos);
+                        }
+                    }
+                    
+                    LEdge edge = (LEdge) node.getProperty(Properties.ORIGIN);
+                    if (nodeMaxInputDiff >= MIN_VERT_DIFF && nodeMaxOutputDiff >= MIN_VERT_DIFF) {
+                        // both the incoming and the outgoing edge have significant difference
+                        double layerSize = layer.getSize().x;
+                        double diff = Math.max(nodeMaxInputDiff, nodeMaxOutputDiff);
+                        double deviation = diff / (layerSize / 2 + spacing
+                                + LAYER_SPACE_FAC * edgeSpaceFac * diff) * layerSize / 2;
+                        if (deviation >= edgeSpaceFac * spacing) {
+                            // insert two bend points, one left and one right
+                            edge.getBendPoints().add(xpos, node.getPosition().y);
+                            edge.getBendPoints().add(xpos + layerSize, node.getPosition().y);
+                        } else {
+                            // insert only one bend point in the middle
+                            edge.getBendPoints().add(xpos + layerSize / 2, node.getPosition().y);
+                        }
+                        
+                    } else if (nodeMaxInputDiff >= MIN_VERT_DIFF) {
+                        // only the incoming edge has significant difference
+                        edge.getBendPoints().add(xpos, node.getPosition().y);
+                        
+                    } else if (nodeMaxOutputDiff >= MIN_VERT_DIFF) {
+                        // only the outgoing edge has significant difference
+                        edge.getBendPoints().add(xpos + layer.getSize().x, node.getPosition().y);
+                    }
+                }
+                
+                maxVertDiff = Math.max(maxVertDiff, nodeMaxOutputDiff);
             }
+            
+            // determine placement of next layer based on the maximal vertical difference
+            layerSpacing = spacing + LAYER_SPACE_FAC * edgeSpaceFac * maxVertDiff;
+            xpos += layer.getSize().x + layerSpacing;
         }
-        return false;
+        layeredGraph.getSize().x = xpos - layerSpacing;
+        
+        getMonitor().done();
     }
 
 }
