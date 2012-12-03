28e53e4096f11844c0f3f57c8b21d450d15c3210
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2012-12-03 14:41:59 +0100

    klay.layered: - Changed: Renamed Properties.EXT_PORT_RATIO_OR_POSITION to Properties.PORT_RATIO_OR_POSITION to be used for ports of regular nodes as well.

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/HierarchicalPortOrthogonalEdgeRouter.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/HierarchicalPortOrthogonalEdgeRouter.java
@@ -365,7 +365,7 @@ public class HierarchicalPortOrthogonalEdgeRouter extends AbstractAlgorithm impl
     private void applyNorthSouthDummyRatio(final LNode dummy, final double width) {
         KVector anchor = dummy.getProperty(Properties.PORT_ANCHOR);
         double offset = anchor == null ? 0 : anchor.x;
-        dummy.getPosition().x = width * dummy.getProperty(Properties.EXT_PORT_RATIO_OR_POSITION)
+        dummy.getPosition().x = width * dummy.getProperty(Properties.PORT_RATIO_OR_POSITION)
                 - offset;
     }
     
@@ -377,7 +377,7 @@ public class HierarchicalPortOrthogonalEdgeRouter extends AbstractAlgorithm impl
     private void applyNorthSouthDummyPosition(final LNode dummy) {
         KVector anchor = dummy.getProperty(Properties.PORT_ANCHOR);
         double offset = anchor == null ? 0 : anchor.x;
-        dummy.getPosition().x = dummy.getProperty(Properties.EXT_PORT_RATIO_OR_POSITION) - offset;
+        dummy.getPosition().x = dummy.getProperty(Properties.PORT_RATIO_OR_POSITION) - offset;
     }
     
     /**
@@ -433,8 +433,8 @@ public class HierarchicalPortOrthogonalEdgeRouter extends AbstractAlgorithm impl
         
         Arrays.sort(dummyArray, new Comparator<LNode>() {
             public int compare(final LNode a, final LNode b) {
-                double diff = a.getProperty(Properties.EXT_PORT_RATIO_OR_POSITION)
-                        - b.getProperty(Properties.EXT_PORT_RATIO_OR_POSITION);
+                double diff = a.getProperty(Properties.PORT_RATIO_OR_POSITION)
+                        - b.getProperty(Properties.PORT_RATIO_OR_POSITION);
                 
                 // We cannot simply return diff cast to an int: if diff == 0.4, the returned value
                 // would be 0, which is wrong
@@ -752,13 +752,13 @@ public class HierarchicalPortOrthogonalEdgeRouter extends AbstractAlgorithm impl
             case EAST:
             case WEST:
                 if (constraints == PortConstraints.FIXED_RATIO) {
-                    double ratio = node.getProperty(Properties.EXT_PORT_RATIO_OR_POSITION);
+                    double ratio = node.getProperty(Properties.PORT_RATIO_OR_POSITION);
                     nodePosition.y = graphActualSize.y * ratio
                             - node.getProperty(Properties.PORT_ANCHOR).y;
                     requiredActualGraphHeight = nodePosition.y + extPortSize.y;
                     node.borderToContentAreaCoordinates(false, true);
                 } else if (constraints == PortConstraints.FIXED_POS) {
-                    nodePosition.y = node.getProperty(Properties.EXT_PORT_RATIO_OR_POSITION)
+                    nodePosition.y = node.getProperty(Properties.PORT_RATIO_OR_POSITION)
                             - node.getProperty(Properties.PORT_ANCHOR).y;
                     requiredActualGraphHeight = nodePosition.y + extPortSize.y;
                     node.borderToContentAreaCoordinates(false, true);
