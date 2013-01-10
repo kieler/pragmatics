20543284862ba333131b9bebc2628330ff9c076a
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2013-01-10 13:11:40 +0100

    klay.layered: - Changed: Refactored a tiny bit.

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/NodeMarginCalculator.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/NodeMarginCalculator.java
@@ -55,126 +55,136 @@ public final class NodeMarginCalculator implements ILayoutProcessor {
      */
     public void process(final LGraph layeredGraph, final IKielerProgressMonitor monitor) {
         monitor.begin("Node margin calculation", 1);
+        
         double spacing = layeredGraph.getProperty(Properties.OBJ_SPACING);
 
         // Iterate through the layers
         for (Layer layer : layeredGraph) {
             // Iterate through the layer's nodes
             for (LNode node : layer) {
-                // This will be our bounding box. We'll start with one that's the same size
-                // as our node, and at the same position.
-                Rectangle2D.Double boundingBox = new Rectangle2D.Double(
-                        node.getPosition().x,
-                        node.getPosition().y,
-                        node.getSize().x,
-                        node.getSize().y);
+                processNode(node, spacing);
+            }
+        }
+        
+        monitor.done();
+    }
+
+    /**
+     * Calculates the margin of the given node.
+     * 
+     * @param node the node whose margin to calculate.
+     * @param spacing object spacing set on the layered graph.
+     */
+    private void processNode(final LNode node, final double spacing) {
+        // This will be our bounding box. We'll start with one that's the same size
+        // as our node, and at the same position.
+        Rectangle2D.Double boundingBox = new Rectangle2D.Double(
+                node.getPosition().x,
+                node.getPosition().y,
+                node.getSize().x,
+                node.getSize().y);
+        
+        // We'll reuse this rectangle as our box for elements to add to the bounding box
+        Rectangle2D.Double elementBox = new Rectangle2D.Double();
+        
+        // Put the node's labels into the bounding box
+        for (LLabel label : node.getLabels()) {
+            elementBox.x = label.getPosition().x + node.getPosition().x;
+            elementBox.y = label.getPosition().y + node.getPosition().y;
+            elementBox.width = label.getSize().x;
+            elementBox.height = label.getSize().y;
+            
+            Rectangle2D.union(boundingBox, elementBox, boundingBox);
+        }
+        
+        // Do the same for ports and their labels
+        for (LPort port : node.getPorts()) {
+            // Calculate the port's upper left corner's x and y coordinate
+            double portX = port.getPosition().x + node.getPosition().x;
+            double portY = port.getPosition().y + node.getPosition().y;
+            
+            // The port itself
+            elementBox.x = portX;
+            elementBox.y = portY;
+            elementBox.width = port.getSize().x;
+            elementBox.height = port.getSize().y;
+            
+            Rectangle2D.union(boundingBox, elementBox, boundingBox);
+            
+            // The port's labels
+            for (LLabel label : port.getLabels()) {
+                elementBox.x = label.getPosition().x + portX;
+                elementBox.y = label.getPosition().y + portY;
+                elementBox.width = label.getSize().x;
+                elementBox.height = label.getSize().y;
                 
-                // We'll reuse this rectangle as our box for elements to add to the bounding box
-                Rectangle2D.Double elementBox = new Rectangle2D.Double();
+                Rectangle2D.union(boundingBox, elementBox, boundingBox);
+            }
+        }
+        
+        // Do the same for end labels and port labels on edges connected to the node
+        for (LPort port : node.getPorts()) {
+            // Calculate the port's upper left corner's x and y coordinate
+            double portX = port.getPosition().x + node.getPosition().x;
+            double portY = port.getPosition().y + node.getPosition().y;
+            double portLabelX = 0;
+            double portLabelY = 0;
+            
+            //TODO: maybe leave space for manually placed ports 
+            if (node.getProperty(Properties.PORT_LABEL_PLACEMENT) == PortLabelPlacement.OUTSIDE) {
                 
-                // Put the node's labels into the bounding box
-                for (LLabel label : node.getLabels()) {
-                    elementBox.x = label.getPosition().x + node.getPosition().x;
-                    elementBox.y = label.getPosition().y + node.getPosition().y;
-                    elementBox.width = label.getSize().x;
-                    elementBox.height = label.getSize().y;
-                    
-                    Rectangle2D.union(boundingBox, elementBox, boundingBox);
+                for (LLabel label : port.getLabels()) {
+                    if (portLabelX < label.getSize().x) {
+                        portLabelX = label.getSize().x;
+                    }
+                    if (portLabelY < label.getSize().y) {
+                        portLabelY = label.getSize().y;
+                    }
                 }
-                
-                // Do the same for ports and their labels
-                for (LPort port : node.getPorts()) {
-                    // Calculate the port's upper left corner's x and y coordinate
-                    double portX = port.getPosition().x + node.getPosition().x;
-                    double portY = port.getPosition().y + node.getPosition().y;
-                    
-                    // The port itself
-                    elementBox.x = portX;
-                    elementBox.y = portY;
-                    elementBox.width = port.getSize().x;
-                    elementBox.height = port.getSize().y;
-                    
-                    Rectangle2D.union(boundingBox, elementBox, boundingBox);
-                    
-                    // The port's labels
-                    for (LLabel label : port.getLabels()) {
-                        elementBox.x = label.getPosition().x + portX;
-                        elementBox.y = label.getPosition().y + portY;
+            }
+            
+            for (LEdge edge : port.getOutgoingEdges()) {
+                // For each edge, the head labels of outgoing edges ...
+                for (LLabel label : edge.getLabels()) {
+                    if (label.getProperty(LayoutOptions.EDGE_LABEL_PLACEMENT)
+                            == EdgeLabelPlacement.HEAD) {
+                        
+                        elementBox.x = portX;
+                        elementBox.y = portY;
+                        elementBox.width = label.getSize().x + portLabelX;
+                        elementBox.height = label.getSize().y + portLabelY;
+                        
+                        Rectangle2D.union(boundingBox, elementBox, boundingBox);
+                    }
+                }
+            }
+            
+            for (LEdge edge : port.getIncomingEdges()) {
+                // ... and the tail label of incoming edges shall be considered 
+                for (LLabel label : edge.getLabels()) {
+                    if (label.getProperty(LayoutOptions.EDGE_LABEL_PLACEMENT)
+                            == EdgeLabelPlacement.TAIL) {
+                        
+                        elementBox.x = portX - label.getSize().x;
+                        elementBox.y = portY;
                         elementBox.width = label.getSize().x;
                         elementBox.height = label.getSize().y;
                         
                         Rectangle2D.union(boundingBox, elementBox, boundingBox);
                     }
                 }
-                
-                // Do the same for end labels and port labels on edges connected to the node
-                for (LPort port : node.getPorts()) {
-                    // Calculate the port's upper left corner's x and y coordinate
-                    double portX = port.getPosition().x + node.getPosition().x;
-                    double portY = port.getPosition().y + node.getPosition().y;
-                    double portLabelX = 0;
-                    double portLabelY = 0;
-                    
-                    //TODO: maybe leave space for manually placed ports 
-                    if (node.getProperty(Properties.PORT_LABEL_PLACEMENT)
-                            == PortLabelPlacement.OUTSIDE) {
-                        
-                        for (LLabel label : port.getLabels()) {
-                            if (portLabelX < label.getSize().x) {
-                                portLabelX = label.getSize().x;
-                            }
-                            if (portLabelY < label.getSize().y) {
-                                portLabelY = label.getSize().y;
-                            }
-                        }
-                    }
-                    
-                    for (LEdge edge : port.getOutgoingEdges()) {
-                        // For each edge, the head labels of outgoing edges ...
-                        for (LLabel label : edge.getLabels()) {
-                            if (label.getProperty(LayoutOptions.EDGE_LABEL_PLACEMENT)
-                                    == EdgeLabelPlacement.HEAD) {
-                                
-                                elementBox.x = portX;
-                                elementBox.y = portY;
-                                elementBox.width = label.getSize().x + portLabelX;
-                                elementBox.height = label.getSize().y + portLabelY;
-                                
-                                Rectangle2D.union(boundingBox, elementBox, boundingBox);
-                            }
-                        }
-                    }
-                    
-                    for (LEdge edge : port.getIncomingEdges()) {
-                        // ... and the tail label of incoming edges shall be considered 
-                        for (LLabel label : edge.getLabels()) {
-                            if (label.getProperty(LayoutOptions.EDGE_LABEL_PLACEMENT)
-                                    == EdgeLabelPlacement.TAIL) {
-                                
-                                elementBox.x = portX - label.getSize().x;
-                                elementBox.y = portY;
-                                elementBox.width = label.getSize().x;
-                                elementBox.height = label.getSize().y;
-                                
-                                Rectangle2D.union(boundingBox, elementBox, boundingBox);
-                            }
-                        }
-                    }
-                }
-                
-                // Reset the margin
-                LInsets.Double margin = node.getMargin();
-                margin.top = node.getPosition().y - boundingBox.y;
-                margin.bottom = boundingBox.getMaxY() - (node.getPosition().y + node.getSize().y);
-                margin.left = node.getPosition().x - boundingBox.x;
-                margin.right = boundingBox.getMaxX() - (node.getPosition().x + node.getSize().x);
-                
-                // Process comments that are placed near the node
-                processComments(node, spacing);
             }
         }
         
-        monitor.done();
+        // Reset the margin
+        LInsets.Double margin = node.getMargin();
+        margin.top = node.getPosition().y - boundingBox.y;
+        margin.bottom = boundingBox.getMaxY() - (node.getPosition().y + node.getSize().y);
+        margin.left = node.getPosition().x - boundingBox.x;
+        margin.right = boundingBox.getMaxX() - (node.getPosition().x + node.getSize().x);
+        
+        // Process comments that are placed near the node
+        processComments(node, spacing);
     }
     
     /**
