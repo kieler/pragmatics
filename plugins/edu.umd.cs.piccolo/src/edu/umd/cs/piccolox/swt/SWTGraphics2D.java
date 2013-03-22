87866c28b36661837bc385ff188d1f47c73a54cd
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-03-22 15:36:19 +0100

    edu.umd.cs.piccolo: fixed transparency calculation (I misunderstood the semantics of the float value, see PNode class)

--- plugins/edu.umd.cs.piccolo/src/edu/umd/cs/piccolox/swt/SWTGraphics2D.java
+++ plugins/edu.umd.cs.piccolo/src/edu/umd/cs/piccolox/swt/SWTGraphics2D.java
@@ -1853,7 +1853,7 @@ public class SWTGraphics2D extends Graphics2D {
     public void setComposite(final Composite comp) {
         if (comp instanceof AlphaComposite) {
             AlphaComposite ac = (AlphaComposite) comp;
-            gc.setAlpha((int) ((1-ac.getAlpha()) * 255));
+            gc.setAlpha((int) ac.getAlpha() * 255);
             this.transparency = ac;
         } else {
             gc.setAlpha(255);
