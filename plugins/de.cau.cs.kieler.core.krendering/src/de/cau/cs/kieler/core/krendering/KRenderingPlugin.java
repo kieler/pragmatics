05241c400170f4e3ec58f802a34758c64caecf01
Christian Schneider <chsch@informatik.uni-kiel.de>
2013-04-22 16:14:56 +0200

    core.krendering: introduced activator 'KRenderingPlugin' providing the plugin id

---
+++ plugins/de.cau.cs.kieler.core.krendering/src/de/cau/cs/kieler/core/krendering/KRenderingPlugin.java
@@ -0,0 +1,28 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2012 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.core.krendering;
+
+import org.eclipse.core.runtime.Plugin;
+
+/**
+ * The representing class of the bundle 'de.cau.cs.kieler.core.krendering'.
+ * 
+ * @author chsch
+ */
+public class KRenderingPlugin extends Plugin {
+
+    /** The plug-in ID. */
+    public static final String PLUGIN_ID = "de.cau.cs.kieler.core.krendering";
+
+}
