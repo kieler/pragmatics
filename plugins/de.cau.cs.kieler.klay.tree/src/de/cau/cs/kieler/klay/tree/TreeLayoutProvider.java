8a24517262c2e89725d10d6133a911fd8b60ae63
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2013-04-25 13:54:03 +0200

    klay.tree: - Added: New KLay Tree layout algorithm skeleton.

---
+++ plugins/de.cau.cs.kieler.klay.tree/src/de/cau/cs/kieler/klay/tree/TreeLayoutProvider.java
@@ -0,0 +1,35 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2013 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klay.tree;
+
+import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
+import de.cau.cs.kieler.core.kgraph.KNode;
+import de.cau.cs.kieler.kiml.AbstractLayoutProvider;
+
+/**
+ * TODO: Document this class.
+ * 
+ * @author cds
+ */
+public class TreeLayoutProvider extends AbstractLayoutProvider {
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doLayout(KNode parentNode, IKielerProgressMonitor progressMonitor) {
+        // TODO Auto-generated method stub
+    }
+    
+}
