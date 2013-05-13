83394a047c84d0e4f815ace8346c938aa4f3e047
Ulf Rüegg <uru@informatik.uni-kiel.de>
2013-05-13 16:59:51 +0200

    - adapted cycle breaker test to new infrastructure - added new tests for the layering phase

---
+++ test/de.cau.cs.kieler.klay.layered.test/src/de/cau/cs/kieler/klay/layered/test/phases/AbstractLayeredPhaseTest.java
@@ -0,0 +1,93 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2013 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klay.layered.test.phases;
+
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Before;
+
+import de.cau.cs.kieler.klay.layered.KlayLayered;
+import de.cau.cs.kieler.klay.layered.LayeredLayoutProvider;
+import de.cau.cs.kieler.klay.layered.graph.LGraph;
+import de.cau.cs.kieler.klay.test.KlayAutomatedJUnitTest;
+import de.cau.cs.kieler.klay.test.config.ILayoutConfigurator;
+import de.cau.cs.kieler.klay.test.utils.GraphTestObject;
+import de.cau.cs.kieler.klay.test.utils.TestPath;
+
+/**
+ * Common setup for tests run for the layered layout algorithm.
+ * 
+ * @author uru
+ */
+public abstract class AbstractLayeredPhaseTest extends KlayAutomatedJUnitTest {
+
+    // CHECKSTYLEOFF Modifier - Members used in subclasses.
+
+    /** The internal graph information, i.e., the file and the KNode. */
+    protected GraphTestObject graphObject;
+    /** Holds the LGraphs resulting from a layout run. <b>Not</b> assigned by default! */
+    protected List<LGraph> lgraphs;
+    /** Holds the layered layout provider that is used to perform the layout. */
+    protected LayeredLayoutProvider layeredProvider = new LayeredLayoutProvider();
+    /** Holds the current layout configurator. */
+    protected ILayoutConfigurator configurator;
+    /**
+     * Holds an instance of the layered algorithm with
+     * {@link LayeredLayoutProvider#startLayoutTest(de.cau.cs.kieler.core.kgraph.KNode)} already
+     * called with the root node.
+     */
+    protected KlayLayered layered;
+    
+    /** Chose a fixed seed to allow reproducibility of failing tests. */
+    protected static final int SEED = 1337;
+    /** random object if required. */
+    protected Random random = new Random(SEED);
+
+    // CHECKSTYLEON Modifier
+
+    /**
+     * @param testObject
+     *            the test object
+     * @param config
+     *            the layout configuration for a certain layout run
+     */
+    public AbstractLayeredPhaseTest(final GraphTestObject testObject,
+            final ILayoutConfigurator config) {
+        this.graphObject = testObject;
+        this.configurator = config;
+    }
+
+    /**
+     * Configuration cannot be performed in the constructor. @Before methods in subclasses are
+     * called <em>after</em> this one.
+     */
+    @Before
+    public void configure() {
+        // apply the configurator
+        configurator.applyConfiguration(graphObject.getKnode());
+
+        // get an instance of layered
+        this.layered = layeredProvider.startLayoutTest(graphObject.getKnode());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected TestPath[] getBundleTestPath() {
+        TestPath[] testPaths = { new TestPath("random", false, false, TestPath.Type.KGRAPH) };
+        return testPaths;
+    }
+
+}
--- test/de.cau.cs.kieler.klay.layered.test/src/de/cau/cs/kieler/klay/layered/test/phases/CycleBreakerTest.java
+++ test/de.cau.cs.kieler.klay.layered.test/src/de/cau/cs/kieler/klay/layered/test/phases/CycleBreakerTest.java
@@ -11,23 +11,20 @@
  * This code is provided under the terms of the Eclipse Public License (EPL).
  * See the file epl-v10.html for the license text.
  */
-package de.cau.cs.kieler.klay.layered.test;
+package de.cau.cs.kieler.klay.layered.test.phases;
 
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
-import java.util.Random;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
-import de.cau.cs.kieler.core.alg.BasicProgressMonitor;
 import de.cau.cs.kieler.core.kgraph.KNode;
 import de.cau.cs.kieler.kiml.klayoutdata.KShapeLayout;
-import de.cau.cs.kieler.klay.layered.ILayoutPhase;
-import de.cau.cs.kieler.klay.layered.LayeredLayoutProvider;
 import de.cau.cs.kieler.klay.layered.graph.LEdge;
 import de.cau.cs.kieler.klay.layered.graph.LGraph;
 import de.cau.cs.kieler.klay.layered.graph.LNode;
@@ -35,95 +32,93 @@ import de.cau.cs.kieler.klay.layered.p1cycles.CycleBreakingStrategy;
 import de.cau.cs.kieler.klay.layered.p1cycles.GreedyCycleBreaker;
 import de.cau.cs.kieler.klay.layered.p1cycles.InteractiveCycleBreaker;
 import de.cau.cs.kieler.klay.layered.properties.Properties;
-import de.cau.cs.kieler.klay.test.KlayAutomatedJUnitTest;
+import de.cau.cs.kieler.klay.test.config.ILayoutConfigurator;
+import de.cau.cs.kieler.klay.test.config.SimplePhaseLayoutConfigurator;
 import de.cau.cs.kieler.klay.test.utils.GraphTestObject;
-import de.cau.cs.kieler.klay.test.utils.TestPath;
 
 /**
  * Basic tests for the cycle removing phase.
  * 
  * @author uru
  */
-public class CycleBreakerTest extends KlayAutomatedJUnitTest {
-
-    // Object containing the current graphTestObject that contains both, a File and a KNode.
-    private GraphTestObject graphObject;
-
-    private List<LGraph> lgraphs;
-
-    private LayeredLayoutProvider layered = new LayeredLayoutProvider();
+public class CycleBreakerTest extends AbstractLayeredPhaseTest {
 
     // Configuring the interactive cycle breaker
     private static final int INTERACTIVE_MAX_POS = 100;
-    private static final int SEED = 1337;
-    private Random random = new Random(SEED);
 
     /**
-     * Instantiates a new KlayTestExample test and set the graphObject to the current graph to test.
+     * Instantiates a new cycle breaker test and set the graphObject to the current graph to test.
      * 
      * @param testObject
      *            the test object
+     * @param config
+     *            layout configurator
      */
-    public CycleBreakerTest(final GraphTestObject testObject) {
-        graphObject = testObject;
+    public CycleBreakerTest(final GraphTestObject testObject, final ILayoutConfigurator config) {
+        super(testObject, config);
     }
 
     /**
-     * {@inheritDoc}
-     */
-    protected TestPath[] getBundleTestPath() {
-        TestPath[] testPaths = { new TestPath("random", false, false, TestPath.Type.KGRAPH) };
-        return testPaths;
-    }
-
-    /**
-     * Tests whether the current graph is acyclic with the interactive strategy. For the interactive
-     * strategy it is important that every node has an assigned position.
-     */
-    @Test
-    public void testAcyclicInteractive() {
-
-        // assign random coordinates to the nodes
-        for (KNode n : graphObject.getKnode().getChildren()) {
-            KShapeLayout shape = n.getData(KShapeLayout.class);
-            if (shape.getXpos() == 0) {
-                shape.setXpos(random.nextInt(INTERACTIVE_MAX_POS));
-            }
-            if (shape.getYpos() == 0) {
-                shape.setYpos(random.nextInt(INTERACTIVE_MAX_POS));
-            }
-        }
-
-        testWithStrategy(CycleBreakingStrategy.INTERACTIVE, InteractiveCycleBreaker.class);
-    }
-
-    /**
-     * Tests whether the current graph is acyclic with the greedy strategy.
-     */
-    @Test
-    public void testAcyclicGreedy() {
-        testWithStrategy(CycleBreakingStrategy.GREEDY, GreedyCycleBreaker.class);
-    }
-
-    /**
-     * Performs the actual test by applying the layout algorithm with the requested strategy until
-     * the cycle breaking phase finished. Afterwards the acyclic test is performed.
+     * Add a configurator for following strategies.
+     * <ul>
+     * <li> {@link GreedyCycleBreaker}</li>
+     * <li> {@link InteractiveCycleBreaker}</li>
+     * </ul>
      * 
-     * @param strategy
-     *            One of the available cycle breaking strategies.
-     * @param processor
-     *            The corresponding {{@link ILayoutPhase}
+     * @return the possible configurations for this test.
      */
-    private void testWithStrategy(final CycleBreakingStrategy strategy,
-            final Class<? extends ILayoutPhase> processor) {
-        // set the desired strategy
-        graphObject.getKnode().getData(KShapeLayout.class)
-                .setProperty(Properties.CYCLE_BREAKING, strategy);
+    @Override
+    protected List<ILayoutConfigurator> getConfigurators() {
+        List<ILayoutConfigurator> configs = Lists.newArrayList();
 
-        // perform the layout
-        lgraphs = layered.doLayoutTest(graphObject.getKnode(), new BasicProgressMonitor(),
-                processor);
+        // INTERACTIVE
+        // For the interactive strategy it is important that every node has an assigned position.
+        configs.add(new SimplePhaseLayoutConfigurator(Properties.CYCLE_BREAKING,
+                CycleBreakingStrategy.INTERACTIVE, InteractiveCycleBreaker.class) {
 
+            public void applyConfiguration(final KNode root) {
+                // assign random coordinates to the nodes
+                for (KNode n : root.getChildren()) {
+                    KShapeLayout shape = n.getData(KShapeLayout.class);
+                    if (shape.getXpos() == 0) {
+                        shape.setXpos(random.nextInt(INTERACTIVE_MAX_POS));
+                    }
+                    if (shape.getYpos() == 0) {
+                        shape.setYpos(random.nextInt(INTERACTIVE_MAX_POS));
+                    }
+                }
+
+                // call super to apply the original config
+                super.applyConfiguration(root);
+            }
+        });
+
+        // GREEDY
+        configs.add(new SimplePhaseLayoutConfigurator(Properties.CYCLE_BREAKING,
+                CycleBreakingStrategy.GREEDY, GreedyCycleBreaker.class));
+
+        return configs;
+    }
+
+    /**
+     * Run the layout algorithm until the specified cycle breaking strategy finished.
+     */
+    @Before
+    public void runUntil() {
+        if (configurator instanceof SimplePhaseLayoutConfigurator) {
+            SimplePhaseLayoutConfigurator simple = (SimplePhaseLayoutConfigurator) configurator;
+            lgraphs = layered.runLayoutTestUntil(simple.getStrategyImpl());
+        } else {
+            throw new IllegalArgumentException("Every cycle breaking configurator should be "
+                    + "a subclass of SimplePhaseLayoutConfigurator");
+        }
+    }
+
+    /**
+     * Tests whether the current graph is acyclic.
+     */
+    @Test
+    public void testIsAcyclic() {
         // check every component
         for (LGraph lg : lgraphs) {
             List<LNode> nodes = lg.getLayerlessNodes();
