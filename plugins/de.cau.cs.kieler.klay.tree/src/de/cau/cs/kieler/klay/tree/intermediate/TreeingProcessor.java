c391b31196b9b45c9e1293196a0ccbc0d425a792
Sven Oliver Reimers <sor@informatik.uni-kiel.de>
2013-07-03 02:08:47 +0200

    Further treeing approaches including post-processor debugged/updated

---
+++ plugins/de.cau.cs.kieler.klay.tree/src/de/cau/cs/kieler/klay/tree/intermediate/TreeingProcessor.java
@@ -0,0 +1,48 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2013 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klay.tree.intermediate;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
+import de.cau.cs.kieler.klay.tree.ILayoutProcessor;
+import de.cau.cs.kieler.klay.tree.graph.TEdge;
+import de.cau.cs.kieler.klay.tree.graph.TGraph;
+import de.cau.cs.kieler.klay.tree.graph.TNode;
+import de.cau.cs.kieler.klay.tree.properties.Properties;
+
+/**
+ * @author sor
+ * @author sgu
+ *
+ */
+public class TreeingProcessor implements ILayoutProcessor {
+
+    List<TEdge> edges = new LinkedList<TEdge>();
+    /**
+     * {@inheritDoc}
+     */
+    public void process(TGraph tGraph, IKielerProgressMonitor progressMonitor) {
+        
+        edges.clear();
+        edges = tGraph.getProperty(Properties.REMOVABLE_EDGES);
+        
+        for (TEdge e : edges) {
+            TNode src = e.getSource();
+            TNode target = e.getTarget();
+            tGraph.addEdge(src, target);
+        }
+    }
+}
