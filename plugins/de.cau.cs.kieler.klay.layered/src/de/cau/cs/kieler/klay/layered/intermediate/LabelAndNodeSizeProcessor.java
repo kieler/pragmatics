14cef921cbba22354e6c855f432e7cabdbdc8489
Ulf Rüegg <uru@informatik.uni-kiel.de>
2014-01-23 21:26:24 +1100

    klay.layered: first test to incorporate kiml node size calculation

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/LabelAndNodeSizeProcessor.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/intermediate/LabelAndNodeSizeProcessor.java
@@ -14,23 +14,38 @@
 package de.cau.cs.kieler.klay.layered.intermediate;
 
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
 import java.util.EnumSet;
+import java.util.List;
+
+import com.google.common.collect.Lists;
 
 import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
 import de.cau.cs.kieler.core.math.KVector;
+import de.cau.cs.kieler.core.properties.IProperty;
 import de.cau.cs.kieler.kiml.options.LayoutOptions;
 import de.cau.cs.kieler.kiml.options.NodeLabelPlacement;
 import de.cau.cs.kieler.kiml.options.PortConstraints;
 import de.cau.cs.kieler.kiml.options.PortLabelPlacement;
+import de.cau.cs.kieler.kiml.options.PortSide;
 import de.cau.cs.kieler.kiml.options.SizeConstraint;
 import de.cau.cs.kieler.kiml.options.SizeOptions;
 import de.cau.cs.kieler.kiml.util.KimlUtil;
+import de.cau.cs.kieler.kiml.util.NodeSizeCalculation;
+import de.cau.cs.kieler.kiml.util.NodeSizeCalculation.GraphAdapter;
+import de.cau.cs.kieler.kiml.util.NodeSizeCalculation.GraphElementAdapter;
+import de.cau.cs.kieler.kiml.util.NodeSizeCalculation.LabelAdapter;
+import de.cau.cs.kieler.kiml.util.NodeSizeCalculation.NodeAdapter;
+import de.cau.cs.kieler.kiml.util.NodeSizeCalculation.PortAdapter;
+import de.cau.cs.kieler.kiml.util.NodeSizeCalculation.SizeCalculationAdapterFactory;
 import de.cau.cs.kieler.klay.layered.ILayoutProcessor;
 import de.cau.cs.kieler.klay.layered.graph.LGraph;
+import de.cau.cs.kieler.klay.layered.graph.LGraphElement;
 import de.cau.cs.kieler.klay.layered.graph.LInsets;
 import de.cau.cs.kieler.klay.layered.graph.LLabel;
 import de.cau.cs.kieler.klay.layered.graph.LNode;
 import de.cau.cs.kieler.klay.layered.graph.LPort;
+import de.cau.cs.kieler.klay.layered.graph.LShape;
 import de.cau.cs.kieler.klay.layered.graph.Layer;
 import de.cau.cs.kieler.klay.layered.graph.LLabel.LabelSide;
 import de.cau.cs.kieler.klay.layered.properties.Properties;
@@ -150,6 +165,11 @@ public final class LabelAndNodeSizeProcessor implements ILayoutProcessor {
      */
     public void process(final LGraph layeredGraph, final IKielerProgressMonitor monitor) {
         monitor.begin("Node and Port Label Placement and Node Sizing", 1);
+        
+        new NodeSizeCalculation().calculateNodeSizes(layeredGraph, new LGraphSizeCalculatorFactory());
+        if (1==1) {
+            return;
+        }
         
         double objectSpacing = layeredGraph.getProperty(Properties.OBJ_SPACING);
         double labelSpacing = layeredGraph.getProperty(LayoutOptions.LABEL_SPACING);
@@ -1089,4 +1109,234 @@ public final class LabelAndNodeSizeProcessor implements ILayoutProcessor {
         }
     }
     
+    
+    /*
+     * Adaptor implementation
+     */
+
+    class LGraphSizeCalculatorFactory implements SizeCalculationAdapterFactory<LGraph> {
+        /**
+         * {@inheritDoc}
+         */
+        public GraphAdapter<LGraph> getGraphAdapter(final LGraph graph) {
+            return new LGraphAdaptor(graph);
+        }
+    }
+
+    class AbstractLGraphAdaptor<T extends LShape> implements GraphElementAdapter<T> {
+
+        protected T element;
+
+        /**
+     * 
+     */
+        public AbstractLGraphAdaptor(final T element) {
+            this.element = element;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public KVector getSize() {
+            return element.getSize();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void setSize(final KVector size) {
+            element.getSize().x = size.x;
+            element.getSize().y = size.y;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public KVector getPosition() {
+            return element.getPosition();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void setPosition(final KVector pos) {
+            element.getPosition().x = pos.x;
+            element.getPosition().y = pos.y;
+            
+            System.out.println("Position of " + element + " " + pos + " " + element.getPosition());
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public <P> P getProperty(final IProperty<P> prop) {
+            return element.getProperty(prop);
+        }
+
+    }
+
+    class LGraphAdaptor implements GraphAdapter<LGraph> {
+
+        protected LGraph element;
+
+        /**
+      * 
+      */
+        public LGraphAdaptor(final LGraph element) {
+            this.element = element;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public KVector getSize() {
+            return element.getSize();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void setSize(final KVector size) {
+            element.getSize().x = size.x;
+            element.getSize().y = size.y;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public KVector getPosition() {
+            throw new UnsupportedOperationException("Not supported by LGraph");
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void setPosition(final KVector pos) {
+            throw new UnsupportedOperationException("Not supported by LGraph");
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public <P> P getProperty(final IProperty<P> prop) {
+            return element.getProperty(prop);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public List<NodeAdapter<?>> getNodes() {
+            List<NodeAdapter<?>> nodeAdapter = Lists.newLinkedList();
+            for (Layer l : element.getLayers()) {
+                for (LNode n : l.getNodes()) {
+                    nodeAdapter.add(new LNodeAdapter(n));
+                }
+            }
+            return nodeAdapter;
+        }
+
+    }
+
+    class LNodeAdapter extends AbstractLGraphAdaptor<LNode> implements NodeAdapter<LNode> {
+
+        /**
+         * @param element
+         */
+        public LNodeAdapter(final LNode element) {
+            super(element);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public List<LabelAdapter<?>> getLabels() {
+            List<LabelAdapter<?>> labelAdapters = Lists.newLinkedList();
+            for (LLabel l : element.getLabels()) {
+                labelAdapters.add(new LLabelAdapter(l));
+            }
+            return labelAdapters;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public List<PortAdapter<?>> getPorts() {
+            List<PortAdapter<?>> portAdapters = Lists.newLinkedList();
+            for (LPort p : element.getPorts()) {
+                portAdapters.add(new LPortAdapter(p));
+            }
+            return portAdapters;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void setInsets(Rectangle2D.Double insets) {
+            element.getInsets().left = insets.x;
+            element.getMargin().top = insets.y;
+            element.getMargin().right = insets.width;
+            element.getMargin().bottom = insets.height;
+
+        }
+
+    }
+
+    class LPortAdapter extends AbstractLGraphAdaptor<LPort> implements PortAdapter<LPort> {
+
+        /**
+         * @param element
+         */
+        public LPortAdapter(final LPort element) {
+            super(element);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public PortSide getSide() {
+            return element.getSide();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public Double getMargin() {
+            LInsets margin = element.getMargin();
+            return new Rectangle2D.Double(margin.left, margin.top, margin.right, margin.bottom);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void setMargin(final Rectangle2D.Double margin) {
+            element.getMargin().left = margin.x;
+            element.getMargin().top = margin.y;
+            element.getMargin().right = margin.width;
+            element.getMargin().bottom = margin.height;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public List<LabelAdapter<?>> getLabels() {
+            List<LabelAdapter<?>> labelAdapters = Lists.newLinkedList();
+            for (LLabel l : element.getLabels()) {
+                labelAdapters.add(new LLabelAdapter(l));
+            }
+            return labelAdapters;
+        }
+
+    }
+
+    class LLabelAdapter extends AbstractLGraphAdaptor<LLabel> implements LabelAdapter<LLabel> {
+
+        /**
+         * @param element
+         */
+        public LLabelAdapter(LLabel element) {
+            super(element);
+        }
+
+    }
+
 }
