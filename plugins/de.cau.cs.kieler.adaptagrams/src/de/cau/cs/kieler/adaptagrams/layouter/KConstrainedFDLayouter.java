1575d0583f7147f7753d8839b0e3c210cf18366b
Ulf Rueegg <uru@informatik.uni-kiel.de>
2014-06-05 11:55:50 +0200

    adaptagrams: wrapper for fdlayout

---
+++ plugins/de.cau.cs.kieler.adaptagrams/src/de/cau/cs/kieler/adaptagrams/layouter/KConstrainedFDLayouter.java
@@ -0,0 +1,215 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2014 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.adaptagrams.layouter;
+
+import org.adaptagrams.ConstrainedFDLayout;
+import org.adaptagrams.Doubles;
+import org.adaptagrams.TestConvergence;
+import org.adaptagrams.UnsatisfiableConstraintInfoPtrs;
+
+import de.cau.cs.kieler.adaptagrams.cgraph.CGraph;
+
+/**
+ * Wrapper class for adaptagram's {@link ConstrainedFDLayout} algorithm. It allows convenient
+ * configuration and execution.
+ * 
+ * FIXME currently we cannot call {@link ConstrainedFDLayout#freeAssociatedObjects()} as elements
+ * might be reused that are deleted by this call. Thus, we have a memory leak.
+ * 
+ * @author uru
+ */
+public class KConstrainedFDLayouter {
+
+    /** The graph to be layouted. */
+    private CGraph graph;
+    /** Whether overlap removal is performed during layout. */
+    private boolean overlapPrevention = false;
+    /** The ideal edge length for all edges or a multiplier for individual edge lengths. */
+    private double idealEdgeLength = 1;
+    /** Whether to use individual edge lengths. */
+    private boolean individualEdgeLengths = false;
+    /** A custom convergence test. */
+    private TestConvergence convergenceTest = null;
+
+    /** The internal layouter object. */
+    private ConstrainedFDLayout layouter;
+
+    // for debugging
+    private UnsatisfiableConstraintInfoPtrs xPtrs;
+    private UnsatisfiableConstraintInfoPtrs yPtrs;
+
+    /**
+     * Creates a new wrapper object that can be configured.
+     */
+    public KConstrainedFDLayouter() {
+    }
+
+    /**
+     * @param cgraph
+     *            the {@link CGraph} to layout.
+     * @return this.
+     */
+    public KConstrainedFDLayouter withCGraph(final CGraph cgraph) {
+        if (graph != null) {
+            throw new IllegalStateException(
+                    "A CGraph has already been specified for this layouter.");
+        } else if (cgraph == null) {
+            throw new IllegalArgumentException("null is not allowed for cgraph.");
+        }
+        this.graph = cgraph;
+        return this;
+    }
+
+    /**
+     * Turns on overlap removal during the layout.
+     * 
+     * @return this.
+     */
+    public KConstrainedFDLayouter withRemoveOverlaps() {
+        this.overlapPrevention = true;
+        return this;
+    }
+
+    /**
+     * Turns overlap removal during the layout on or off.
+     * 
+     * @param removeOverlaps
+     *            Whether to remove overlaps.
+     * @return this.
+     */
+    public KConstrainedFDLayouter withRemoveOverlaps(final boolean removeOverlaps) {
+        this.overlapPrevention = removeOverlaps;
+        return this;
+    }
+
+    /**
+     * @param ie
+     *            the ideal edge length to use for all edges. If
+     *            {@link #withIndividualEdgeLengths()} is used, this value serves as a multiplier
+     *            for every value in the individual length array. Default is 1.0.
+     * @return this.
+     */
+    public KConstrainedFDLayouter withIdealEdgeLength(final double ie) {
+        this.idealEdgeLength = ie;
+        return this;
+    }
+
+    /**
+     * If activated, the {@link CGraph#getIdealEdgeLengths()} are used as edge lengths. Note that
+     * the value specified by {@link #withIdealEdgeLength(double)} is used as a multiplier.
+     * 
+     * @return this.
+     */
+    public KConstrainedFDLayouter withIndividualEdgeLengths() {
+        this.individualEdgeLengths = true;
+        return this;
+    }
+
+    /**
+     * @param test
+     *            a subclass of {@link TestConvergence}.
+     * @return this.
+     */
+    public KConstrainedFDLayouter withConvergenceTest(final TestConvergence test) {
+        this.convergenceTest = test;
+        return this;
+    }
+
+    /**
+     * Mainly for debugging purposes. Dont use this if you dont know what it means :).
+     * 
+     * @param xPtrrs
+     *            .
+     * @param yPtrrs
+     *            .
+     * @return this.
+     */
+    public KConstrainedFDLayouter withUnsatisfiableConstraintInfos(
+            final UnsatisfiableConstraintInfoPtrs xPtrrs,
+            final UnsatisfiableConstraintInfoPtrs yPtrrs) {
+        this.xPtrs = xPtrrs;
+        this.yPtrs = yPtrrs;
+        return this;
+    }
+
+    /**
+     * Instantiates an internal layouter object and configures it appropriately.
+     */
+    public void prepare() {
+
+        if (graph == null) {
+            throw new IllegalStateException("Cannot execute layouter without specified cgraph.");
+        } else if (layouter != null) {
+            throw new IllegalStateException("Layouter has already been prepared.");
+        }
+
+        if (convergenceTest != null && individualEdgeLengths) {
+            // use the test and specified edge lengths
+            layouter =
+                    new ConstrainedFDLayout(graph.nodes, graph.edges, idealEdgeLength,
+                            overlapPrevention, graph.getIdealEdgeLengths(), convergenceTest);
+        } else if (convergenceTest != null) {
+            // use the test but add a default array for individual edge lengths
+            Doubles ones = new Doubles();
+            for (int i = 0; i < graph.edges.size(); ++i) {
+                ones.add(1);
+            }
+            layouter =
+                    new ConstrainedFDLayout(graph.nodes, graph.edges, idealEdgeLength,
+                            overlapPrevention, ones, convergenceTest);
+        } else if (individualEdgeLengths) {
+            layouter =
+                    new ConstrainedFDLayout(graph.nodes, graph.edges, idealEdgeLength,
+                            overlapPrevention, graph.getIdealEdgeLengths());
+        } else {
+            layouter =
+                    new ConstrainedFDLayout(graph.nodes, graph.edges, idealEdgeLength,
+                            overlapPrevention);
+        }
+
+        layouter.setClusterHierarchy(graph.rootCluster);
+        layouter.setConstraints(graph.constraints);
+
+        // debugging
+        if (xPtrs != null && yPtrs != null) {
+            layouter.setUnsatisfiableConstraintInfo(xPtrs, yPtrs);
+        }
+    }
+
+    /**
+     * Runs the layouter, if {@link #prepare()} has not been called beforehand this is done here.
+     */
+    public void run() {
+
+        if (layouter == null) {
+            // prepare
+            prepare();
+        }
+
+        layouter.run();
+
+        // FIXME see javadoc of the class
+        // layouter.freeAssociatedObjects();
+    }
+
+    /**
+     * For debugging purposes only! Do <b>not</b> call run manually on the layouter object.
+     * 
+     * @return the layouter
+     */
+    public ConstrainedFDLayout getLayouter() {
+        return layouter;
+    }
+
+}
