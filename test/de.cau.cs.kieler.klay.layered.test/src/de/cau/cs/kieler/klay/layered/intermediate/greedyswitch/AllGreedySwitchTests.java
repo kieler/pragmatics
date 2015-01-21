a7aa87d2b87ecdbc8f12b98e0e144f9d9b239e6a
Alan Schelten <stu111405@mail.uni-kiel.de>
2015-01-21 17:11:31 +0100

    Test Suite added.

---
+++ test/de.cau.cs.kieler.klay.layered.test/src/de/cau/cs/kieler/klay/layered/intermediate/greedyswitch/AllGreedySwitchTests.java
@@ -0,0 +1,12 @@
+package de.cau.cs.kieler.klay.layered.intermediate.greedyswitch;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CrossingCounterTest.class, DoublyLinkeHashSetTest.class, SwitchDeciderTest.class,
+        TwoNodeInLayerEdgeCrossingCounterTest.class, TwoNodeTwoLayerCrossingCounterTest.class })
+public class AllGreedySwitchTests {
+
+}
