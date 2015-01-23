4662e9631b3746bb7532bd017ba1f7e0f645368a
Ulf Rueegg <uru@informatik.uni-kiel.de>
2015-01-23 10:58:21 +0100

    kiml.formats.gml: started refactoring towards new gml parser

--- plugins/de.cau.cs.kieler.kiml.formats.gml/src/de/cau/cs/kieler/kiml/formats/gml/GmlFormatHandler.java
+++ plugins/de.cau.cs.kieler.kiml.formats.gml/src/de/cau/cs/kieler/kiml/formats/gml/GmlFormatHandler.java
@@ -3,7 +3,7 @@
  *
  * http://www.informatik.uni-kiel.de/rtsys/kieler/
  * 
- * Copyright 2011 by
+ * Copyright 2015 by
  * + Christian-Albrechts-University of Kiel
  *   + Department of Computer Science
  *     + Real-Time and Embedded Systems Group
@@ -13,108 +13,46 @@
  */
 package de.cau.cs.kieler.kiml.formats.gml;
 
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.xtext.resource.XtextResourceSet;
-
-import com.google.inject.Inject;
-import com.google.inject.Provider;
-
-import de.cau.cs.kieler.core.kgraph.KLabel;
 import de.cau.cs.kieler.core.kgraph.KNode;
-import de.cau.cs.kieler.kiml.formats.AbstractEmfHandler;
+import de.cau.cs.kieler.kiml.formats.IGraphFormatHandler;
 import de.cau.cs.kieler.kiml.formats.IGraphTransformer;
-import de.cau.cs.kieler.kiml.formats.gml.gml.Element;
-import de.cau.cs.kieler.kiml.formats.gml.gml.GmlFactory;
-import de.cau.cs.kieler.kiml.formats.gml.gml.GmlModel;
-import de.cau.cs.kieler.kiml.klayoutdata.KPoint;
+import de.cau.cs.kieler.kiml.formats.TransformationData;
 
 /**
- * A transformer for GML.
- *
- * @author msp
- * @kieler.design proposed by msp
- * @kieler.rating proposed yellow by msp
+ * @author uru
  */
-public class GmlFormatHandler extends AbstractEmfHandler<GmlModel> {
-    
-    /** Identifier of the GML format. */
-    public static final String ID = "de.uni-passau.fim.gml";
-    
-    @Inject private Provider<XtextResourceSet> resourceSetProvider;
+public class GmlFormatHandler implements IGraphFormatHandler<GMLModel> {
 
     /**
      * {@inheritDoc}
      */
-    @Override
-    protected ResourceSet createResourceSet() {
-        return resourceSetProvider.get();
+    public void deserialize(String serializedGraph, TransformationData<GMLModel, KNode> transData) {
+        // TODO Auto-generated method stub
     }
-    
-    private GmlImporter importer = new GmlImporter();
 
     /**
      * {@inheritDoc}
      */
-    public IGraphTransformer<GmlModel, KNode> getImporter() {
-        return importer;
+    public String serialize(TransformationData<KNode, GMLModel> transData) {
+        // TODO Auto-generated method stub
+        return null;
     }
-    
-    private GmlExporter exporter = new GmlExporter();
 
     /**
      * {@inheritDoc}
      */
-    public IGraphTransformer<KNode, GmlModel> getExporter() {
-        return exporter;
-    }
-    
-    /**
-     * Create a GML point for a KPoint.
-     * 
-     * @param point a KPoint
-     * @return a GML element with the coordinates of the given point
-     */
-    public static Element createPoint(final KPoint point) {
-        Element p = GmlFactory.eINSTANCE.createElement();
-        p.setKey("point");
-        Element x = GmlFactory.eINSTANCE.createElement();
-        x.setKey("x");
-        x.setValue(Float.toString(point.getX()));
-        p.getElements().add(x);
-        Element y = GmlFactory.eINSTANCE.createElement();
-        y.setKey("y");
-        y.setValue(Float.toString(point.getY()));
-        p.getElements().add(y);
-        return p;
-    }
-    
-    /** the maximal allowed character value for GML. */
-    private static final char MAX_CHAR = 127;
-    
-    /**
-     * Create a GML label for a KLabel.
-     * 
-     * @param klabel a KLabel
-     * @return a GML element with the label text
-     */
-    public static Element createLabel(final KLabel klabel) {
-        Element l = GmlFactory.eINSTANCE.createElement();
-        l.setKey("label");
-        StringBuilder text = new StringBuilder(klabel.getText());
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (c == '\"') {
-                text.replace(i, i + 1, "&quot;");
-            } else if (c == '&') {
-                text.replace(i, i + 1, "&amp;");
-            } else if (c > MAX_CHAR) {
-                text.replace(i, i + 1, "&#" + (int) c + ";");
-            }
-        }
-        text.insert(0, '\"');
-        text.append('\"');
-        l.setValue(text.toString());
-        return l;
+    public IGraphTransformer<GMLModel, KNode> getImporter() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public IGraphTransformer<KNode, GMLModel> getExporter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
 }
