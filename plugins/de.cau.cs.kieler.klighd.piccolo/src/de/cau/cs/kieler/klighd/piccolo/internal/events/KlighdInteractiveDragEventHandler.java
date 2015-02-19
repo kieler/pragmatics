c0272eef5ee13ebb5802f12a4fefdf7ab725b4e1
Malte Kruse <stu113808@mail.uni-kiel.de>
2015-02-19 16:26:42 +0100

    klighd.piccolo: fixed wrong (confused) super class + formatting

--- plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/internal/events/KlighdInteractiveDragEventHandler.java
+++ plugins/de.cau.cs.kieler.klighd.piccolo/src/de/cau/cs/kieler/klighd/piccolo/internal/events/KlighdInteractiveDragEventHandler.java
@@ -17,14 +17,13 @@ import java.awt.geom.Dimension2D;
 import java.awt.geom.Rectangle2D;
 
 import de.cau.cs.kieler.klighd.KlighdConstants;
-import de.cau.cs.kieler.klighd.LightDiagramServices;
 import de.cau.cs.kieler.klighd.piccolo.KlighdSWTGraphics;
 import de.cau.cs.kieler.klighd.piccolo.internal.nodes.KNodeNode;
 import de.cau.cs.kieler.klighd.piccolo.internal.util.KlighdPaintContext;
 import de.cau.cs.kieler.klighd.piccolo.internal.util.NodeUtil;
 import de.cau.cs.kieler.klighd.piccolo.viewer.PiccoloViewer;
 import edu.umd.cs.piccolo.PNode;
-import edu.umd.cs.piccolo.event.PDragEventHandler;
+import edu.umd.cs.piccolo.event.PDragSequenceEventHandler;
 import edu.umd.cs.piccolo.event.PInputEvent;
 import edu.umd.cs.piccolo.util.PAffineTransform;
 import edu.umd.cs.piccolo.util.PPaintContext;
@@ -33,7 +32,7 @@ import edu.umd.cs.piccolo.util.PPaintContext;
  *
  * @author chsch
  */
-public class KlighdInteractiveDragEventHandler extends PDragEventHandler {
+public class KlighdInteractiveDragEventHandler extends PDragSequenceEventHandler {
 
     private static final int HALF_OPACITY = KlighdConstants.ALPHA_FULL_OPAQUE / 2;
 
@@ -44,72 +43,81 @@ public class KlighdInteractiveDragEventHandler extends PDragEventHandler {
     /**
      * Constructor.
      *
-     * @param pViewer the {@link PiccoloViewer} this handler is attached to
+     * @param pViewer
+     *            the {@link PiccoloViewer} this handler is attached to
      */
     public KlighdInteractiveDragEventHandler(final PiccoloViewer pViewer) {
-            this.pViewer = pViewer;
+        this.pViewer = pViewer;
     }
 
     @Override
     protected boolean shouldStartDragInteraction(final PInputEvent event) {
-            return event.isShiftDown() && super.shouldStartDragInteraction(event);
+        return (event.getPickedNode() instanceof KNodeNode) && event.isShiftDown()
+                && super.shouldStartDragInteraction(event);
     }
 
     @Override
     protected void startDrag(final PInputEvent event) {
-            super.startDrag(event);
 
-            final KNodeNode nodeNode = (KNodeNode) event.getPickedNode();
-            final Rectangle2D bounds = nodeNode.getFullBounds();
+        super.startDrag(event);
 
-            final PAffineTransform invertedNodeNodeTransform = new PAffineTransform(
-                            NodeUtil.invert(nodeNode.getTransformReference(true)));
-            invertedNodeNodeTransform.transform(bounds, bounds);
+        final KNodeNode nodeNode = (KNodeNode) event.getPickedNode();
+        final Rectangle2D bounds = nodeNode.getFullBounds();
 
-            node = new PNode() {
+        final PAffineTransform invertedNodeNodeTransform =
+                new PAffineTransform(NodeUtil.invert(nodeNode.getTransform()));
+        invertedNodeNodeTransform.transform(bounds, bounds);
 
-                    private static final long serialVersionUID = -3737436221817065320L;
+        node = new PNode() {
 
-                    @Override
-                    public void fullPaint(final PPaintContext paintContext) {
+            private static final long serialVersionUID = -3737436221817065320L;
+
+            @Override
+            public void fullPaint(final PPaintContext paintContext) {
                 paintContext.pushTransform(getTransformReference(false));
 
                 final KlighdPaintContext pc = (KlighdPaintContext) paintContext;
-                            final KlighdSWTGraphics graphics = pc.getKlighdGraphics();
-                            final int alpha = graphics.getAlpha();
+                final KlighdSWTGraphics graphics = pc.getKlighdGraphics();
+                final int alpha = graphics.getAlpha();
 
-                            graphics.setAlpha(HALF_OPACITY);
+                graphics.setAlpha(HALF_OPACITY);
 
-                            paintContext.pushTransform(invertedNodeNodeTransform);
-                            nodeNode.fullPaint(paintContext);
+                paintContext.pushTransform(invertedNodeNodeTransform);
+                nodeNode.fullPaint(paintContext);
 
-                            paintContext.popTransform(invertedNodeNodeTransform);
+                paintContext.popTransform(invertedNodeNodeTransform);
 
-                            graphics.setAlpha(alpha);
+                graphics.setAlpha(alpha);
                 paintContext.popTransform(getTransformReference(false));
-                    };
             };
-            node.setBounds(bounds);
-            node.setTransform(event.getPath().getPathTransformTo(nodeNode));
+        };
+        node.setBounds(bounds);
+        node.setTransform(event.getPath().getPathTransformTo(nodeNode));
 
-            event.getTopCamera().addChild(node);
-            event.setHandled(true);
+        event.getTopCamera().addChild(node);
+        event.setHandled(true);
     }
 
     @Override
     protected void drag(final PInputEvent event) {
-            final Dimension2D d = event.getDelta();
-            node.translate(d.getWidth(), d.getHeight());
-            super.drag(event);
-            event.setHandled(true);
+        super.drag(event);
+
+        final Dimension2D d = event.getDelta();
+        node.translate(d.getWidth(), d.getHeight());
+
+        event.setHandled(true);
     }
 
     @Override
     protected void endDrag(final PInputEvent event) {
-            super.endDrag(event);
-            node.removeFromParent();
+        super.endDrag(event);
+        node.removeFromParent();
 
-            LightDiagramServices.layoutDiagram(pViewer.getViewContext());
-            event.setHandled(true);
+//        pViewer.getControl().getDisplay().asyncExec(new Runnable() {
+//            public void run() {
+//                LightDiagramServices.layoutDiagram(pViewer.getViewContext());
+//            }
+//        });
+        event.setHandled(true);
     }
 }
