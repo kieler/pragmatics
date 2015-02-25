87e203b6c5281859c3ab6f9773bd2f590ad99220
Ulf Rueegg <uru@informatik.uni-kiel.de>
2015-02-25 13:20:14 +0100

    klay.layered.nodeplace: improved optimal node placement

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/p4nodes/OptiPlacer.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/p4nodes/OptiPlacer.java
@@ -24,6 +24,7 @@ import com.google.common.io.CharStreams;
 
 import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
 import de.cau.cs.kieler.core.util.Pair;
+import de.cau.cs.kieler.kiml.options.LayoutOptions;
 import de.cau.cs.kieler.klay.layered.ILayoutPhase;
 import de.cau.cs.kieler.klay.layered.IntermediateProcessingConfiguration;
 import de.cau.cs.kieler.klay.layered.graph.LEdge;
@@ -35,6 +36,7 @@ import de.cau.cs.kieler.klay.layered.intermediate.IntermediateProcessorStrategy;
 import de.cau.cs.kieler.klay.layered.properties.GraphProperties;
 import de.cau.cs.kieler.klay.layered.properties.InternalProperties;
 import de.cau.cs.kieler.klay.layered.properties.NodeType;
+import de.cau.cs.kieler.klay.layered.properties.Properties;
 import de.cau.cs.kieler.klay.layered.solver.AbstractCPLEXModel;
 import de.cau.cs.kieler.klay.layered.solver.ISolverModel;
 import de.cau.cs.kieler.klay.layered.solver.ModelRunner;
@@ -141,14 +143,19 @@ public class OptiPlacer implements ILayoutPhase {
             // nodes
             int[] n_height = new int[N];
             int n_cnt = 0;
+            int upperBoundHeight = 0;
 
             // ports
             int[] p = new int[P];
-            float[] p_offset = new float[P];
+            int[] p_offset = new int[P];
             int p_cnt = 0;
 
             // edges
-            int[][] e = new int[P][P];
+            int E = 0;
+            int[][] edges = new int[P][4];
+            
+            int Eprime = 0;
+            int[][] segments = new int[P][P];
             List<Pair<Integer, Integer>> longEdgeDummyPairs = Lists.newArrayList();
 
             for (Layer l : layeredGraph.getLayers()) {
@@ -162,11 +169,17 @@ public class OptiPlacer implements ILayoutPhase {
                 for (LNode n : l.getNodes()) {
                     n.id = n_cnt++;
                     n_height[n.id] = (int) n.getSize().y;
+                    upperBoundHeight += Math.ceil(n.getSize().y);
 
                     for (LPort po : n.getPorts()) {
+                        
+                        // FIXME we don't like float port positions
+                        po.getAnchor().y = 0;
+                        po.getPosition().y = Math.ceil(po.getPosition().y); 
+                        
                         po.id = p_cnt++;
                         p[po.id] = po.getNode().id + 1;
-                        p_offset[po.id] = (float) po.getPosition().y;
+                        p_offset[po.id] = (int) po.getPosition().y;
                     }
                 }
             }
@@ -176,9 +189,11 @@ public class OptiPlacer implements ILayoutPhase {
             for (Layer l : layeredGraph.getLayers()) {
                 for (LNode n : l.getNodes()) {
                     for (LPort po : n.getPorts()) {
-                        int[] src = e[po.id];
+                        int[] src = segments[po.id];
                         for (LEdge edge : po.getOutgoingEdges()) {
+                            Eprime++;
                             src[edge.getTarget().id] = 1;
+                            //System.out.println("Segment: " + edge);
                             
                             // record long edge dummies
                             if (edge.getSource().getNode().getProperty(InternalProperties.NODE_TYPE) 
@@ -193,10 +208,66 @@ public class OptiPlacer implements ILayoutPhase {
                     }
                 }
             }
+            
+            // collect real edges. That is from real node to real node
+            for (Layer l : layeredGraph) {
+                for (LNode n : l) {
+                    if (n.getProperty(InternalProperties.NODE_TYPE) != NodeType.NORMAL
+                            && n.getProperty(InternalProperties.NODE_TYPE) != NodeType.EXTERNAL_PORT) {
+                        continue;
+                    }
+                    for (LPort po : n.getPorts()) {
+                        for (LEdge edge : po.getOutgoingEdges()) {
 
+                            // find the target
+                            LPort target = edge.getTarget();
+                            LEdge dummyEdge = edge;
+                            while (target.getNode().getProperty(InternalProperties.NODE_TYPE) != NodeType.NORMAL
+                                    && target.getNode().getProperty(InternalProperties.NODE_TYPE) != NodeType.EXTERNAL_PORT) {
+                                
+                                boolean found = false;
+                                for (LEdge tmpEdge : target.getNode().getOutgoingEdges()) {
+                                    dummyEdge = tmpEdge;
+                                    target = tmpEdge.getTarget();
+                                    found = true;
+                                }
+                                
+                                if (!found) {
+                                throw new IllegalStateException("Could not infer original edge. "
+                                        + " Found a "
+                                        + target.getNode()
+                                                .getProperty(InternalProperties.NODE_TYPE));
+                                }
+                            }
+                            
+                            // SUPPRESS CHECKSTYLE NEXT 10 MagicNumber
+                            int[] src = edges[E];
+                            E++;
+                            src[0] = edge.getSource().id + 1;
+                            src[1] = edge.getTarget().id + 1;
+                            src[2] = dummyEdge.getSource().id + 1;
+                            src[3] = dummyEdge.getTarget().id + 1;
+                            
+                            // System.out.println("Edge: " + po.getNode() + " " + target.getNode());
+                        }
+                    }
+                }
+            }
+            
+            float nodeSpacing = layeredGraph.getProperty(Properties.OBJ_SPACING)
+                    * layeredGraph.getProperty(Properties.OBJ_SPACING_IN_LAYER_FACTOR);
+            float edgeSpacing = nodeSpacing * layeredGraph.getProperty(Properties.EDGE_SPACING_FACTOR);
+            
+            // upper bound on height
+            upperBoundHeight += (N - 1) * nodeSpacing; 
+            
             sb.append("N = " + N + ";");
             sb.append("\nP = " + P + ";");
             sb.append("\nL = " + L + ";");
+            
+            sb.append("\nnodeSpacing = " + (int) nodeSpacing + ";");
+            sb.append("\nedgeSpacing = " + (int) edgeSpacing + ";");
+            sb.append("\nupperBoundHeight = " + upperBoundHeight + ";");
 
             sb.append("\n\nn_height = " + Arrays.toString(n_height) + ";");
             sb.append("\np_offset = " + Arrays.toString(p_offset) + ";");
@@ -205,15 +276,30 @@ public class OptiPlacer implements ILayoutPhase {
 
             sb.append("\n\np = " + Arrays.toString(p) + ";");
 
-            sb.append("\n\ne = [");
+            sb.append("\n\nE = " + E + ";");
+            sb.append("\nedges = [");
             int i = 0;
-            for (int[] arr : e) {
+            //for (int[] arr : edges) {
+            for (int k = 0; k < E; k++) {
+                int[] arr = edges[k];
+                sb.append("\n\t" + Arrays.toString(arr));
+                if (i++ < E - 1) {
+                    sb.append(",");
+                }
+            }
+            sb.append("];");
+            
+            sb.append("\n\nEprime = " + Eprime + ";");
+            sb.append("\nsegments = [");
+            i = 0;
+            for (int[] arr : segments) {
                 sb.append("\n\t" + Arrays.toString(arr));
                 if (i++ < P - 1) {
                     sb.append(",");
                 }
             }
             sb.append("];");
+            
             
             sb.append("\n\nLD = " + longEdgeDummyPairs.size() + ";");
             sb.append("\nd = [");
@@ -239,6 +325,7 @@ public class OptiPlacer implements ILayoutPhase {
             int minPos = Integer.MAX_VALUE;
             boolean start = false;
             for (String line : lines) {
+                // System.out.println(line);
                 if (line.startsWith("Done")) {
                     break;
                 }
