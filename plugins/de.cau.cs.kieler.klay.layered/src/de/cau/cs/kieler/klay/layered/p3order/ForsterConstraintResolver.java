bed0e1c258257118e04e1a2fc184c5ad29d6a549
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2015-03-23 14:20:16 +0100

    klay.layered: - Fixed a floating point comparison problem with active assertions.

--- plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/p3order/ForsterConstraintResolver.java
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/p3order/ForsterConstraintResolver.java
@@ -188,6 +188,9 @@ public final class ForsterConstraintResolver implements IConstraintResolver {
         return null;
     }
 
+    /** Delta that two barycenters can differ by to still be considered equal. */
+    private static final float BARYCENTER_EQUALITY_DELTA = 0.0001F;
+    
     /**
      * Handles the case of a violated constraint. The node groups must be sorted by their
      * barycenter values. After this method has finished, the list of node groups is smaller
@@ -207,8 +210,8 @@ public final class ForsterConstraintResolver implements IConstraintResolver {
         // Create a new vertex from the two constrain-violating vertices; this also
         // automatically calculates the new vertex's barycenter value
         NodeGroup newNodeGroup = new NodeGroup(firstNodeGroup, secondNodeGroup);
-        assert newNodeGroup.barycenter >= secondNodeGroup.barycenter;
-        assert newNodeGroup.barycenter <= firstNodeGroup.barycenter;
+        assert newNodeGroup.barycenter + BARYCENTER_EQUALITY_DELTA >= secondNodeGroup.barycenter;
+        assert newNodeGroup.barycenter - BARYCENTER_EQUALITY_DELTA <= firstNodeGroup.barycenter;
 
         // Iterate through the vertices. Remove the old vertices. Insert the new one
         // according to the barycenter value, thereby keeping the list sorted. Along
