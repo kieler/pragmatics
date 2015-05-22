85c611bfdd3f9337c8fbecbbf02f7635d5367d2e
Marc Adolf <mad@informatik.uni-kiel.de>
2015-05-22 15:39:18 +0200

    implemented the estimated help-functions and improves the code with the pair structure

---
+++ plugins/de.cau.cs.kieler.klay.layered/src/de/cau/cs/kieler/klay/layered/p2layers/StretchWidth.java
@@ -0,0 +1,261 @@
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+ *
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2015 by
+ * + Christian-Albrechts-University of Kiel
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
+package de.cau.cs.kieler.klay.layered.p2layers;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import de.cau.cs.kieler.core.alg.IKielerProgressMonitor;
+import de.cau.cs.kieler.core.util.Pair;
+import de.cau.cs.kieler.klay.layered.ILayoutPhase;
+import de.cau.cs.kieler.klay.layered.IntermediateProcessingConfiguration;
+import de.cau.cs.kieler.klay.layered.graph.LEdge;
+import de.cau.cs.kieler.klay.layered.graph.LGraph;
+import de.cau.cs.kieler.klay.layered.graph.LNode;
+import de.cau.cs.kieler.klay.layered.graph.Layer;
+
+/**
+ * @author marc
+ *
+ */
+public class StretchWidth implements ILayoutPhase {
+    // indicates the width of the currently buidl layer
+    private int widthCurrent = 0;
+    // estimated width of the next layer
+    private int widthUp = 0;
+    // indicates the width of the layering
+    // will be changed dynamically increased if the layering fails for lower values
+    // pseudo codes says initialize with 0 but i think you can start with 1 or mb higher/average
+    // out-degree
+    private int maxWidth = 0;
+    // the Graph the layering is done for
+    private LGraph graph;
+    // average out-degree
+    private int avOut;
+    // sorted list of layerless nodes
+    private List<LNode> sortedLayerlessNodes;
+    // Set of nodes placed in the current layer
+    private HashSet<LNode> u = new HashSet<LNode>();
+    // Set of nodes in all layers except the current
+    private HashSet<LNode> z = new HashSet<LNode>();
+
+    List<LNode> tempLayerlessNodes;
+
+    /**
+     * {@inheritDoc}
+     */
+
+    public IntermediateProcessingConfiguration getIntermediateProcessingConfiguration(
+            final LGraph graph) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void process(final LGraph layeredGraph, final IKielerProgressMonitor progressMonitor) {
+        /*
+         * pseudo code Requires DAG G=(V,E) U={} Z={} currentLayer = 1; widthCurrent=0;widthUp=0;
+         * maxWidth=0;
+         */
+        // while U!=V do
+        // Select node v€V\U with N⁺_g(v) subset of Z and ConditionSelect
+        // if no node has been selected or (ConditionGoUp and U\Z != {}) then
+        // move to the upper layer
+        // currentLayer = currentLayer+1;
+        // Z=Union(Z,U);
+        // widthCurrent=widthUp;
+        // widthUp=0;
+        // else if ConditionGoUp then
+        // /*reset layering */
+        // currentLayer=1;
+        // widthCurrent=0;
+        // widthUp=0; U={} Z={}
+        // maxWidth=maxWidth+1;
+        // else
+        // /*add node to current layer*/
+        // Assign v to the layer with a number currentLayer
+        // U=Union(U,{v})
+        // widthCurrent=withCurrent-d⁺(v)+1;
+        // widthUp=widthUp+d⁻(v)
+        // end if
+        // end if
+        // end while
+
+        // set graph
+        graph = layeredGraph;
+        // Layer currently worked on
+        Layer currentLayer = new Layer(graph);
+        // compute average out-degree
+        computeAverageDegree();
+        // sort the nodes at beginning, since the rank will not change
+        computeSortedNodes();
+        // copy the sorted layerless nodes so we don't overwrite it for the reset case
+        tempLayerlessNodes = Lists.newArrayList(sortedLayerlessNodes);
+        LNode selectedNode;
+        // variable to compute the difference of u and z
+        HashSet<LNode> diff;
+        List<Layer> layering = graph.getLayers();
+
+        while (!tempLayerlessNodes.isEmpty()) {
+            // TODO SelectNode
+            selectedNode = selectNode();
+            // compute u\z
+            diff = new HashSet<LNode>(u);
+            diff.removeAll(z);
+            if (selectedNode == null || (conditionGoUp() && !diff.isEmpty())) {
+                /* go to the next layer */
+                // TODO
+            } else {
+                if (conditionGoUp()) {
+                    /* reset layering */
+
+                    // TODO
+                } else {
+                    /* add node to current layer */
+                    // TODO
+                }
+            }
+
+        }
+
+        // layering done, delete original layerlessNodes
+        layeredGraph.getLayerlessNodes().clear();
+        // Algorithm is Bottom-Up -> reverse Layers
+        java.util.Collections.reverse(layeredGraph.getLayers());
+    }
+
+    /**
+     * Checks if the layering should go up to the next layer.
+     * 
+     * @return
+     */
+    private Boolean conditionGoUp() {
+        return widthCurrent > maxWidth || (widthUp > maxWidth * avOut);
+    }
+
+    /**
+     * .
+     * 
+     * @return
+     */
+    private LNode selectNode() {
+        List<LNode> accSucc = Lists.newArrayList();
+        for (LNode node : tempLayerlessNodes) {
+            for (LEdge edge : node.getOutgoingEdges()) {
+                accSucc.add(edge.getTarget().getNode());
+            }
+            // if all successors of node are n z, choose this node, since the list is sorted by rank
+            if (z.containsAll(accSucc)) {
+                tempLayerlessNodes.remove(node);
+                return node;
+            }
+            accSucc.clear();
+
+        }
+        // if no candidate was found return null?
+        return null;
+    }
+
+    /**
+     * sorts a list of nodes after its rank. the rank is defined as max(d⁺(v),max(d⁺(u):(u,v)∈ E))
+     * 
+     * 
+     */
+    private void computeSortedNodes() {
+        List<LNode> unsortedNodes = graph.getLayerlessNodes();
+        List<Pair<LNode, Integer>> nodesWithRank = Lists.newArrayList();
+        sortedLayerlessNodes = Lists.newArrayList();
+        // didn't find something useful to Sort a Map by its value, so I used the Pair-construct
+        for (LNode node : unsortedNodes) {
+            nodesWithRank.add(new Pair<LNode, Integer>(node, getRank(node)));
+        }
+        // sorting the Pair after its value
+        java.util.Collections.sort(nodesWithRank, new Comparator<Pair<LNode, Integer>>() {
+            // TODO maybe implement the comparator in the Pair-class
+            public int compare(final Pair<LNode, Integer> o1, final Pair<LNode, Integer> o2) {
+                // descending sort
+                if ((Integer) o1.getSecond() < (Integer) o2.getSecond()) {
+                    return -1;
+                } else if ((Integer) o1.getSecond() > (Integer) o2.getSecond()) {
+                    return 1;
+                }
+                return 0;
+            }
+        });
+        // only the nodes are needed now
+        for (Pair<LNode, Integer> pair : nodesWithRank) {
+            sortedLayerlessNodes.add(pair.getFirst());
+        }
+
+    }
+
+    /**
+     * Computes the rank of a node.
+     * 
+     * @param node
+     *            node to compute the rank for
+     * @return rank of the node
+     */
+    private Integer getRank(final LNode node) {
+        // TODO
+        int max = getOutDegree(node);
+        int temp;
+        LNode pre;
+
+        // compute max of predecessors out-degree and
+        for (LEdge preEdge : node.getIncomingEdges()) {
+            pre = preEdge.getSource().getNode();
+            temp = getOutDegree(pre);
+            Math.max(max, temp);
+        }
+
+        return max;
+    }
+
+    /**
+     * Computes the out-degree of a node.
+     * 
+     * @param node
+     *            node to compute the out-degree for
+     * @return out-degree of the node
+     */
+    private Integer getOutDegree(final LNode node) {
+        // TODO geht das?
+        int i = 0;
+        while (node.getOutgoingEdges().iterator().hasNext()) {
+            i++;
+        }
+        return i;
+    }
+
+    /**
+     * computes the average out-degree of the graph.
+     */
+    private void computeAverageDegree() {
+        int nodes = 0;
+        int edges = 0;
+        for (LNode node : graph.getLayerlessNodes()) {
+            nodes++;
+            edges += getOutDegree(node);
+
+        }
+        avOut = edges / nodes;
+
+    }
+
+}
