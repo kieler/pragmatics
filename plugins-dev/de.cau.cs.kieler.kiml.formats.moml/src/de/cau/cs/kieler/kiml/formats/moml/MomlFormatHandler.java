8049bfd24c84a2028ad411d180b07e352711ee73
Ulf Rueegg <uru@informatik.uni-kiel.de>
2015-06-04 22:31:22 +0200

    kiml.formats.moml: corrected plugin name

--- plugins-dev/de.cau.cs.kieler.kiml.formats.moml/src/de/cau/cs/kieler/kiml/formats/moml/MomlFormatHandler.java
+++ plugins-dev/de.cau.cs.kieler.kiml.formats.moml/src/de/cau/cs/kieler/kiml/formats/moml/MomlFormatHandler.java
@@ -11,7 +11,7 @@
  * This code is provided under the terms of the Eclipse Public License (EPL).
  * See the file epl-v10.html for the license text.
  */
-package de.cau.cs.kieler.kiml.format.moml;
+package de.cau.cs.kieler.kiml.formats.moml;
 
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
