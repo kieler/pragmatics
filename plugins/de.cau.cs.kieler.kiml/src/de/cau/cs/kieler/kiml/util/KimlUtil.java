95319d3fac802bec26b4874dea63a22a32efea0e
Christoph Daniel Schulze <cds@informatik.uni-kiel.de>
2015-06-18 17:21:11 +0200

    kiml: - Fixed the fix of the ConcurrentModificationException while loading data elements in a Graph. (KIPRA-1541)
    core.kgraph.text, kiml, kiml.formats, kiml.grana:
    - Changed calls of KimlUtil.loadDataElements(...) to new versions that allow to clear properties beforehand.

--- plugins/de.cau.cs.kieler.kiml/src/de/cau/cs/kieler/kiml/util/KimlUtil.java
+++ plugins/de.cau.cs.kieler.kiml/src/de/cau/cs/kieler/kiml/util/KimlUtil.java
@@ -21,8 +21,13 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.emf.common.util.AbstractTreeIterator;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.EContentsEList;
+import org.eclipse.emf.ecore.util.EContentsEList.FeatureFilter;
+import org.eclipse.emf.ecore.util.EContentsEList.FeatureIteratorImpl;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
@@ -38,6 +43,7 @@ import de.cau.cs.kieler.core.kgraph.KEdge;
 import de.cau.cs.kieler.core.kgraph.KGraphData;
 import de.cau.cs.kieler.core.kgraph.KGraphElement;
 import de.cau.cs.kieler.core.kgraph.KGraphFactory;
+import de.cau.cs.kieler.core.kgraph.KGraphPackage;
 import de.cau.cs.kieler.core.kgraph.KLabel;
 import de.cau.cs.kieler.core.kgraph.KLabeledGraphElement;
 import de.cau.cs.kieler.core.kgraph.KNode;
@@ -707,6 +713,19 @@ public final class KimlUtil {
     }
     
     /**
+     * Calls {@link #loadDataElements(KNode, boolean, IProperty...)} with {@code clearProperties} set to
+     * {@code false}.
+     * 
+     * @param graph
+     *            the root element of the graph to load elements of.
+     * @param knownProps
+     *            a set of additional properties that are known, hence should be parsed properly
+     */
+    public static void loadDataElements(final KNode graph, final IProperty<?>... knownProps) {
+        loadDataElements(graph, PREDICATE_IS_KLAYOUTDATA, false, knownProps);
+    }
+    
+    /**
      * Loads all {@link de.cau.cs.kieler.core.properties.IProperty IProperty} of {@link KLayoutData}
      * elements of a KGraph by deserializing {@link PersistentEntry} tuples. Values are parsed using
      * layout option data obtained from the {@link LayoutMetaDataService}. Options that cannot be
@@ -715,12 +734,18 @@ public final class KimlUtil {
      * 
      * @param graph
      *            the root element of the graph to load elements of.
+     * @param clearProperties
+     *            {@code true} if the properties of a property holder should be cleared before
+     *            repopulating them based on persistent entries. Required if the removal of a persistent
+     *            entry should result in the removal of the correspondind property. This is for example
+     *            the case in our Xtext-based KGT editor.
      * @param knownProps
      *            a set of additional properties that are known, hence should be parsed properly
      */
-    public static void loadDataElements(final KNode graph,
+    public static void loadDataElements(final KNode graph, final boolean clearProperties,
             final IProperty<?>... knownProps) {
-        loadDataElements(graph, PREDICATE_IS_KLAYOUTDATA, knownProps);
+        
+        loadDataElements(graph, PREDICATE_IS_KLAYOUTDATA, clearProperties, knownProps);
     }
 
     /**
@@ -733,6 +758,68 @@ public final class KimlUtil {
                     return input instanceof KLayoutData;
                 }
             };
+            
+    /**
+     * Calls {@link #loadDataElements(KNode, Predicate, boolean, IProperty...)} with
+     * {@code clearProperties} set to {@code false}.
+     * 
+     * @param graph
+     *            the root element of the graph to load elements of.
+     * @param handledTypes
+     *            a predicate checking if we desire to load data elements for a certain subclass of
+     *            {@link EMapPropertyHolder}.
+     * @param knownProps
+     *            a set of additional properties that are known, hence should be parsed properly.
+     * 
+     * @return the graph itself
+     */
+    public static KNode loadDataElements(final KNode graph,
+            final Predicate<EMapPropertyHolder> handledTypes, final IProperty<?>... knownProps) {
+
+        return loadDataElements(graph, handledTypes, false, knownProps);
+    }
+    
+    /**
+     * A tree iterator that skips properties of {@link EMapPropertyHolder}s. For an explanation of
+     * why this is necessary, see the implementation of
+     * {@link KimlUtil#loadDataElements(KNode, Predicate, boolean, IProperty...)}.
+     * 
+     * @author cds
+     */
+    private static class PropertiesSkippingTreeIterator extends AbstractTreeIterator<EObject> {
+        /** Bogus serial version ID. */
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * {@inheritDoc}.
+         */
+        public PropertiesSkippingTreeIterator(final Object object, final boolean includeRoot) {
+            super(object, includeRoot);
+        }
+
+        @Override
+        protected Iterator<? extends EObject> getChildren(final Object object) {
+            // We know that the object is an EObject; get an iterator over its content
+            Iterator<EObject> iterator = ((EObject) object).eContents().iterator();
+            
+            // The iterator will usually be a FeatureIteratorImpl that we can set a feature filter on
+            if (iterator instanceof FeatureIteratorImpl) {
+                ((FeatureIteratorImpl<EObject>) iterator).filter(new FeatureFilter() {
+                    public boolean isIncluded(final EStructuralFeature eStructuralFeature) {
+                        // We include everything but properties (layout options)
+                        if (eStructuralFeature.getContainerClass().equals(EMapPropertyHolder.class)) {
+                            return eStructuralFeature.getFeatureID()
+                                    != KGraphPackage.EMAP_PROPERTY_HOLDER__PROPERTIES;
+                        } else {
+                            return true;
+                        }
+                    }
+                });
+            }
+            
+            return iterator;
+        }
+    }
     
     /**
      * Loads all {@link de.cau.cs.kieler.core.properties.IProperty IProperty} of elements that pass
@@ -746,13 +833,19 @@ public final class KimlUtil {
      * @param handledTypes
      *            a predicate checking if we desire to load data elements for a certain subclass of
      *            {@link EMapPropertyHolder}.
+     * @param clearProperties
+     *            {@code true} if the properties of a property holder should be cleared before
+     *            repopulating them based on persistent entries. Required if the removal of a persistent
+     *            entry should result in the removal of the correspondind property. This is for example
+     *            the case in our Xtext-based KGT editor.
      * @param knownProps
-     *            a set of additional properties that are known, hence should be parsed properly
+     *            a set of additional properties that are known, hence should be parsed properly.
      * 
      * @return the graph itself
      */
     public static KNode loadDataElements(final KNode graph,
-            final Predicate<EMapPropertyHolder> handledTypes, final IProperty<?>... knownProps) {
+            final Predicate<EMapPropertyHolder> handledTypes, final boolean clearProperties,
+            final IProperty<?>... knownProps) {
 
         Map<String, IProperty<?>> knowPropsMap = Maps.newHashMap();
         for (IProperty<?> p : knownProps) {
@@ -760,22 +853,30 @@ public final class KimlUtil {
         }
 
         LayoutMetaDataService dataService = LayoutMetaDataService.getInstance();
-        TreeIterator<EObject> iterator = graph.eAllContents();
+        
+        /* This is basically the same as graph.eAllContents(). However, using the latter would cause a
+         * ConcurrentModificationException. The reason we're walking through the graph here is that we
+         * rebuild properties based on persistent entries. But with eAllContents(), we would also iterate
+         * over the properties we're modifying. That's where the exception comes in. To avoid that, we're
+         * using a special tree iterator that skips over properties of EMapPropertyHolders. Magic!
+         * (KIPRA-1541)
+         */
+        TreeIterator<EObject> iterator = new PropertiesSkippingTreeIterator(graph, false);
         while (iterator.hasNext()) {
             EObject eObject = iterator.next();
             if (eObject instanceof EMapPropertyHolder
                     && handledTypes.apply((EMapPropertyHolder) eObject)) {
+                
                 final EMapPropertyHolder holder = (EMapPropertyHolder) eObject;
+                
+                if (clearProperties && holder.getProperties() != null) {
+                    holder.getProperties().clear();
+                }
+                
                 for (PersistentEntry persistentEntry : holder.getPersistentEntries()) {
                     loadDataElement(dataService, holder, persistentEntry.getKey(),
                             persistentEntry.getValue(), knowPropsMap);
                 }
-                
-                // Since we have looked at all the descendants of the property holder that are relevant
-                // to us, skip all of its descendants when walking through the tree. Not doing so
-                // actually causes a ConcurrentModificationException because we're modifying persistent
-                // entries while iterating over the EObject tree. (KIPRA-1541)
-                iterator.prune();
             }
         }
         
