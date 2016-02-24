28a5e580bb09d49c9d79f2be996931d0869f6141
Ulf Rueegg <uru@informatik.uni-kiel.de>
2016-02-24 15:42:01 +0100

    kiml.libavoid: migrated, moved layout options to new Libavoid.elkm

--- plugins/de.cau.cs.kieler.kiml.libavoid/src/de/cau/cs/kieler/kiml/libavoid/LibavoidProperties.java
+++ plugins/de.cau.cs.kieler.kiml.libavoid/src/de/cau/cs/kieler/kiml/libavoid/LibavoidProperties.java
@@ -13,11 +13,10 @@
  */
 package de.cau.cs.kieler.kiml.libavoid;
 
-import de.cau.cs.kieler.core.properties.IProperty;
-import de.cau.cs.kieler.core.properties.Property;
-
 /**
  * Class contains options to configure the Libavoid router.
+ * 
+ * FIXME remove this class as soon as default values have been migrated to the Libavoid.elkm
  * 
  * @author uru
  */
@@ -36,56 +35,56 @@ public final class LibavoidProperties {
     /*
      * Routing Penalties
      */
-    public static final IProperty<Float> SEGMENT_PENALTY = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.segmentPenalty", 10f, 0f);
-
-    public static final IProperty<Float> ANGLE_PENALTY = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.anglePenalty", 0f, 0f);
-
-    /** Experimental. */
-    public static final IProperty<Float> CROSSING_PENALTY = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.crossingPenalty", 0f, 0f);
-
-    /** Experimental. */
-    public static final IProperty<Float> CLUSTER_CROSSING_PENALTY = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.clusterCrossingPenalty", 4000f, 0f);
-
-    /** Experimental. */
-    public static final IProperty<Float> FIXED_SHARED_PATH_PENALTY = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.fixedSharedPathPenalty", 0f, 0f);
-
-    /** Experimental. */
-    public static final IProperty<Float> PORT_DIRECTION_PENALTY = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.portDirectionPenalty", 100f, 0f);
-
-    public static final IProperty<Float> SHAPE_BUFFER_DISTANCE = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.shapeBufferDistance", 4.0f, 0f);
-
-    public static final IProperty<Float> IDEAL_NUDGING_DISTANCE = new Property<Float>(
-            "de.cau.cs.kieler.kiml.libavoid.idealNudgingDistance", 4.0f, 0f);
+//    public static final IProperty<Float> SEGMENT_PENALTY = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.segmentPenalty", 10f, 0f);
+//
+//    public static final IProperty<Float> ANGLE_PENALTY = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.anglePenalty", 0f, 0f);
+//
+//    /** Experimental. */
+//    public static final IProperty<Float> CROSSING_PENALTY = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.crossingPenalty", 0f, 0f);
+//
+//    /** Experimental. */
+//    public static final IProperty<Float> CLUSTER_CROSSING_PENALTY = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.clusterCrossingPenalty", 4000f, 0f);
+//
+//    /** Experimental. */
+//    public static final IProperty<Float> FIXED_SHARED_PATH_PENALTY = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.fixedSharedPathPenalty", 0f, 0f);
+//
+//    /** Experimental. */
+//    public static final IProperty<Float> PORT_DIRECTION_PENALTY = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.portDirectionPenalty", 100f, 0f);
+//
+//    public static final IProperty<Float> SHAPE_BUFFER_DISTANCE = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.shapeBufferDistance", 4.0f, 0f);
+//
+//    public static final IProperty<Float> IDEAL_NUDGING_DISTANCE = new Property<Float>(
+//            "de.cau.cs.kieler.kiml.libavoid.idealNudgingDistance", 4.0f, 0f);
 
     /*
      * Routing Options
      */
-    public static final IProperty<Boolean> NUDGE_ORTHOGONAL_SEGMENTS = new Property<Boolean>(
-            "de.cau.cs.kieler.kiml.libavoid.nudgeOrthogonalSegmentsConnectedToShapes", false);
-
-    public static final IProperty<Boolean> IMPROVE_HYPEREDGES = new Property<Boolean>(
-            "de.cau.cs.kieler.kiml.libavoid.improveHyperedgeRoutesMovingJunctions", true);
-
-    /** Experimental. */
-    public static final IProperty<Boolean> PENALISE_ORTH_SHATE_PATHS = new Property<Boolean>(
-            "de.cau.cs.kieler.kiml.libavoid.penaliseOrthogonalSharedPathsAtConnEnds", false);
-
-    public static final IProperty<Boolean> NUDGE_ORTHOGONAL_COLINEAR_SEGMENTS =
-            new Property<Boolean>(
-                    "de.cau.cs.kieler.kiml.libavoid.nudgeOrthogonalTouchingColinearSegments", false);
-
-    public static final IProperty<Boolean> NUDGE_PREPROCESSING = new Property<Boolean>(
-            "de.cau.cs.kieler.kiml.libavoid.performUnifyingNudgingPreprocessingStep", true);
-
-    public static final IProperty<Boolean> IMPROVE_HYPEREDGES_ADD_DELETE = new Property<Boolean>(
-            "de.cau.cs.kieler.kiml.libavoid."
-                    + "improveHyperedgeRoutesMovingAddingAndDeletingJunctions", true);
+//    public static final IProperty<Boolean> NUDGE_ORTHOGONAL_SEGMENTS = new Property<Boolean>(
+//            "de.cau.cs.kieler.kiml.libavoid.nudgeOrthogonalSegmentsConnectedToShapes", false);
+//
+//    public static final IProperty<Boolean> IMPROVE_HYPEREDGES = new Property<Boolean>(
+//            "de.cau.cs.kieler.kiml.libavoid.improveHyperedgeRoutesMovingJunctions", true);
+//
+//    /** Experimental. */
+//    public static final IProperty<Boolean> PENALISE_ORTH_SHATE_PATHS = new Property<Boolean>(
+//            "de.cau.cs.kieler.kiml.libavoid.penaliseOrthogonalSharedPathsAtConnEnds", false);
+//
+//    public static final IProperty<Boolean> NUDGE_ORTHOGONAL_COLINEAR_SEGMENTS =
+//            new Property<Boolean>(
+//                    "de.cau.cs.kieler.kiml.libavoid.nudgeOrthogonalTouchingColinearSegments", false);
+//
+//    public static final IProperty<Boolean> NUDGE_PREPROCESSING = new Property<Boolean>(
+//            "de.cau.cs.kieler.kiml.libavoid.performUnifyingNudgingPreprocessingStep", true);
+//
+//    public static final IProperty<Boolean> IMPROVE_HYPEREDGES_ADD_DELETE = new Property<Boolean>(
+//            "de.cau.cs.kieler.kiml.libavoid."
+//                    + "improveHyperedgeRoutesMovingAddingAndDeletingJunctions", true);
 
 }
