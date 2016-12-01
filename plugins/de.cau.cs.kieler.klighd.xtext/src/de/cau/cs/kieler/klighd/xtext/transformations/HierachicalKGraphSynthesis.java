0acef739dffa2673c060d9d5c2e2cfbb9a708193
ybl <ybl@informatik.uni-kiel.de>
2016-12-01 11:05:42 +0100

    The nodes are now expanded and edges appear between the hierarchy level

--- plugins/de.cau.cs.kieler.klighd.xtext/src/de/cau/cs/kieler/klighd/xtext/transformations/HierachicalKGraphSynthesis.java
+++ plugins/de.cau.cs.kieler.klighd.xtext/src/de/cau/cs/kieler/klighd/xtext/transformations/HierachicalKGraphSynthesis.java
@@ -1,13 +1,21 @@
 package de.cau.cs.kieler.klighd.xtext.transformations;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import org.eclipse.elk.core.klayoutdata.KEdgeLayout;
 import org.eclipse.elk.core.klayoutdata.KLayoutData;
+import org.eclipse.elk.core.klayoutdata.KLayoutDataFactory;
+import org.eclipse.elk.core.klayoutdata.KShapeLayout;
+import org.eclipse.elk.graph.KEdge;
+import org.eclipse.elk.graph.KGraphFactory;
 import org.eclipse.elk.graph.KNode;
+import org.eclipse.elk.graph.impl.KGraphFactoryImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
 
-import de.cau.cs.kieler.klighd.krendering.KRendering;
 import de.cau.cs.kieler.klighd.util.KlighdProperties;
 
 /*
@@ -24,13 +32,17 @@ import de.cau.cs.kieler.klighd.util.KlighdProperties;
  */
 
 /**
- * @author lucy
+ * @author ybl
  *
  */
 public class HierachicalKGraphSynthesis {
+    private static Map<KNode, KNode> parents = new HashMap<>();
 
     /**
-     * Transform the graph to have the representation we want
+     * Transform the graph to have the representation we want.
+     * 
+     * @param diagram
+     *            the method takes a graph as input.
      */
     public static void transform(final KNode diagram) {
         // put the inner nodes onto the highest hierarchy level
@@ -39,20 +51,31 @@ public class HierachicalKGraphSynthesis {
         diagram.getChildren().addAll(nodes);
 
         deletion(diagram.getChildren());
+        addEdges(diagram);
+
     }
 
     private static List<KNode> recursiveTraversal(final KNode parent) {
         List<KNode> copiedChildren = new ArrayList<>();
 
         for (KNode child : parent.getChildren()) {
-            if (!child.getChildren().isEmpty()) { 
+            if (!child.getChildren().isEmpty()) {
 
                 // extract/copy content of children
                 Copier copier = new Copier();
                 KNode copy = (KNode) copier.copy(child);
+                
+                // delete the existing edges for the copy
+                if (copy.getOutgoingEdges() != null) {
+                    copy.getOutgoingEdges().clear();
+                }
+                if (copy.getIncomingEdges() != null) {
+                    copy.getIncomingEdges().clear();
+                }
                 copiedChildren.add(copy);
+                parents.put(copy, parent);
 
-                copiedChildren.addAll(recursiveTraversal(child));
+                copiedChildren.addAll(recursiveTraversal(copy));
             }
         }
 
@@ -60,7 +83,7 @@ public class HierachicalKGraphSynthesis {
     }
 
     /**
-     * delete all the deeper hierachy levels of the copied children
+     * Delete all the deeper hierachy levels of the copied children.
      * 
      * @param allNodes
      */
@@ -74,12 +97,36 @@ public class HierachicalKGraphSynthesis {
                 layoutDataChildOfChild.setProperty(KlighdProperties.EXPAND, true);
             }
 
-            //try to expand all the original children
+            // try to expand all the original children
             KLayoutData layoutDataNode = node.getData(KLayoutData.class);
             layoutDataNode.setProperty(KlighdProperties.COLLAPSED_RENDERING, false);
             layoutDataNode.setProperty(KlighdProperties.EXPANDED_RENDERING, true);
-            //layoutDataNode.setProperty(KlighdProperties.EXPAND, true);
+            layoutDataNode.setProperty(KlighdProperties.EXPAND, true);
+            KShapeLayout shape = node.getData(KShapeLayout.class);
+            shape.setHeight(200.0f);
+            shape.setWidth(200.0f);
         }
+    }
+
+    /**
+     * @param diagram
+     */
+    private static void addEdges(final KNode diagram) {
+        for (Entry<KNode, KNode> entry : parents.entrySet()) {
+            KNode child = entry.getKey();
+            KNode parent = entry.getValue();
+
+            // create an edge
+            KEdge edge = KGraphFactoryImpl.eINSTANCE.createKEdge();
+            KEdgeLayout edgeLayout = KLayoutDataFactory.eINSTANCE.createKEdgeLayout();
+            edge.getData().add(edgeLayout);
+
+            edge.setSource(parent);
+            edge.setTarget(child);
+            
+            parent.getOutgoingEdges().add(edge);
+        }
+
     }
 
 }
