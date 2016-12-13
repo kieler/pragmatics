da16883e2740053cd647d98bb45730f38b173cbf
Alexander Barbie <abar@informatik.uni-kiel.de>
2016-12-13 12:48:34 +0100

    clearing blue/grey boxes

--- plugins/de.cau.cs.kieler.klighd.xtext/src/de/cau/cs/kieler/klighd/xtext/transformations/HierachicalKGraphSynthesis.java
+++ plugins/de.cau.cs.kieler.klighd.xtext/src/de/cau/cs/kieler/klighd/xtext/transformations/HierachicalKGraphSynthesis.java
@@ -6,15 +6,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.eclipse.elk.core.klayoutdata.KLayoutDataFactory;
+import org.eclipse.elk.graph.properties.IProperty;
 import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
 
 import de.cau.cs.kieler.klighd.kgraph.KEdge;
-import de.cau.cs.kieler.klighd.kgraph.KEdgeLayout;
-import de.cau.cs.kieler.klighd.kgraph.KLayoutData;
 import de.cau.cs.kieler.klighd.kgraph.KNode;
-import de.cau.cs.kieler.klighd.kgraph.KShapeLayout;
 import de.cau.cs.kieler.klighd.kgraph.impl.KGraphFactoryImpl;
+import de.cau.cs.kieler.klighd.krendering.KRectangle;
 import de.cau.cs.kieler.klighd.util.KlighdProperties;
 
 /*
@@ -46,68 +44,66 @@ public class HierachicalKGraphSynthesis {
     public static void transform(final KNode diagram) {
         // put the inner nodes onto the highest hierarchy level
         List<KNode> nodes = recursiveTraversal(diagram);
-        
+
         diagram.getChildren().clear();
         diagram.getChildren().addAll(nodes);
 
-//        deletion(diagram.getChildren());
-//        addEdges(diagram);
+        // deletion(diagram.getChildren());
+        // addEdges(diagram);
 
     }
 
-    private static boolean isLeaf(final KNode node) {
-        return node.getChildren().isEmpty();
-    }
-    
     private static KNode copyWithoutBlueBox(final KNode parent) {
         List<KNode> copies = new ArrayList<KNode>();
         Copier copier = new Copier();
-        
-        for(KNode child : parent.getChildren()) {
+
+        for (KNode child : parent.getChildren()) {
             List<KNode> children = child.getChildren();
             KNode copy;
             // Remove useless blue boxes, if there is only one expandable child inside
-            if(!(children.size() == 1 && !children.get(0).getChildren().isEmpty())) {
+            if (!(children.size() == 1 && !children.get(0).getChildren().isEmpty())) {
                 clearGrandchildren(child);
-                copy = (KNode) copier.copy(child); 
+                copy = (KNode) copier.copy(child);
+                copy.getChildren().clear();
+
             } else {
-                for(KNode grandChild : child.getChildren()) {
+                for (KNode grandChild : child.getChildren()) {
                     clearGrandchildren(grandChild);
                 }
-                copy = (KNode) copier.copy(child.getChildren().get(0));
+                // copy = (KNode) copier.copy(child.getChildren().get(0));
+                copy = (KNode) copier.copy(child);
             }
-            
+
             copier.copyReferences();
-            
+
             copies.add(copy);
         }
-        
+
         parent.getChildren().clear();
         parent.getChildren().addAll(copies);
         return parent;
     }
-    
+
     private static List<KNode> recursiveTraversal(final KNode parent) {
         List<KNode> copiedChildren = new ArrayList<>();
-        
-        if(parent.getChildren().isEmpty()) {
+
+        if (parent.getChildren().isEmpty()) {
             return copiedChildren;
         }
-        
-        
+
         for (KNode child : parent.getChildren()) {
             copiedChildren.addAll(recursiveTraversal(child));
-            
+
             List<KNode> grandChildren = child.getChildren();
 
             // Ignore blue boxes and copied only the child inside
-            if(!(grandChildren.size() == 1 && !grandChildren.get(0).getChildren().isEmpty())) {
+            if (!(grandChildren.size() == 1 && !grandChildren.get(0).getChildren().isEmpty())) {
                 if (!grandChildren.isEmpty()) {
-                    
+
                     // extract/copy content of children
-                    
+
                     KNode copy = copyWithoutBlueBox(child);
-                    
+
                     // delete the existing edges for the copy
                     if (copy.getOutgoingEdges() != null) {
                         copy.getOutgoingEdges().clear();
@@ -117,6 +113,23 @@ public class HierachicalKGraphSynthesis {
                     }
                     copiedChildren.add(copy);
                     parents.put(copy, parent);
+
+                    // Display expandable Nodes smaller without their content.
+                    // TODO shrink size of root node.
+                    for (KNode k : grandChildren) {
+                        Map<IProperty<?>, Object> map =
+                                k.getData(KRectangle.class).getAllProperties();
+                        if (!map.isEmpty()) {
+                            for (Map.Entry<IProperty<?>, Object> entry : map.entrySet()) {
+                                if (entry.getKey().toString()
+                                        .equals("de.cau.cs.kieler.klighd.collapsedRendering")) {
+                                    k.setHeight(200.0f);
+                                    k.setWidth(200.0f);
+                                }
+                            }
+                        }
+                    }
+
                 }
             }
         }
@@ -125,11 +138,11 @@ public class HierachicalKGraphSynthesis {
     }
 
     private static void clearGrandchildren(KNode child) {
-        for(KNode grandChild : child.getChildren()) {
+        for (KNode grandChild : child.getChildren()) {
             grandChild.getChildren().clear();
-        }      
+        }
     }
-    
+
     /**
      * Delete all the deeper hierachy levels of the copied children.
      * 
@@ -139,18 +152,18 @@ public class HierachicalKGraphSynthesis {
         for (KNode node : allNodes) {
             for (KNode childOfChild : node.getChildren()) {
                 childOfChild.getChildren().clear();
-                //KLayoutData layoutDataChildOfChild = childOfChild.getData(KLayoutData.class);
+                // KLayoutData layoutDataChildOfChild = childOfChild.getData(KLayoutData.class);
                 node.setProperty(KlighdProperties.COLLAPSED_RENDERING, true);
                 node.setProperty(KlighdProperties.EXPANDED_RENDERING, false);
                 node.setProperty(KlighdProperties.EXPAND, true);
             }
 
             // try to expand all the original children
-//            KLayoutData layoutDataNode = node.getData(KLayoutData.class);
+            // KLayoutData layoutDataNode = node.getData(KLayoutData.class);
             node.setProperty(KlighdProperties.COLLAPSED_RENDERING, false);
             node.setProperty(KlighdProperties.EXPANDED_RENDERING, true);
             node.setProperty(KlighdProperties.EXPAND, true);
-//            KShapeLayout shape = node.getData(KShapeLayout.class);
+            // KShapeLayout shape = node.getData(KShapeLayout.class);
             node.setHeight(200.0f);
             node.setWidth(200.0f);
         }
@@ -166,12 +179,12 @@ public class HierachicalKGraphSynthesis {
 
             // create an edge
             KEdge edge = KGraphFactoryImpl.eINSTANCE.createKEdge();
-//            KEdgeLayout edgeLayout = KLayoutDataFactory.eINSTANCE.createKEdgeLayout();
-//            edge.getData().add(edgeLayout);
+            // KEdgeLayout edgeLayout = KLayoutDataFactory.eINSTANCE.createKEdgeLayout();
+            // edge.getData().add(edgeLayout);
 
             edge.setSource(parent);
             edge.setTarget(child);
-            
+
             parent.getOutgoingEdges().add(edge);
         }
 
