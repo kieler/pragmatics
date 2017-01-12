ff837e281dade8c91f6c5372be367d211e4d701f
Daniel Jahn <dja@informatik.uni-kiel.de>
2017-01-12 23:42:29 +0100

    First Version of my own Grid Layouter that minimizes whitespace of the Hierarchical Diagram.

--- de.cau.cs.kieler.hierarchicalLayoutAlgorithms/src/de/cau/cs/kieler/hierarchicalLayoutAlgorithms/GridSnapLayoutProvider.java
+++ de.cau.cs.kieler.hierarchicalLayoutAlgorithms/src/de/cau/cs/kieler/hierarchicalLayoutAlgorithms/GridSnapLayoutProvider.java
@@ -1,5 +1,6 @@
 package de.cau.cs.kieler.hierarchicalLayoutAlgorithms;
 
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,6 +29,9 @@ public class GridSnapLayoutProvider extends AbstractLayoutProvider {
 	 * an int overflow.
 	 */
 	private static final int INFINITY = (Integer.MAX_VALUE / 2) - 1;
+	private static final double DOUBLE_INFINITY = Double.MAX_VALUE;
+	// TODO make as option
+	private static final float SPACING = 100;
 	private static float dL;
 	private static float wp;
 	private static float width;
@@ -41,35 +45,201 @@ public class GridSnapLayoutProvider extends AbstractLayoutProvider {
 	private static Map<KNode, Float> dispMap = new HashMap<KNode, Float>();
 	private static int gridwidth;
 	private static int gridheight;
+	private static List<Point2D.Double> availablePositions = new ArrayList<Point2D.Double>();
+	private static int averageNodeWidth;
 
 	@Override
 	public void layout(KNode layoutGraph, IElkProgressMonitor progressMonitor) {
 		progressMonitor.begin("Grid Snap Layouter", 1);
 
-		simpleGrid(layoutGraph);
+		minimizingWhitespaceGrid(layoutGraph);
 
 		// initializeGrid(layoutGraph);
 		// initializeNodes(layoutGraph);
 		// pStress();
 
 		routeEdges();
-		//routeBetterEdges();
+		// routeBetterEdges();
 
 		progressMonitor.done();
 	}
 
+	/**
+	 * 
+	 * @param diagram
+	 */
+	private static void minimizingWhitespaceGrid(KNode diagram) {
+		// KLayoutData diagramData = diagram.getData(KLayoutData.class);
+		ForceLayoutProvider force = new ForceLayoutProvider();
+		// diagramData.setProperty(ForceOptions.SPACING_NODE, 20.0f);
+		// diagramData.setProperty(ForceOptions.SPACING_BORDER, 20.0f);
+		BasicProgressMonitor forceMonitor = new BasicProgressMonitor();
+		force.layout(diagram, forceMonitor);
+
+		edges.clear();
+		children.clear();
+		width = 0.0f;
+		height = 0.0f;
+		availablePositions.clear();
+
+		children.addAll(diagram.getChildren());
+
+		// Initialize Grid
+		int gridNodesSize = children.size();
+		int gridSize = (int) Math.ceil(Math.sqrt(gridNodesSize));
+		// System.out.println("Nodes: " + gridNodesSize);
+		// System.out.println("Size: " + gridSize);
+
+		Map<Point2D.Double, Integer> gridNodePosition = new HashMap<Point2D.Double, Integer>();
+		int xPos = 0;
+		int yPos = 0;
+		for (int i = 0; i < gridSize; i++) {
+			xPos = 0;
+			for (int j = 0; j < gridSize; j++) {
+				Point2D.Double point = new Point2D.Double(xPos, yPos);
+				gridNodePosition.put(point, i * gridSize + j);
+				availablePositions.add(point);
+				xPos += 50;
+			}
+			yPos += 50;
+		}
+
+		// calculate ralative closest grid position and check if available
+		KShapeLayout diagramLayout = diagram.getData(KShapeLayout.class);
+		double diagramwidth = diagramLayout.getWidth();
+		double diagramheight = diagramLayout.getHeight();
+		gridwidth = xPos - 50;
+		gridheight = yPos - 50;
+		Map<Integer, KNode> gridNodeMap = new HashMap<Integer, KNode>();
+		int nodewidth = 0;
+
+		for (KNode node : children) {
+			for (KEdge edge : node.getOutgoingEdges()) {
+				if (children.contains(edge.getTarget())) {
+					edges.add(edge);
+				}
+			}
+
+			KShapeLayout nodeLayout = node.getData(KShapeLayout.class);
+			nodewidth += nodeLayout.getWidth();
+			int gridXpos = (int) ((nodeLayout.getXpos() + nodeLayout.getWidth() / diagramwidth) * gridwidth);
+			int gridYpos = (int) ((nodeLayout.getYpos() + nodeLayout.getHeight() / diagramheight) * gridheight);
+			// TODO Fix calculation of nearest Position
+			Point2D point = nearestAvailablePosition(
+					new Point2D.Double(round(gridXpos, gridwidth), round(gridYpos, gridheight)));
+			int mapPosition = gridNodePosition.get(point);
+			gridNodeMap.put(mapPosition, node);
+		}
+		averageNodeWidth = nodewidth / children.size();
+		float newWidth = 20;
+		float newHeight = 20;
+		Map<Integer, Float> rowwidth = new HashMap<Integer, Float>();
+		for (int i = 0; i < gridSize; i++) {
+			for (int j = 0; j < gridSize; j++) {
+				if (!(gridNodeMap.get(i * gridSize + j) == null)) {
+					KNode node = gridNodeMap.get(i * gridSize + j);
+					KShapeLayout nodeLayout = node.getData(KShapeLayout.class);
+					if (rowwidth.containsKey(j)) {
+						if (rowwidth.get(j) < nodeLayout.getWidth()) {
+							rowwidth.put(j, nodeLayout.getWidth());
+						}
+					} else {
+						rowwidth.put(j, nodeLayout.getWidth());
+					}
+				}
+			}
+		}
+
+		float nextXpos = 20;
+		float nextYpos = 20;
+		float rowheight = 20;
+		for (int i = 0; i < gridSize; i++) {
+			for (int j = 0; j < gridSize; j++) {
+				if (!(gridNodeMap.get(i * gridSize + j) == null)) {
+					KNode node = gridNodeMap.get(i * gridSize + j);
+					KShapeLayout nodeLayout = node.getData(KShapeLayout.class);
+					if (rowheight < nodeLayout.getHeight()) {
+						rowheight = nodeLayout.getHeight();
+					}
+					nodeLayout.setPos(nextXpos, nextYpos);
+					if (newWidth < nodeLayout.getXpos() + nodeLayout.getWidth()) {
+						newWidth = nodeLayout.getXpos() + nodeLayout.getWidth();
+					}
+					if (newHeight < nodeLayout.getYpos() + nodeLayout.getHeight()) {
+						newHeight = nodeLayout.getYpos() + nodeLayout.getHeight();
+					}
+					nextXpos = nodeLayout.getXpos() + rowwidth.get(j) + SPACING;
+				} else {
+					nextXpos += rowwidth.get(j) + SPACING;
+				}
+			}
+			nextYpos += rowheight + SPACING;
+			rowheight = 0;
+			nextXpos = 20;
+		}
+
+		diagramLayout.setWidth(newWidth + 20);
+		diagramLayout.setHeight(newHeight + 20);
+	}
+
+	/**
+	 * 
+	 * @param point
+	 * @return
+	 */
+	private static Point2D nearestAvailablePosition(Point2D point) {
+		Point2D.Double position = new Point2D.Double(0, 0);
+		double smallestDistance = DOUBLE_INFINITY;
+		double currentDistance;
+		for (Point2D.Double p : availablePositions) {
+			currentDistance = point.distance(p);
+			if (currentDistance < smallestDistance) {
+				smallestDistance = currentDistance;
+				position = p;
+			}
+		}
+		availablePositions.remove(position);
+
+		return position;
+	}
+
+	/**
+	 * 
+	 * @param x
+	 * @param maxvalue
+	 * @return
+	 */
+	private static double round(int x, int maxvalue) {
+		int result;
+		if ((x % 50) > 50 / 2) {
+			result = x + 50 - x % 50;
+		} else {
+			result = x - x % 50;
+		}
+
+		if (result > maxvalue) {
+			result = maxvalue;
+		}
+
+		return (double) result;
+	}
+
+	/**
+	 * 
+	 * @param diagram
+	 */
 	private static void simpleGrid(KNode diagram) {
 		KLayoutData data = diagram.getData(KLayoutData.class);
-//		LayeredLayoutProvider layered = new LayeredLayoutProvider();
-//		BasicProgressMonitor layeredMonitor = new BasicProgressMonitor();
-//		layered.layout(diagram, layeredMonitor);
-		
+		// LayeredLayoutProvider layered = new LayeredLayoutProvider();
+		// BasicProgressMonitor layeredMonitor = new BasicProgressMonitor();
+		// layered.layout(diagram, layeredMonitor);
+
 		ForceLayoutProvider force = new ForceLayoutProvider();
 		data.setProperty(ForceOptions.SPACING_NODE, 20.0f);
 		data.setProperty(ForceOptions.SPACING_BORDER, 20.0f);
 		BasicProgressMonitor forceMonitor = new BasicProgressMonitor();
 		force.layout(diagram, forceMonitor);
-		
+
 		edges.clear();
 		children.clear();
 		width = 0.0f;
@@ -85,15 +255,15 @@ public class GridSnapLayoutProvider extends AbstractLayoutProvider {
 		gridheight = (int) (height / (children.size() / 2));
 
 		for (KNode node : children) {
-//			if (node.getChildren().size() == 3) {
-//				KShapeLayout shape = node.getData(KShapeLayout.class);
-//				shape.setHeight(100);
-//				shape.setWidth(700);
-//				shape.setPos(0, 0);
-//				BasicProgressMonitor layeredMonitor = new BasicProgressMonitor();
-//				LayeredLayoutProvider layered = new LayeredLayoutProvider();
-//				layered.layout(node, layeredMonitor);
-//			}
+			// if (node.getChildren().size() == 3) {
+			// KShapeLayout shape = node.getData(KShapeLayout.class);
+			// shape.setHeight(100);
+			// shape.setWidth(700);
+			// shape.setPos(0, 0);
+			// BasicProgressMonitor layeredMonitor = new BasicProgressMonitor();
+			// LayeredLayoutProvider layered = new LayeredLayoutProvider();
+			// layered.layout(node, layeredMonitor);
+			// }
 			for (KEdge edge : node.getOutgoingEdges()) {
 				if (children.contains(edge.getTarget())) {
 					edges.add(edge);
@@ -107,20 +277,10 @@ public class GridSnapLayoutProvider extends AbstractLayoutProvider {
 			xPos = calculateNewPos(xPos, gridwidth);
 			yPos = calculateNewPos(yPos, gridheight);
 
-			//TODO check availability
+			// TODO check availability
 			nodeLayout.setPos(xPos, yPos);
 		}
 
-	}
-	
-	/**
-	 * 
-	 * @param diagram
-	 */
-	private static void minimizingWhitespaceGrid(KNode diagram) {
-		// TODO init grid
-		// TODO calculate ralative closest grid position and check if available
-		// TODO maybe make grid bigger because nodes are too big
 	}
 
 	/**
