28a3d7adb861909a17fafb099767a55e45372b28
Daniel Jahn <dja@informatik.uni-kiel.de>
2017-01-27 09:30:39 +0100

    Added a method to get all the hierarchical edges of the graph.

--- plugins/de.cau.cs.kieler.hierarchicalLayoutAlgorithms/src/de/cau/cs/kieler/hierarchicalLayoutAlgorithms/HierarchicalUtil.java
+++ plugins/de.cau.cs.kieler.hierarchicalLayoutAlgorithms/src/de/cau/cs/kieler/hierarchicalLayoutAlgorithms/HierarchicalUtil.java
@@ -8,6 +8,11 @@ import org.eclipse.elk.graph.KNode;
 
 public class HierarchicalUtil {
 
+	/**
+	 * Computes a List of nodes that are the neighbours with outgoing edges.
+	 * @param node
+	 * @return List of neighbours with outgoing edges.
+	 */
 	public static List<KNode> getSuccessor(KNode node) {
 		List<KNode> children = new ArrayList<>();
 		for (KEdge outgoingEdge : node.getOutgoingEdges()) {
@@ -19,6 +24,11 @@ public class HierarchicalUtil {
 		return children;
 	}
 	
+	/**
+	 * Computes the root node of the graph.
+	 * @param graph
+	 * @return root node of graph.
+	 */
 	public static KNode findRoot(KNode graph) {
 		for (KNode child : graph.getChildren()) {
 			if (child.getIncomingEdges().isEmpty()) {
@@ -28,6 +38,11 @@ public class HierarchicalUtil {
 		return null;
 	}
 	
+	/**
+	 * Computes the number of Leafs that a node has.
+	 * @param node
+	 * @return number of leafs.
+	 */
 	public static int getNumberOfLeafs(KNode node) {
 		int leafs = 0;
 		if (HierarchicalUtil.getSuccessor(node).isEmpty()) {
@@ -38,5 +53,22 @@ public class HierarchicalUtil {
 			}
 		}
 		return leafs;
+	}
+	
+	/**
+	 * Computes a List of Edges that are the Hierarchical edges of the Graph.
+	 * @param graph
+	 * @return List of Hierarchical Edges.
+	 */
+	public static List<KEdge> getHierarchicalEdges(KNode graph) {
+		List<KEdge> edges = new ArrayList<KEdge>();
+		for (KNode node : graph.getChildren()) {
+			for (KEdge edge : node.getOutgoingEdges()) {
+				if (graph.getChildren().contains(edge.getTarget())) {
+					edges.add(edge);
+				}
+			}
+		}
+		return edges;
 	}
 }
