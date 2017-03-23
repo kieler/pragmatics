7765c415c482da7ed03dd3ad1d9fc6ca0cd86706
Daniel Jahn <dja@informatik.uni-kiel.de>
2017-03-23 13:43:40 +0100

    Changed elk header to kieler header.
    Signed-off-by: Daniel Jahn <dja@informatik.uni-kiel.de>

--- plugins/de.cau.cs.kieler.hierarchicalLayoutAlgorithms/src/de/cau/cs/kieler/hierarchicalLayoutAlgorithms/radial/ExplosionLineRouter.java
+++ plugins/de.cau.cs.kieler.hierarchicalLayoutAlgorithms/src/de/cau/cs/kieler/hierarchicalLayoutAlgorithms/radial/ExplosionLineRouter.java
@@ -1,13 +1,16 @@
-/*******************************************************************************
- * Copyright (c) 2017 Kiel University and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
  *
- * Contributors:
- *    Kiel University - initial API and implementation
- *******************************************************************************/
+ * http://www.informatik.uni-kiel.de/rtsys/kieler/
+ * 
+ * Copyright 2017 by
+ * + Kiel University
+ *   + Department of Computer Science
+ *     + Real-Time and Embedded Systems Group
+ * 
+ * This code is provided under the terms of the Eclipse Public License (EPL).
+ * See the file epl-v10.html for the license text.
+ */
 package de.cau.cs.kieler.hierarchicalLayoutAlgorithms.radial;
 
 import java.util.ArrayList;
